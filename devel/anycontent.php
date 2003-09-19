04215c91ee21e7385b05527bd2f5e4e479e6ac5b
tarique <tarique@62f5abee-3c11-0410-b416-987344bf19e2>
2003-09-19 09:53:28 +0000

    The placeholder file for the anycontent block

---
+++ devel/anycontent.php
@@ -0,0 +1,21 @@
+<?php
+// ------------------------------------------------------------------------- //
+//  Coppermine Photo Gallery                                                 //
+// ------------------------------------------------------------------------- //
+//  Copyright (C) 2002,2003  Gr�ory DEMAR <gdemar@wanadoo.fr>                //
+//  http://www.chezgreg.net/coppermine/                                      //
+// ------------------------------------------------------------------------- //
+//  Based on PHPhotoalbum by Henning Stverud <henning@stoverud.com>          //
+//  http://www.stoverud.com/PHPhotoalbum/                                    //
+// ------------------------------------------------------------------------- //
+//  Hacked by Tarique Sani <tarique@sanisoft.com>                            //
+//  see http://www.sanisoft.com/cpg/README.txt for                           //
+//  details                                                                  //
+// ------------------------------------------------------------------------- //
+//  This program is free software; you can redistribute it and/or modify     //
+//  it under the terms of the GNU General Public License as published by     //
+//  the Free Software Foundation; either version 2 of the License, or        //
+//  (at your option) any later version.                                      //
+// ------------------------------------------------------------------------- //
+?>
+<h1>Edit the contents of anycontent.php to change this content</h1>
