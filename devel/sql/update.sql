ff7c2eeb3976e06578f4fb1f14970b640ca7bac9
datajack <datajack@62f5abee-3c11-0410-b416-987344bf19e2>
2003-09-25 20:57:33 +0000

    Fixed some table names to use the CPG macro

--- devel/sql/update.sql
+++ devel/sql/update.sql
@@ -5,8 +5,8 @@
 ALTER TABLE CPG_comments add msg_raw_ip tinytext;
 ALTER TABLE CPG_comments add msg_hdr_ip tinytext;
 
-INSERT INTO `cpg11d_config` VALUES ('thumb_use', 'ht');
-INSERT INTO `cpg11d_config` VALUES ('show_private', '0');
-INSERT INTO `cpg11d_config` VALUES ('first_level', '1');
-INSERT INTO `cpg11d_config` VALUES ('display_film_strip', '1');
-INSERT INTO `cpg11d_config` VALUES ('max_film_strip_items', '5');
+INSERT INTO CPG_config VALUES ('thumb_use', 'ht');
+INSERT INTO CPG_config VALUES ('show_private', '0');
+INSERT INTO CPG_config VALUES ('first_level', '1');
+INSERT INTO CPG_config VALUES ('display_film_strip', '1');
+INSERT INTO CPG_config VALUES ('max_film_strip_items', '5');
