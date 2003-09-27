9a22fca1747ad53e7a88e131884278bb5490ae5b
gtroll <gtroll@62f5abee-3c11-0410-b416-987344bf19e2>
2003-09-27 12:48:12 +0000

    CPG prefix fix

--- devel/sql/update.sql
+++ devel/sql/update.sql
@@ -12,4 +12,4 @@ INSERT INTO CPG_config VALUES ('display_film_strip', '1');
 INSERT INTO CPG_config VALUES ('max_film_strip_items', '5');
 INSERT INTO CPG_config VALUES ('comment_email_notification', '0');
 
-ALTER TABLE `cpg11d_pictures` ADD `description` VARCHAR( 255 ) NOT NULL AFTER `caption` ;
+ALTER TABLE `CPG_pictures` ADD `description` VARCHAR( 255 ) NOT NULL AFTER `caption` ;
