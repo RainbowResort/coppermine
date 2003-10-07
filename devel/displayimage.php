8f5320008fba1af2751e4385f5a5edbe84994f2f
gtroll <gtroll@62f5abee-3c11-0410-b416-987344bf19e2>
2003-10-07 02:08:17 +0000

    Fix for niceurl display bug discussed here http://coppermine.sourceforge.net/board/viewtopic.php?t=315

--- devel/displayimage.php
+++ devel/displayimage.php
@@ -276,7 +276,7 @@ function html_picinfo()
         }
 
         //Create the absolute URL for display in info
-        $info['URL']='<a href='.$CONFIG["ecards_more_pic_target"].$_SERVER["SCRIPT_NAME"]."?pos=-$CURRENT_PIC_DATA[pid]".' >'.$CONFIG["ecards_more_pic_target"].$_SERVER["SCRIPT_NAME"]."?pos=-$CURRENT_PIC_DATA[pid]".'</a>';
+        $info['URL']='<a href='.$CONFIG["ecards_more_pic_target"].'/'.basename($_SERVER['PHP_SELF'])."?pos=-$CURRENT_PIC_DATA[pid]".' >'.$CONFIG["ecards_more_pic_target"].'/'.basename($_SERVER['PHP_SELF'])."?pos=-$CURRENT_PIC_DATA[pid]".'</a>';
                 //with subdomains the variable is $_SERVER["SERVER_NAME"] does not return the right value instead of using a new config variable I reused $CONFIG["ecards_more_pic_target"] no trailing slash in the configure
 
         //Create the add to fav link
