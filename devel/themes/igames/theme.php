6d6291fffb0a1b162b2f3b80c0aa1744033d5829
jasendorf <jasendorf@62f5abee-3c11-0410-b416-987344bf19e2>
2003-10-16 03:50:58 +0000

    fixed "name edit-smilies off" bug in all themes.

--- devel/themes/igames/theme.php
+++ devel/themes/igames/theme.php
@@ -630,6 +630,11 @@ $template_image_comments = <<<EOT
                                                 <form name="f{MSG_ID}" method="POST" action="db_input.php">
                                                 <input type="hidden" name="event" value="comment_update">
                                                 <input type="hidden" name="msg_id" value="{MSG_ID}">
+                                                <td> 
+                                                <input type=text name=msg_author value={MSG_AUTHOR} class="textinput" size=10> 
+                                                </td> 
+                                        </tr>
+                                        <tr>
                                                 <td width="100%">
                                                         <textarea cols="40" rows="2" class="textinput" name="msg_body" style="width: 100%;">{MSG_BODY_RAW}</textarea>
                                                 </td>
