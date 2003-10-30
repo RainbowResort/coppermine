8e908c9cec6580e8fc9592cfc503f6fad9481d2c
jasendorf <jasendorf@62f5abee-3c11-0410-b416-987344bf19e2>
2003-10-30 15:07:36 +0000

    changed line 187 in editpics 'x' to ' &times; ' and seperated the KB from the value with a space editted arabic.php and english.php changed x to &times; and seperated KB from the value with a space

--- devel/editpics.php
+++ devel/editpics.php
@@ -184,7 +184,7 @@ function form_pic_info($text)
 	global $CURRENT_PIC, $THUMB_ROWSPAN, $CONFIG, $lang_byte_units, $lang_editpics_php;
 
 	if (UPLOAD_APPROVAL_MODE) {
-	    $pic_info = $CURRENT_PIC['pwidth'].'x'.$CURRENT_PIC['pheight'].' - '.($CURRENT_PIC['filesize'] >> 10).$lang_byte_units[1];
+	    $pic_info = $CURRENT_PIC['pwidth'].' &times; '.$CURRENT_PIC['pheight'].' - '.($CURRENT_PIC['filesize'] >> 10).$lang_byte_units[1];
 		if($CURRENT_PIC['owner_name']){
 			$pic_info .= ' - <a href ="profile.php?uid='.$CURRENT_PIC['owner_id'].'" target="_blank">'.$CURRENT_PIC['owner_name'].'</a>';
 		}
