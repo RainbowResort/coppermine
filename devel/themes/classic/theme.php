d43c87906087eea05379b28fbab67a7f8770f8ef
oddeveloper <oddeveloper@62f5abee-3c11-0410-b416-987344bf19e2>
2004-05-01 00:07:59 +0000

    changed two occurances of 'themes/default' to 'themes/classic'

--- devel/themes/classic/theme.php
+++ devel/themes/classic/theme.php
@@ -247,7 +247,7 @@ EOT;
 $template_film_strip = <<<EOT
 
         <tr>
-         <td valign="top" background='themes/default/images/tile.gif' align="center" height='30'>&nbsp;</td>
+         <td valign="top" background='themes/classic/images/tile.gif' align="center" height='30'>&nbsp;</td>
         </tr>
         <tr>
         <td valign="bottom" class="thumbnails" align="center">
@@ -255,7 +255,7 @@ $template_film_strip = <<<EOT
         </td>
         </tr>
         <tr>
-         <td valign="top" background='themes/default/images/tile.gif' align="center" height='30'>&nbsp;</td>
+         <td valign="top" background='themes/classic/images/tile.gif' align="center" height='30'>&nbsp;</td>
         </tr>
 <!-- BEGIN thumb_cell -->
                                         <a href="{LINK_TGT}">{THUMB}</a>&nbsp;
