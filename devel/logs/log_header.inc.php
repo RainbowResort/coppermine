6f339a6aee00d14ab1d2037110ac88b39a7fab4b
oddeveloper <oddeveloper@62f5abee-3c11-0410-b416-987344bf19e2>
2004-06-19 01:36:54 +0000

    log header file

---
+++ devel/logs/log_header.inc.php
@@ -0,0 +1,24 @@
+<?php
+// ------------------------------------------------------------------------- //
+// Coppermine Photo Gallery 1.3.0                                            //
+// ------------------------------------------------------------------------- //
+// Copyright (C) 2002,2003 Gregory DEMAR                                     //
+// http://www.chezgreg.net/coppermine/                                       //
+// ------------------------------------------------------------------------- //
+// Updated by the Coppermine Dev Team                                        //
+// (http://coppermine.sf.net/team/)                                          //
+// see /docs/credits.html for details                                        //
+// ------------------------------------------------------------------------- //
+// This program is free software; you can redistribute it and/or modify      //
+// it under the terms of the GNU General Public License as published by      //
+// the Free Software Foundation; either version 2 of the License, or         //
+// (at your option) any later version.                                       //
+// ------------------------------------------------------------------------- //
+// If you know regular expressions scroll down to ADD YOUR OWN PARSEMODES    //
+// HERE to modify the included parsemodes.                                   //
+// ------------------------------------------------------------------------- //
+if (!defined('IN_COPPERMINE')) {
+        die();
+}
+?>
+
