81dfc0763c36b33a798184bedf6638c016187060
tarique <tarique@62f5abee-3c11-0410-b416-987344bf19e2>
2004-07-10 13:47:42 +0000

    Added PHPdoc style header and comments

--- devel/addpic.php
+++ devel/addpic.php
@@ -14,8 +14,16 @@
 // the Free Software Foundation; either version 2 of the License, or         //
 // (at your option) any later version.                                       //
 // ------------------------------------------------------------------------- //
-/*
-$Id$
+/**
+* Coppermine Photo Gallery 1.3.0 addpic.php
+* 
+* This file file gets called in the img src when you do batch add, there is nothing 
+* much to look here the grunt work is done by the function add_picture 
+* 
+* @copyright 2002,2003 Gregory DEMAR, Coppermine Dev Team
+* @license http://opensource.org/licenses/gpl-license.php GNU General Public License V2
+* @package Coppermine
+* @version $Id$
 */
 
 define('IN_COPPERMINE', true);
