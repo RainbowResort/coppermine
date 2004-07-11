be8b276cc674aa3c6c43ed73cb6ebaf7b39c7312
oddeveloper <oddeveloper@62f5abee-3c11-0410-b416-987344bf19e2>
2004-07-11 17:08:09 +0000

    added plugin api settings

--- devel/sql/basic.sql
+++ devel/sql/basic.sql
@@ -123,6 +123,19 @@ INSERT INTO CPG_config VALUES ('smtp_host', '');
 INSERT INTO CPG_config VALUES ('smtp_username', '');
 INSERT INTO CPG_config VALUES ('smtp_password', '');
 
+INSERT INTO CPG_config VALUES ('enable_plugins', '1');
+
+CREATE TABLE CPG_plugins (
+  plugin_id int(10) unsigned NOT NULL auto_increment,
+  name varchar(64) NOT NULL default '',
+  path varchar(128) NOT NULL default '',
+  priority int(10) unsigned NOT NULL default '0',
+  PRIMARY KEY  (plugin_id),
+  UNIQUE KEY name (name),
+  UNIQUE KEY path (path)
+) TYPE=MyISAM COMMENT='Stores the plugins';
+
+
 #
 # Dumping data for table `CPG_filetypes`
 #
