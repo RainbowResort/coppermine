77410fa7e21e4c6e008bb555ebaf4a90469d5e96
oddeveloper <oddeveloper@62f5abee-3c11-0410-b416-987344bf19e2>
2004-07-27 15:47:09 +0000

    expanded sample to show how to utilize configure action

--- devel/plugins/sample/codebase.php
+++ devel/plugins/sample/codebase.php
@@ -1,10 +1,46 @@
 <?php
+// Add an install action
+cpg_add_action('plugin_install','sample_install');
+
+// Add a configure action
+cpg_add_action('plugin_configure','sample_configure');
+
 // Add a filter for the gallery header
 cpg_add_filter('gallery_header','sample_header');
+
 
 // Sample function to modify gallery header html
 function sample_header($html) {
     global $thisplugin;
     return '<p style="color:red;"><b>This is sample data returned from plugin "'.$thisplugin->name.'".</b></p>'.$html;
+}
+
+// Install function
+// Checks if uid is 'me' and pwd is 'you'; If so, then install the plugin
+function sample_install() {
+
+    // Install
+    if ($_POST['uid']=='me' && $_POST['pwd']=='you') {
+
+        return true;
+
+    // Loop again
+    } else {
+
+        return 1;
+    }
+}
+
+// Configure function
+// Displays the form
+function sample_configure() {
+    echo <<< EOT
+    <h3>Enter the username ('me') and password ('you') to install</h3>
+    <form action="{$_SERVER['REQUEST_URI']}" method="post">
+        uid:<input type="text" name="uid" /><br />
+        pwd:<input type="text" name="pwd" /><br />
+        <input type="submit" value="Go!" />
+    </form>
+EOT;
 }
 ?>
