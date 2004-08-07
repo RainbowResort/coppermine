97384104d0f8d27935d42e13e3af5e48ceb34327
gaugau <gaugau@62f5abee-3c11-0410-b416-987344bf19e2>
2004-08-07 09:22:20 +0000

    added romanian translation (user contribution)

--- devel/include/functions.inc.php
+++ devel/include/functions.inc.php
@@ -1678,16 +1678,16 @@ function& get_pic_url(&$pic_row, $mode,$system_pic = false)
         if (is_null($filepathname)) {
             $filepathname = $url_prefix[$pic_row['url_prefix']]. path2url($pic_row['filepath']. $pic_prefix[$mode]. $pic_row['filename']);
         }
-        
+
         $pic_row['url'] = $filepathname;
         $pic_row['mode'] = $mode;
-        
+
         if (defined('DISPLAYIMAGE_PHP') && ($mode == 'fullsize' || $mode == 'normal')) {
             $pic_row = CPGPluginAPI::filter('file_data',$pic_row);
         } elseif ($mode == 'thumb') {
             $pic_row = CPGPluginAPI::filter('thumb_data',$pic_row);
         }
-        
+
         return $pic_row['url'];
 }
 
@@ -2100,6 +2100,7 @@ $lang_language_data['malay'] = array('Malay','Bahasa Melayu','my');
 $lang_language_data['norwegian'] = array('Norwegian','Norsk','no');
 $lang_language_data['polish'] = array('Polish','Polski','pl');
 $lang_language_data['portuguese'] = array('Portuguese [Portugal]','Portugu&ecirc;s','pt');
+$lang_language_data['romanian'] = array('Romanian','Rom&acirc;n&atilde;','ro');
 $lang_language_data['russian'] = array('Russian','&#1056;&#1091;&#1089;&#1089;&#1082;&#1080;&#1081;','ru');
 $lang_language_data['slovak'] = array('Slovak','Slovensky','sl');
 $lang_language_data['slovenian'] = array('Slovenian','Slovensko','si');
--- stable/include/functions.inc.php
+++ stable/include/functions.inc.php
@@ -1622,6 +1622,7 @@ $lang_language_data['malay'] = array('Malay','Bahasa Melayu','my');
 $lang_language_data['norwegian'] = array('Norwegian','Norsk','no');
 $lang_language_data['polish'] = array('Polish','Polski','pl');
 $lang_language_data['portuguese'] = array('Portuguese [Portugal]','Portugu&ecirc;s','pt');
+$lang_language_data['romanian'] = array('Romanian','Rom&acirc;n&atilde;','ro');
 $lang_language_data['russian'] = array('Russian','&#1056;&#1091;&#1089;&#1089;&#1082;&#1080;&#1081;','ru');
 $lang_language_data['slovak'] = array('Slovak','Slovensky','sl');
 $lang_language_data['slovenian'] = array('Slovenian','Slovensko','si');
