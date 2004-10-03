8e20c3ebc2e8325e4f34713df76541fe2746c008
datajack <datajack@62f5abee-3c11-0410-b416-987344bf19e2>
2004-10-03 21:24:59 +0000

    Login page handled entirely within Coppermine now :)

--- devel/bridge/smf-api.inc.php
+++ devel/bridge/smf-api.inc.php
@@ -43,17 +43,6 @@
 // WARNING : Do not activate this bridge if you already have pictures or     //
 //           usergroups in your gallery.                                     //
 // ------------------------------------------------------------------------- //
-// In order for the login to redirect back to Coppermine, you'll need to edit
-// one SMF file.  Open up Sources/LogInOut.php and do the following:
-//
-// In function Login(), right before the closing }, add this:
-//
-//        if (isset($_SESSION['cpg_smf_login']))
-//        {
-//          $_SESSION['login_url'] = $_SESSION['old_url'];
-//          unset($_SESSION['cpg_smf_login']);
-//        }
-// ------------------------------------------------------------------------- //
 // Modify the values below according to your Board installation if you don't //
 // want to use the bridge manager integration by setting $use_bridgemgr = 0; //
 // ------------------------------------------------------------------------- //
@@ -255,10 +244,105 @@ function udb_register_page()
 // Login
 function udb_login_page()
 {
-    $_SESSION['old_url'] = $_SERVER['HTTP_REFERER'];
-    $_SESSION['cpg_smf_login'] = 1;
-    $target = 'index.php?action=login';
-    udb_redirect($target);
+	
+	global $smf_user_info, $_GET, $_POST, $lang_login_php, $raw_ip, $hdr_ip, $CONFIG;
+	
+	$referer = $_GET['referer'] ? $_GET['referer'] : 'index.php';
+	
+	if (isset($_POST['submitted'])) {
+		if (isset($_POST['remember_me'])) {
+    		$cookie_life_time = 86400 * 30;
+		} else {
+    		$cookie_life_time = 86400;
+		}	
+    	smf_setLoginCookie($cookie_life_time, addslashes($_POST['username']), addslashes($_POST['password']), 0);
+
+	   	$authenticated = smf_authenticateUser();
+
+	    if ($authenticated) {
+			smf_logOnline($smf_action);
+			smf_loadSession();
+
+	        pageheader($lang_login_php['login'], "<META http-equiv=\"refresh\" content=\"3;url=$referer\">");
+	        msg_box($lang_login_php['login'], sprintf($lang_login_php['welcome'], $smf_user_info['user_name']), $lang_continue, $referer);
+	        pagefooter();
+	        exit;
+	    } else {
+			log_write("Failed login attempt with Username: {$_POST['username']} Password: {$_POST['password']} from IP {$_SERVER['REMOTE_ADDR']} on " . localised_date(-1,$log_date_fmt),CPG_SECURITY_LOG);
+
+        $login_failed = <<<EOT
+                  <tr>
+                          <td colspan="2" align="center" class="tableh2">
+                        <font size="1" color="red"><b>{$lang_login_php['err_login']}<b></font>
+                        </td>
+                  </tr>
+
+EOT;
+
+// get IP address of the person who tried to log in, look it up on the banning table and increase the brute force counter. If the brute force counter has reached a critical limit, set a regular banning record
+$result = cpg_db_query("SELECT * FROM {$CONFIG['TABLE_BANNED']} WHERE ip_addr='$raw_ip' OR ip_addr='$hdr_ip'");
+$failed_logon_counter = mysql_fetch_array($result);
+mysql_free_result($result);
+$expiry_date = date("Y-m-d H:i:s", mktime(date('H'), date('i')+$CONFIG['login_expiry'], date('s'), date('m'), date('d'),date('Y')));
+if ($failed_logon_counter['brute_force']) {
+    $failed_logon_counter['brute_force'] = $failed_logon_counter['brute_force'] - 1;
+    $query_string = "UPDATE {$CONFIG['TABLE_BANNED']} SET brute_force='".$failed_logon_counter['brute_force']."',  expiry='".$expiry_date."' WHERE ban_id=".$failed_logon_counter['ban_id'];
+}else{
+    $failed_logon_counter['brute_force'] = $CONFIG['login_threshold'];
+    $query_string = "INSERT INTO {$CONFIG['TABLE_BANNED']} (ip_addr, expiry, brute_force) VALUES ('$raw_ip', '$expiry_date','".$failed_logon_counter['brute_force']."')";
+}
+
+//write the logon counter to the database
+cpg_db_query($query_string);
+    }
+}
+
+if (!isset($_COOKIE[$CONFIG['cookie_name'] . '_data'])) {
+    $cookie_warning = <<<EOT
+                  <tr>
+                          <td colspan="2" align="center" class="tableh2">
+                        <font size="1" color="red"><b>{$lang_login_php['cookie_warning']}<b></font>
+                        </td>
+                  </tr>
+
+EOT;
+}
+
+pageheader($lang_login_php['login']);
+$referer = urlencode($referer);
+starttable('-1', $lang_login_php['enter_login_pswd'], 2);
+echo <<< EOT
+                  $login_failed
+                  $cookie_warning
+                  <tr>
+                    <form action="login.php?referer=$referer" method="post" name="loginbox">
+                        <td class="tableb" width="40%">{$lang_login_php['username']}</td>
+                        <td class="tableb" width="60%"><input type="text" class="textinput" name="username" style="width: 100%" /></td>
+                        <script language="javascript" type="text/javascript">
+                        <!--
+                        document.loginbox.username.focus();
+                        -->
+                        </script>
+                  </tr>
+                  <tr>
+                          <td class="tableb">{$lang_login_php['password']}</td>
+                        <td class="tableb"><input type="password" class="textinput" name="password" style="width: 100%" /></td>
+                  </tr>
+                  <tr>
+                    <td colspan="2" align="center" class="tableb">{$lang_login_php['remember_me']} <input name="remember_me" type="checkbox" class="checkbox" value="1"></td>
+                  </tr>
+                  <tr>
+                    <td align="center" class="tablef"><a href="forgot_passwd.php" class="topmenu">{$lang_login_php['forgot_password_link']}</a></td>
+                    <td align="left" class="tablef"><input name="submitted" type="submit" class="button" value="{$lang_login_php['login']}"></td>
+                    </form>
+                  </tr>
+
+EOT;
+
+endtable();
+pagefooter();
+ob_end_flush();
+exit;
 }
 
 // Logout
