6a9a2d63e485097702eea95347866199906239df
kegobeer <kegobeer@62f5abee-3c11-0410-b416-987344bf19e2>
2004-10-24 22:28:43 +0000

    Fixed numerous parse errors: replaced {$CONFIG['fullpath']} with '.$CONFIG['fullpath'].'

--- devel/upload.php
+++ devel/upload.php
@@ -865,7 +865,7 @@ $max_file_size = $CONFIG['max_upl_size'] << 10;
 if (!isset($_REQUEST['control'])) {
 
     // Do some cleanup in the edit directory.
-    spring_cleaning('./{$CONFIG['fullpath']}edit',CPG_HOUR);
+    spring_cleaning('./'.$CONFIG['fullpath'].'edit',CPG_HOUR);
 
     // Do some cleaning in the temp data table.
     clean_table();
@@ -1198,7 +1198,7 @@ if ((isset($_POST['control'])) and ($_POST['control'] == 'phase_1')) {
                 $seed = substr(md5(uniqid("")), 0, 8);
 
                 // Assemble the file path.
-                $path_to_image = './{$CONFIG['fullpath']}edit/'. $prefix . $seed . '.' . $suffix;
+                $path_to_image = './'.$CONFIG['fullpath'].'edit/'. $prefix . $seed . '.' . $suffix;
 
             } while (file_exists($path_to_image));
 
@@ -1636,7 +1636,7 @@ if ((isset($_POST['control'])) and ($_POST['control'] == 'phase_1')) {
                 $seed = substr(md5(uniqid("")), 0, 8);
 
                 // Assemble the file path.
-                $path_to_image = './{$CONFIG['fullpath']}edit/'. $prefix . $seed . '.' . $suffix;
+                $path_to_image = './'.$CONFIG['fullpath'].'edit/'. $prefix . $seed . '.' . $suffix;
 
             } while (file_exists($path_to_image));
 
@@ -2249,7 +2249,7 @@ if ((isset($_POST['control'])) and ($_POST['control'] == 'phase_2')) {
         $uploaded_pic = $dest_dir . $picture_name;
 
         // Form path to temporary image.
-        $path_to_image = './{$CONFIG['fullpath']}edit/'.$file_set[1];
+        $path_to_image = './'.$CONFIG['fullpath'].'edit/'.$file_set[1];
 
         // Move the picture into its final location
         if (rename($path_to_image, $uploaded_pic)) {
@@ -2398,7 +2398,7 @@ if ((isset($_POST['control'])) and ($_POST['control'] == 'phase_2')) {
     // Create preview image.
 
     // Create path to image.
-    $path_to_image = './{$CONFIG['fullpath']}edit/'.$file_set[1];
+    $path_to_image = './'.$CONFIG['fullpath'].'edit/'.$file_set[1];
 
     // Create the preview function.
 
@@ -2421,7 +2421,7 @@ if ((isset($_POST['control'])) and ($_POST['control'] == 'phase_2')) {
             $seed = substr(md5(uniqid("")), 0, 8);
 
             // Assemble the file path.
-            $path_to_preview = './{$CONFIG['fullpath']}edit/preview_' . $seed . '.' . $extension;
+            $path_to_preview = './'.$CONFIG['fullpath'].'edit/preview_' . $seed . '.' . $extension;
 
         } while (file_exists($path_to_preview));
 
