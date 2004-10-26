f09bbb35900469bf50bf1a314d62aa9bc326e6d2
gaugau <gaugau@62f5abee-3c11-0410-b416-987344bf19e2>
2004-10-26 06:26:36 +0000

    fixed group creation error (user contrib)

--- devel/groupmgr.php
+++ devel/groupmgr.php
@@ -48,10 +48,14 @@ function display_group_list()
 
     $result = cpg_db_query("SELECT * FROM {$CONFIG['TABLE_USERGROUPS']} WHERE 1 ORDER BY group_id");
     if (!mysql_num_rows($result)) {
-        cpg_db_query("INSERT INTO {$CONFIG['TABLE_USERGROUPS']} VALUES (1, 'Administrators', 0, 1, 1, 1, 1, 1, 1, 3, 0, 5, 3)");
-        cpg_db_query("INSERT INTO {$CONFIG['TABLE_USERGROUPS']} VALUES (2, 'Registered', 1024, 0, 1, 1, 1, 1, 1, 3, 0, 5, 3)");
-        cpg_db_query("INSERT INTO {$CONFIG['TABLE_USERGROUPS']} VALUES (3, 'Anonymous', 0, 0, 0, 0, 1, 0, 0, 0, 0, 5, 3)");
-        cpg_db_query("INSERT INTO {$CONFIG['TABLE_USERGROUPS']} VALUES (4, 'Banned', 0, 0, 0, 0, 0, 0, 0, 0, 0, 5, 3);");
+        db_query("INSERT INTO {$CONFIG['TABLE_USERGROUPS']}
+        VALUES (1, 'Administrators', 0, 1, 1, 1, 1, 1, 1, 0, 0, 3, 0, 5, 3)");
+        db_query("INSERT INTO {$CONFIG['TABLE_USERGROUPS']}
+        VALUES (2, 'Registered', 1024, 0, 1, 1, 1, 1, 1, 1, 0, 3, 0, 5, 3)");
+        db_query("INSERT INTO {$CONFIG['TABLE_USERGROUPS']}
+        VALUES (3, 'Anonymous', 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 5, 3)");
+        db_query("INSERT INTO {$CONFIG['TABLE_USERGROUPS']}
+        VALUES (4, 'Banned', 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 5, 3)");
         cpg_die(CRITICAL_ERROR, 'Group table was empty !<br /><br />Default groups created, please reload this page', __FILE__, __LINE__);
     }
 
