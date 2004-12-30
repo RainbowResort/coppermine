3e3b7e44a738b60ee580f24d424ae0df2f45be36
donnoman <donnoman@62f5abee-3c11-0410-b416-987344bf19e2>
2004-12-30 05:50:37 +0000

    Rebuilt from the 1.32 theme SPLIT {MAIN_MENU} -> {SYS_MENU} & {SUB_MENU}, added {VANITY} for Powered BY PHP & MySQL, XHTML, CSS Validation Rebuilt from the 1.32 theme, shaded report.gif orange to match other navbar graphics.

--- devel/themes/fruity/theme.php
+++ devel/themes/fruity/theme.php
@@ -1,6 +1,6 @@
 <?php
 // ------------------------------------------------------------------------- //
-// Coppermine Photo Gallery 1.4.1                                            //
+// Coppermine Photo Gallery 1.3.2                                            //
 // ------------------------------------------------------------------------- //
 // Copyright (C) 2002-2004 Gregory DEMAR                                     //
 // http://www.chezgreg.net/coppermine/                                       //
@@ -14,21 +14,27 @@
 // the Free Software Foundation; either version 2 of the License, or         //
 // (at your option) any later version.                                       //
 // ------------------------------------------------------------------------- //
-// $Id$
+// CVS version: $Id$
 // ------------------------------------------------------------------------- //
-// This theme has had redundant CORE items removed                           //
+// The theme "Fruity" has been done by GauGau (http://gaugau.de/) based on   //
+// the framed template of studicasa.nl (their website has gone down, so I    //
+// guess no one will care). The usage of this theme is free for personal     //
+// use, not for commercial use (according to the disclaimer of studiocasa)!  //
 // ------------------------------------------------------------------------- //
-
 define('THEME_HAS_RATING_GRAPHICS', 1);
 define('THEME_HAS_NAVBAR_GRAPHICS', 1);
+define('THEME_IS_XHTML10_TRANSITIONAL',1);  // Remove this if you edit this template until
+                                            // you have validated it. See docs/theme.htm.
 
+// HTML template for main menu
+$template_sys_menu = <<<EOT
+                <div class="topmenu">
+                     {BUTTONS}
+                </div>
+EOT;
 
-// HTML template for main menu spacer
-$template_main_menu_spacer = '';
-
-
-
-
+// HTML template for template sys_menu spacer
+$template_sys_menu_spacer ='';
 
 
 ?>
