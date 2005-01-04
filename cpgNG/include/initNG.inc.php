a850309458fb78ebdcee55b528fe558aabc75964
abbas-ali <abbas-ali@62f5abee-3c11-0410-b416-987344bf19e2>
2005-01-04 08:40:51 +0000

    added initializing script for cpgNG

---
+++ cpgNG/include/initNG.inc.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * initNG.inc.php
+ *
+ * Initializing script for cpgNG 
+ *
+ * @package cpgNG
+ * @author Abbas <abbas@sanisoft.com>
+ * @version $Id$
+ */
+ 
+require("classes/cpgDbMysql.class.php");
+
+$db = cpgDbMysql::getInstance();
+?>
