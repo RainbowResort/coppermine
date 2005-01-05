098059a6d47e46ebda40fc30c62d61d2025d06b0
adityamooley <adityamooley@62f5abee-3c11-0410-b416-987344bf19e2>
2005-01-05 10:31:45 +0000

    added code to honour the display of index page config sequence

--- cpgNG/indexNew.php
+++ cpgNG/indexNew.php
@@ -52,56 +52,13 @@ if (isset($_GET['cat'])) {
   $cat = 0;
 }
 $breadcrumbHTML = "";
+$CONTENT = "";
 $cpg_show_private_album = $CONFIG['allow_private_albums'] ? $CONFIG['show_private'] : true;
 
 $albumData = new cpgAlbumData();
 $indexData = new cpgIndexData($cat, $PAGE);
 
 $t = new cpgTemplate();
-
-$elements = preg_split("|/|", $CONFIG['main_page_layout'], -1, PREG_SPLIT_NO_EMPTY);
-
-/**
-* Loop through the $elements array to build the page using the parameters
-* set in the config
-*/
-
-foreach ($elements as $element) {
-  if (preg_match("/(\w+),*(\d+)*/", $element, $matches)){
-    if (!isset($matches[2])) { // added to fix notice about undefined index
-        $matches[2] = 0;
-    }
-    switch ($matches[1]) {
-      case 'breadcrumb':
-          if (!empty($cat)) {
-            $breadcrumb = $albumData->getBreadcrumbData("", "", $cat);
-            $breadcrumbHTML = $t->getBreadcrumbHTML($breadcrumb);
-          }
-          // Added breadcrumb as a separate listable block from config
-          //if (($breadcrumb != '' || count($cat_data) > 0) && $cat != 0) theme_display_breadcrumb($breadcrumb, $cat_data);
-          break;
-
-      case 'catlist':
-          /**
-           * Array to store the category data. Breadcrumb, category data & statistics.
-           */
-          $indexData->getCatList();
-          if (isset($cat) && $cat == USER_GAL_CAT) {
-            $indexData->listUsers();
-          }
-          flush();
-          break;
-
-      case 'alblist':
-          $indexData->listAlbums();
-          flush();
-          break;
-
-      default:
-        break;
-    }
-  }
-}
 
 /**#@+
  * Assign all the data to smarty
@@ -128,7 +85,6 @@ $t->assign("USER_CAN_UPLOAD_PICTURES", USER_CAN_UPLOAD_PICTURES);
 $t->assign("REFERER", $REFERER);
 $t->assign("USER_NAME", USER_NAME);
 $t->assign("my_cat_id", FIRST_USER_CAT + USER_ID);
-$t->assign("breadcrumbHTML", $breadcrumbHTML);
 $t->assign("PAGE_TITLE", $lang_index_php['welcome']);
 $t->assign("GALLERY_DESCRIPTION", $CONFIG["gallery_name"]);
 $t->assign("USER_NAME", USER_NAME);
@@ -141,10 +97,54 @@ if (!USER_ID) {
 }
 /**#@-*/
 
+$elements = preg_split("|/|", $CONFIG['main_page_layout'], -1, PREG_SPLIT_NO_EMPTY);
+
 /**
- * Fetch the index page
+* Loop through the $elements array to build the page using the parameters
+* set in the config
+*/
+
+foreach ($elements as $element) {
+  if (preg_match("/(\w+),*(\d+)*/", $element, $matches)) {
+    if (!isset($matches[2])) { // added to fix notice about undefined index
+        $matches[2] = 0;
+    }
+    switch ($matches[1]) {
+      case 'breadcrumb':
+          if (!empty($cat)) {
+            $breadcrumb = $albumData->getBreadcrumbData("", "", $cat);
+            $breadcrumbHTML = $t->getBreadcrumbHTML($breadcrumb);
+          }
+          break;
+      case 'catlist':
+          /**
+           * Array to store the category data. Breadcrumb, category data & statistics.
+           */
+          $indexData->getCatList();
+          $t->assign("indexData", $indexData);
+          $CONTENT .= $t->fetchHTML("common/category.html");
+          if (isset($cat) && $cat == USER_GAL_CAT) {
+            $indexData->listUsers();
+            $t->assign("indexData", $indexData);
+            $CONTENT .= $t->fetchHTML("common/users.html");
+          }
+          break;
+      case 'alblist':
+          $indexData->listAlbums();
+          $t->assign("indexData", $indexData);
+          $CONTENT .= $t->fetchHTML("common/albums.html");
+          break;
+      default:
+        break;
+    }
+  }
+}
+
+/**
+ * Assign data for main.html
  */
-$t->assign("CONTENT", $t->getIndexHTML($indexData));
+$t->assign("CONTENT", $CONTENT);
+$t->assign("breadcrumbHTML", $breadcrumbHTML);
 
 $t->display ("main.html");
 ?>
