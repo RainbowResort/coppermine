59f2da629f888f3207eae56fa8f116b2d25e9b1d
oddeveloper <oddeveloper@62f5abee-3c11-0410-b416-987344bf19e2>
2005-02-02 23:32:45 +0000

    Added {pseudo} session functionality

--- devel/bridge/coppermine.inc.php
+++ devel/bridge/coppermine.inc.php
@@ -42,6 +42,9 @@ class cpg_udb extends core_udb {
 			$this->use_post_based_groups = @$BRIDGE['use_post_based_groups'];
 		}
 		
+		// A hash that's a little specific to the client's configuration
+		$this->client_id = md5($_SERVER['USER_AGENT'].$_SERVER['HTTP_PROTOCOL']);
+
 		$this->multigroups = 0;
 
 		$this->group_overrride = !$this->use_post_based_groups;
@@ -59,11 +62,13 @@ class cpg_udb extends core_udb {
 		$this->table = array(
 			'users' => 'users',
 			'groups' => 'usergroups',
+			'sessions' => 'sessions'
 		);
 
 		// Derived full table names
 		$this->usertable = '`' . $this->db['name'] . '`.' . $this->db['prefix'] . $this->table['users'];
 		$this->groupstable =  '`' . $this->db['name'] . '`.' . $this->db['prefix'] . $this->table['groups'];
+		$this->sessionstable =  '`' . $this->db['name'] . '`.' . $this->db['prefix'] . $this->table['sessions'];
 
 		// Table field names
 		$this->field = array(
@@ -96,38 +101,51 @@ class cpg_udb extends core_udb {
 		$this->connect($CONFIG['LINK_ID']);
 	}
 	
+
+	// Login function
 	function login( $username = null, $password = null, $remember = false ) {
 		global $CONFIG;
-		
+
+        // Create the session_id from concat(cookievalue,client_id)
+		$session_id = $this->session_id.$this->client_id;
+
+        // Check if encrypted passwords are enabled
 		if ($CONFIG['enable_encrypted_passwords']) {
 			$encpassword = md5($password);
 		} else {
 			$encpassword = $password;
 		}
-		
+
+        // Check for user in users table
 		$sql =  "SELECT user_id, user_name, user_password FROM {$this->usertable} WHERE ";
 		$sql .= "user_name = '$username' AND BINARY user_password = '$encpassword' AND user_active = 'YES'";
-
 		$results = cpg_db_query($sql);
-		
-		if ($results) {
-			$sql =  "UPDATE {$this->usertable} SET user_lastvisit = NOW() ";
-			$sql .= "WHERE user_name = '$username' AND BINARY user_password = '$encpassword' AND user_active = 'YES'";
-			
-			$lastvisit = cpg_db_query($sql);
 
-			$USER_DATA = mysql_fetch_array($results);
+        // If exists update lastvisit value, session, and login
+		if ($results) {
+			
+            // Update lastvisit value
+            $sql =  "UPDATE {$this->usertable} SET user_lastvisit = NOW() ";
+			$sql .= "WHERE user_name = '$username' AND BINARY user_password = '$encpassword' AND user_active = 'YES'";
+			cpg_db_query($sql, $this->link_id);
+
+			$USER_DATA = mysql_fetch_assoc($results);
 			mysql_free_result($results);
 
+            // If this is a 'remember me' login set the remember field to true
 			if ($remember) {
-				$cookie_life_time = 86400 * 30;
+      	        $remember_sql = ",remember = true ";
 			} else {
-				$cookie_life_time = 86400;
+			    $remember_sql = '';
 			}
 	
-			setcookie($CONFIG['cookie_name'] . '_uid', $USER_DATA['user_id'], time() + $cookie_life_time, $CONFIG['cookie_path']);
-			setcookie($CONFIG['cookie_name'] . '_pass', md5($password), time() + $cookie_life_time, $CONFIG['cookie_path']);
-			
+            // Update guest session with user's information
+	        $sql  = "update {$this->sessionstable} set ";
+	        $sql .= "user_id={$USER_DATA['user_id']} ";
+	        $sql .= $remember_sql;
+	        $sql .= "where session_id=md5('$session_id');";
+	        cpg_db_query($sql, $this->link_id);
+
 			return $USER_DATA;
 		} else {
 
@@ -135,14 +153,16 @@ class cpg_udb extends core_udb {
 		}
 	}
 	
+
+	// Logout function
 	function logout() {
 	    global $CONFIG;
 
-        setcookie($CONFIG['cookie_name'] . '_pass', '', time()-86400, $CONFIG['cookie_path']);
-        setcookie($CONFIG['cookie_name'] . '_uid', '', time()-86400, $CONFIG['cookie_path']);
+        // Revert authenticated session to a guest session
+        $sql  = "update {$this->sessionstable} set user_id = 0, remember=false where session_id=md5('{$this->session_id}');";
     }
-	
-	
+
+
 	// Get groups of which user is member
 	function get_groups($row)
 	{
@@ -162,29 +182,12 @@ class cpg_udb extends core_udb {
 		return $data;
 	}
 
-	// definition of how to extract an id and password hash from a cookie
-	function cookie_extraction()
-	{
-        global $CONFIG;
 
-		// Default anonymous values
-		$id = 0;
-		$pass = '';
-
-        // Retrieve cookie stored login information
-        if (isset($_COOKIE[$CONFIG['cookie_name'] . '_uid']) && isset($_COOKIE[$CONFIG['cookie_name'] . '_pass'])) {
-            $id = (int)$_COOKIE[$CONFIG['cookie_name'] . '_uid'];
-            $pass = substr(addslashes($_COOKIE[$CONFIG['cookie_name'] . '_pass']), 0, 32);
-        }
-
-		return ($id) ? array($id, $pass) : false;
-	}
-	
 	// definition of actions required to convert a password from user database form to cookie form
 	function udb_hash_db($password)
 	{
 		global $CONFIG;
-		
+
 		if ($CONFIG['enable_encrypted_passwords']) {
 			return $password;
 		} else {
@@ -192,16 +195,137 @@ class cpg_udb extends core_udb {
 		}
 	}
 
+
+	// definition of how to extract id, name, group from a session cookie
+	function session_extraction()
+	{
+	    global $CONFIG;
+
+		// Default anonymous values
+		$id = 0;
+		$pass = '';
+
+        // Get the session cookie value
+		$sessioncookie = $_COOKIE[$this->client_id];
+		
+		// Create the session id by concat(session_cookie_value, client_id)
+		$session_id = $sessioncookie.$this->client_id;
+
+        // Lifetime of 'remember me' session is 2 weeks
+        $rememberme_life_time = time()-(CPG_WEEK*2);
+        
+        // Lifetime of normal session is 1 hour
+        $session_life_time = time()-CPG_HOUR;
+
+        // Delete old sessions
+        $sql = "delete from {$this->sessionstable} where time<$session_life_time and remember=false;";
+        cpg_db_query($sql, $this->link_id);
+
+        // Delete stale 'remember me' sessions
+        $sql = "delete from {$this->sessionstable} where time<$rememberme_life_time;";
+        cpg_db_query($sql, $this->link_id);
+
+        // Check for valid session if session_cookie_value exists
+        if ($sessioncookie) {
+
+            // Check for valid session
+            $sql =  'select user_id from '.$this->sessionstable.' where session_id=md5("'.$session_id.'");';
+            $result = cpg_db_query($sql);
+
+            // If session exists...
+            if ($result) {
+                $row = mysql_fetch_assoc($result);
+                mysql_free_result($result);
+
+                $row['user_id'] = (int) $row['user_id'];
+
+                // Check if there's a user for this session
+                $sql =  'select user_id as id, user_password as password ';
+                $sql .= 'from '.$this->usertable.' ';    
+                $sql .= 'where user_id='.$row['user_id'];
+                $result = cpg_db_query($sql, $this->link_id);
+
+                // If user exists, use the current session
+                if ($result) {
+                    $row = mysql_fetch_assoc($result);
+                    mysql_free_result($result);
+
+                    $pass = $row['password'];
+                    $id = (int) $row['id'];
+                    $this->session_id = $sessioncookie;
+
+                // If the user doesn't exist, use default guest credentials
+                }
+
+            // If not a valid session exists, create a new session
+            } else {
+
+                $this->create_session();
+            }
+        
+        // No session exists; create one
+        } else {
+            $this->create_session();
+        }
+
+		return ($id) ? array($id, $pass) : false;
+	}
+
+
+    // Function used to keep the session alive
+	function session_update()
+    {	
+        $session_id = $this->session_id.$this->client_id;
+        $sql = "update {$this->sessionstable} set time='".time()."' where session_id=md5('$session_id');";
+        cpg_db_query($sql);
+    }
+
+
+	// Create a new session with the cookie lifetime set to 2 weeks
+	function create_session() {
+		// start session
+        $this->session_id = $this->generateId();
+        $session_id = $this->session_id.$this->client_id;
+
+        $sql =  'insert into '.$this->sessionstable.' (session_id, user_id, time, remember) values ';
+        $sql .= '("'.md5($session_id).'", 0, "'.time().'", false);';
+
+		// insert the guest session
+        cpg_db_query($sql, $this->link_id);
+
+		// set the session cookie
+        setcookie( $this->client_id, $this->session_id, time() + (CPG_WEEK*2), $CONFIG['cookie_path'] );
+	}
+
+
+	/** Modified function taken from Mambo session class */
+	function generateId() {
+		$failsafe = 20;
+		$randnum = 0;
+		while ($failsafe--) {
+			$randnum = md5( uniqid( microtime(), 1 ));
+            $session_id = $randnum.$this->client_id;
+			if ($randnum != "") {
+				$sql = "SELECT session_id FROM {$this->sessionstable} WHERE session_id=MD5('$session_id')";
+				if (!$result = cpg_db_query($sql, $this->link_id)) {
+					break;
+				}
+				mysql_free_result($result);
+			}
+		}
+		return $randnum;
+	}
+
+
 	/*
 	 * Overidden functions !!DO NOT REMOVE OR CPG WILL NOT WORK CORRECTLY!!
 	 */
-	// definition of how to extract id, name, group from a session cookie
-	function session_extraction($cookie_id)
-	{	}
-
-	function session_update() 
-    {	}
-
+	// definition of how to extract an id and password hash from a cookie
+	function cookie_extraction()
+	{
+		return false;
+    }
+	
 	// Register
 	function register_page()
 	{	}
