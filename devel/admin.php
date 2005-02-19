3554e0eb26e6c0c1baf2df5c646a80ddbccc8324
gaugau <gaugau@62f5abee-3c11-0410-b416-987344bf19e2>
2005-02-19 06:52:35 +0000

    fixed encryption handling: there already was a mechanism in place for config items that need to be disabled when bridging.

--- devel/admin.php
+++ devel/admin.php
@@ -26,18 +26,19 @@ require('include/sql_parse.php');
 
 // Options disabled in bridged version
 $options_to_disable = array('reg_notify_admin_email',
-                                                        'reg_requires_valid_email',
-                                                        'allow_duplicate_emails_addr',
-                                                        'allow_email_change',
-                                                        'login_threshold',
-                                                        'login_expiry',
-                                                        'user_profile1_name',
-                                                        'user_profile2_name',
-                                                        'user_profile3_name',
-                                                        'user_profile4_name',
-                                                        'user_profile5_name',
-                                                        'user_profile6_name',
-                                                        );
+    'reg_requires_valid_email',
+    'allow_duplicate_emails_addr',
+    'allow_email_change',
+    'login_threshold',
+    'login_expiry',
+    'user_profile1_name',
+    'user_profile2_name',
+    'user_profile3_name',
+    'user_profile4_name',
+    'user_profile5_name',
+    'user_profile6_name',
+    'enable_encrypted_passwords',
+);
 
 if (!GALLERY_ADMIN_MODE) cpg_die(ERROR, $lang_errors['access_denied'], __FILE__, __LINE__);
 
@@ -768,11 +769,6 @@ function create_form(&$data)
                 case 17 :
                     form_asc_desc($element[0], $element[1], $element[3]);
                     break;
-                case 18 :
-					if (!$CONFIG['enable_encrypted_passwords'] && UDB_INTEGRATION=='coppermine') {
-						form_yes_no($element[0], $element[1], $element[3]);
-					}
-					break;
                 default:
                     die('Invalid action');
             } // switch
@@ -819,9 +815,9 @@ if (count($_POST) > 0) {
                 if ($CONFIG[$element[1]] !== stripslashes($value))
                      {
                         cpg_db_query("UPDATE {$CONFIG['TABLE_CONFIG']} SET value = '$value' WHERE name = '{$element[1]}'");
-						if ($element[1]=='enable_encrypted_passwords' && $value) {
-							cpg_db_query("update {$CONFIG['TABLE_USERS']} set user_password=md5(user_password);");
-						}
+                                                if ($element[1]=='enable_encrypted_passwords' && $value) {
+                                                        cpg_db_query("update {$CONFIG['TABLE_USERS']} set user_password=md5(user_password);");
+                                                }
                         if ($CONFIG['log_mode'] == CPG_LOG_ALL) {
                                 log_write('CONFIG UPDATE SQL: '.
                                           "UPDATE {$CONFIG['TABLE_CONFIG']} SET value = '$value' WHERE name = '{$element[1]}'\n".
