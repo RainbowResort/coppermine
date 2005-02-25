a30a15946e1e5a87662d0b4cf30a2b866c9e642d
adityamooley <adityamooley@62f5abee-3c11-0410-b416-987344bf19e2>
2005-02-25 08:21:42 +0000

    removed condition for lasthit from the hit counter code as per issue 11913.0

--- devel/themes/classic/theme.php
+++ devel/themes/classic/theme.php
@@ -1837,7 +1837,7 @@ function theme_html_picture()
         $USER['liv'] = array();
     }
     // Add 1 to hit counter
-    if (!USER_IS_ADMIN && $album != "lasthits" && !in_array($pid, $USER['liv']) && isset($_COOKIE[$CONFIG['cookie_name'] . '_data'])) {
+    if (!USER_IS_ADMIN && !in_array($pid, $USER['liv']) && isset($_COOKIE[$CONFIG['cookie_name'] . '_data'])) {
         add_hit($pid);
         if (count($USER['liv']) > 4) array_shift($USER['liv']);
         array_push($USER['liv'], $pid);
