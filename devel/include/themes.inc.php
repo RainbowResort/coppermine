ffc1da059597d067945a91e147adb0b43296c0a2
donnoman <donnoman@62f5abee-3c11-0410-b416-987344bf19e2>
2005-04-10 20:31:33 +0000

    adjusted the logic for the custom_link, moved it to a template_extract in theme_main_menu

--- devel/include/themes.inc.php
+++ devel/include/themes.inc.php
@@ -147,9 +147,7 @@ if (!defined('THEME_HAS_NO_SUB_MENU_BUTTONS')) {
   // HTML template for template sub_menu buttons
   if (!isset($sub_menu_buttons)) { //{THEMES}
     // {HREF_LNK}{HREF_TITLE}{HREF_TGT}{BLOCK_ID}{SPACER}
-        if ($CONFIG['custom_lnk_url'] != '') {
-        addbutton($sub_menu_buttons,'{CUSTOM_LNK_LNK}','{CUSTOM_LNK_TITLE}','{CUSTOM_LNK_TGT}','custom_link',$template_sub_menu_spacer);
-  }
+    addbutton($sub_menu_buttons,'{CUSTOM_LNK_LNK}','{CUSTOM_LNK_TITLE}','{CUSTOM_LNK_TGT}','custom_link',$template_sub_menu_spacer);
     addbutton($sub_menu_buttons,'{ALB_LIST_LNK}','{ALB_LIST_TITLE}','{ALB_LIST_TGT}','album_list',$template_sub_menu_spacer);
     addbutton($sub_menu_buttons,'{LASTUP_LNK}','{LASTUP_TITLE}','{LASTUP_TGT}','lastup',$template_sub_menu_spacer);
     addbutton($sub_menu_buttons,'{LASTCOM_LNK}','{LASTCOM_TITLE}','{LASTCOM_TGT}','lastcom',$template_sub_menu_spacer);
@@ -1293,8 +1291,7 @@ function theme_main_menu($which)
     if (!$CONFIG['display_faq']) {
         template_extract_block($template_sys_menu, 'faq');
     }
-
-
+    
     $param = array(
         '{HOME_TGT}' => $CONFIG['home_target'],
         '{HOME_TITLE}' => $lang_main_menu['home_title'],
@@ -1333,13 +1330,18 @@ function theme_main_menu($which)
 
         $sys_menu = template_eval($template_sys_menu, $param);
   } else {
+
+    if (!$CONFIG['custom_lnk_url']) {
+        template_extract_block($template_sub_menu, 'custom_link');
+    }
+    
     $param = array(
         '{ALB_LIST_TGT}' => "index.php$cat_l",
         '{ALB_LIST_TITLE}' => $lang_main_menu['alb_list_title'],
         '{ALB_LIST_LNK}' => $lang_main_menu['alb_list_lnk'],
-                '{CUSTOM_LNK_TGT}' => $CONFIG['custom_lnk_url'],
-                '{CUSTOM_LNK_TITLE}' => $CONFIG['custom_lnk_name'],
-                '{CUSTOM_LNK_LNK}' => $CONFIG['custom_lnk_name'],
+        '{CUSTOM_LNK_TGT}' => $CONFIG['custom_lnk_url'],
+        '{CUSTOM_LNK_TITLE}' => $CONFIG['custom_lnk_name'],
+        '{CUSTOM_LNK_LNK}' => $CONFIG['custom_lnk_name'],
         '{LASTUP_TGT}' => "thumbnails.php?album=lastup$cat_l2",
         '{LASTUP_TITLE}' => $lang_main_menu['lastup_title'],
         '{LASTUP_LNK}' => $lang_main_menu['lastup_lnk'],
