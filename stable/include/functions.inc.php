0ea49f8107cfa4147711b001406c0a32f5046f2f
gaugau <gaugau@62f5abee-3c11-0410-b416-987344bf19e2>
2005-07-23 11:15:15 +0000

    added albanian language (user contrib)

--- stable/include/functions.inc.php
+++ stable/include/functions.inc.php
@@ -1595,6 +1595,7 @@ if ($CONFIG['language_flags'] == 0 && $parameter == 'flags'){
 
 // get an array of english and native language names and flags
 // for now, use a static array definition here - this could later be made into a true database query
+$lang_language_data['albanian'] = array('Albanian','Albanian','al');
 $lang_language_data['arabic'] = array('Arabic','&#1575;&#1604;&#1593;&#1585;&#1576;&#1610;&#1577;','sa');
 $lang_language_data['basque'] = array('Basque','Euskera','baq');
 $lang_language_data['bosnian'] = array('Bosnian','Bosanski','ba');
