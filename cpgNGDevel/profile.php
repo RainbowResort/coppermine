07056ea8d604565db8d745431740dadfbd63c74e
amitrb <amitrb@62f5abee-3c11-0410-b416-987344bf19e2>
2006-02-20 06:19:04 +0000

    Debugging info removed

--- cpgNGDevel/profile.php
+++ cpgNGDevel/profile.php
@@ -276,7 +276,5 @@ $t->assign("CONTENT", $t->fetchHTML("common/profile.html"));
 include ('include/cleanUp.inc.php');
 
 $t->display ('main.html');
-$time_end = cpgGetMicroTime();
-$time = round($time_end - $cpg_time_start, 3);
-echo "TIME: ". $time;
+
 ?>
