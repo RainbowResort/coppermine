d157f3b3a9debde8b80202b519525a8179559c0e
gaugau <gaugau@62f5abee-3c11-0410-b416-987344bf19e2>
2006-04-09 10:35:15 +0000

    Re-added the file that redirects users to the start page to make sure that users who are updating get rid of server_relocate.php rev1.2

---
+++ stable/relocate_server.php
@@ -0,0 +1,34 @@
+<?php
+/*************************
+  Coppermine Photo Gallery
+  ************************
+  Copyright (c) 2003-2006 Coppermine Dev Team
+  v1.1 originally written by Gregory DEMAR
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+  ********************************************
+  Coppermine version: 1.4.5
+  $Source$
+  $Revision$
+  $Author$
+  $Date$
+**********************************************/
+
+define('IN_COPPERMINE', true);
+require('include/init.inc.php');
+if (!GALLERY_ADMIN_MODE) cpg_die(ERROR, $lang_errors['access_denied'], __FILE__, __LINE__);
+
+
+$redirect = "index.php";
+$message = <<< EOT
+You are trying to access a file that has been removed from the coppermine package for security reasons (details can be read on the Coppermine support board announcement thread "<a href="http://coppermine-gallery.net/forum/index.php?topic=24217.0" rel="external" class="external">Remove relocate_server.php file from your website</a>").<br />
+You are now being redirected to your gallery's start page.
+EOT;
+pageheader($lang_info, "<meta http-equiv=\"refresh\" content=\"120;url=$redirect\" />");
+msg_box($lang_info, $message, $lang_continue, $redirect);
+pagefooter();
+ob_end_flush();
+?>
