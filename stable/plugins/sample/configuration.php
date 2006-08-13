86c9f34664deb92813ddf62b49653320208b09cf
donnoman <donnoman@62f5abee-3c11-0410-b416-987344bf19e2>
2006-08-13 18:23:33 +0000

    2006-08-12 [D] Added $extra_info and $install_info implementations to the sample plugin {Donnoman} 2006-08-12 [C] Changed pluginmgr.php to display $extra_info and $install_info clearly with its associated plugin {Donnoman} 2006-08-12 [B] Fixed $extra_info and $install_info repeating for unrelated plugins {Donnoman}

--- stable/plugins/sample/configuration.php
+++ stable/plugins/sample/configuration.php
@@ -21,4 +21,28 @@ $name='Sample Plugin';
 $description='This is a sample plugin.';
 $author='Coppermine Development Team';
 $version='1.0';
+/*
+ * $extra_info is displayed with the title of a plugin that is NOT installed and
+ * can be used to present extra information.  In this case I show a complex
+ * example that forms a button the user can click.
+ */
+$extra_info = <<<EOT
+    <table border="0" cellspacing="0" cellpadding="0"> 
+    <tr>
+        <td class="admin_menu"><a target="_blank" href="docs/index.htm#plugin" title="Plugin Documentation">Documentation</a></td>
+    </tr>
+    </table>
+EOT;
+/*
+ * $install_info is displayed with the title of a plugin that IS installed and
+ * can be used to present extra information. In this case I show a complex
+ * example that forms a button the user can click.
+ */
+$install_info=<<<EOT
+    <table border="0" cellspacing="0" cellpadding="0"> 
+    <tr>
+        <td class="admin_menu"><a target="_blank" href="http://coppermine-gallery.net/forum/index.php?board=53.0" title="Plugin Support">Support</a></td>
+    </tr>
+    </table>
+EOT;
 ?>
