903d30df5fecac08f3ab24bc3762bf1a7caf0e2b
gaugau <gaugau@62f5abee-3c11-0410-b416-987344bf19e2>
2006-12-09 09:25:45 +0000

    Optional transparent overlay against image theft

--- devel/themes/sample/theme.php
+++ devel/themes/sample/theme.php
@@ -1466,7 +1466,7 @@ obfuscated to make it hard for non-coders to remove the tag. The reason behind
 this was an additional license add-on that disallowed users to change the line.
 The dev team has reviewed this policy for cpg1.5.x and decided that end users
 ARE allowed to change or remove the line in legal terms.
-HOWEVER: We have a forum policy for the support board instead: support will 
+HOWEVER: We have a forum policy for the support board instead: support will
 only be given for galleries that show the "Powered by Coppermine" tag
 unobfuscated.
 Before removing the credits, please consider this as well:
@@ -2949,22 +2949,48 @@ function theme_display_fullsize_pic()
   <meta http-equiv="content-type" content="text/html; charset=<?php echo $CONFIG['charset'] == 'language file' ? $lang_charset : $CONFIG['charset'] ?>" />
   <title><?php echo $CONFIG['gallery_name'] ?>: <?php echo $lang_fullsize_popup['click_to_close'];
       ?></title>
-  <script type="text/javascript" src="scripts.js"></script>
   <style type="text/css">
-  body { margin: 0; padding: 0; background-color: gray; }
-  img { margin:0; padding:0; border:0; }
-  #content { margin:0 auto; padding:0; border:0; }
-  table { border:0; height:100%; width:100%; border-collapse:collapse}
-  td {         vertical-align: middle; text-align:center; }
+    body { margin: 0; padding: 0; background-color: gray; }
+    img { margin:0; padding:0; border:0; }
+    #content { margin:0 auto; padding:0; border:0; }
+    table { border:0; width:<?php echo $row['pwidth'] ?>px; height:<?php echo $row['pheight'] ?>px; border-collapse:collapse}
+    td {         vertical-align: middle; text-align:center; }
   </style>
+  <script type="text/javascript" src="scripts.js"></script>
   </head>
-  <body>
+  <body style="margin:0px; padding:0px; background-color: gray;">
     <script language="JavaScript" type="text/JavaScript">
       adjust_popup();
     </script>
-    <table>
+<?php
+if ($CONFIG['transparent_overlay'] == 1) {
+?>
+    <table cellpadding="0" cellspacing="0" align="center" style="padding:0px;">
       <tr>
-            <td>
+              <?php
+        echo '<td align="center" valign="middle" background="' . htmlspecialchars($imagedata['path']) . '" ' . $imagedata['geometry'] . ' class="image">';
+        echo '<div id="content">';
+        echo  '<a href="javascript: window.close()" style="border:none"><img src="images/image.gif?id='
+                . floor(rand()*1000+rand())
+                . '&amp;fullsize=yes" '
+                . $imagedata['geometry']
+                . ' alt="'
+                . htmlspecialchars($imagedata['name'])
+                . '" title="'
+                . htmlspecialchars($imagedata['name'])
+                . "\n" . $lang_fullsize_popup['click_to_close']
+                . '" /></a><br />' ."\n";
+               ?>
+          </div>
+        </td>
+      </tr>
+    </table>
+<?php
+} else {
+?>
+    <table class="fullsize">
+      <tr>
+        <td>
           <div id="content">
               <?php     echo  '<a href="javascript: window.close()"><img src="'
                 . htmlspecialchars($imagedata['path']) . '" '
@@ -2980,6 +3006,9 @@ function theme_display_fullsize_pic()
         </td>
       </tr>
     </table>
+<?php
+}
+?>
   </body>
 </html>
 <?php
