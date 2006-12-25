bd8cc9cbbee408bc70894690f3af2e901f865c15
tarique <tarique@62f5abee-3c11-0410-b416-987344bf19e2>
2006-12-25 06:18:08 +0000

    Sanitising GPC at the begining and putting into $CLEAN

--- devel/addHit.php
+++ devel/addHit.php
@@ -21,14 +21,18 @@ define('IN_COPPERMINE', true);
 
 require('include/init.inc.php');
 
-if (isset($_GET['pid']) && !GALLERY_ADMIN_MODE && $CONFIG['slideshow_hits'] != 0) {
+/**
+ * Clean up GPC and other Globals here
+ */
+ $CLEAN['pid'] = (int)$_GET['pid'];
+
+if ($CLEAN['pid'] && !GALLERY_ADMIN_MODE && $CONFIG['slideshow_hits'] != 0) {
   // Add 1 to hit counter
-  $pid = (int)$_GET['pid'];
-  if (!in_array($pid, $USER['liv']) && isset($HTTP_COOKIE_VARS[$CONFIG['cookie_name'] . '_data'])) {
-      add_hit($pid);
-      if (count($USER['liv']) > 4) array_shift($USER['liv']);
-      array_push($USER['liv'], $pid);
-      user_save_profile();
+  if (!in_array($CLEAN['pid'], $USER['liv']) && isset($HTTP_COOKIE_VARS[$CONFIG['cookie_name'] . '_data'])) {
+	  add_hit($pid);
+	  if (count($USER['liv']) > 4) array_shift($USER['liv']);
+	  array_push($USER['liv'], $pid);
+	  user_save_profile();
   }
 }
 ?>
