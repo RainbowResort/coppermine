fda211388481c1bf37945561e0bd0c639d5cc325
gaugau <gaugau@62f5abee-3c11-0410-b416-987344bf19e2>
2007-04-12 07:22:43 +0000

    Added title tag Moved navigation images into pics folder

--- cpg1.5.x/docs/script.js
+++ cpg1.5.x/docs/script.js
@@ -70,20 +70,20 @@ function dTree(objName) {
                 inOrder                                        : true
         }
         this.icon = {
-                root                                : 'img/base.gif',
-                folder                        : 'img/folder.gif',
-                folderOpen        : 'img/folderopen.gif',
-                node                                : 'img/page.gif',
-                empty                                : 'img/empty.gif',
-                line                                : 'img/line.gif',
-                join                                : 'img/join.gif',
-                joinBottom        : 'img/joinbottom.gif',
-                plus                                : 'img/plus.gif',
-                plusBottom        : 'img/plusbottom.gif',
-                minus                                : 'img/minus.gif',
-                minusBottom        : 'img/minusbottom.gif',
-                nlPlus                        : 'img/nolines_plus.gif',
-                nlMinus                        : 'img/nolines_minus.gif'
+                root                                : 'pics/base.gif',
+                folder                        : 'pics/folder.gif',
+                folderOpen        : 'pics/folderopen.gif',
+                node                                : 'pics/page.gif',
+                empty                                : 'pics/empty.gif',
+                line                                : 'pics/line.gif',
+                join                                : 'pics/join.gif',
+                joinBottom        : 'pics/joinbottom.gif',
+                plus                                : 'pics/plus.gif',
+                plusBottom        : 'pics/plusbottom.gif',
+                minus                                : 'pics/minus.gif',
+                minusBottom        : 'pics/minusbottom.gif',
+                nlPlus                        : 'pics/nolines_plus.gif',
+                nlMinus                        : 'pics/nolines_minus.gif'
         };
         this.obj = objName;
         this.aNodes = [];
