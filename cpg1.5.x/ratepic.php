ba16a99dbff485b7fa745a7a25f86047cdb631e2
gaugau <gaugau@62f5abee-3c11-0410-b416-987344bf19e2>
2007-06-06 07:43:40 +0000

    Added rating confirmation on page

--- cpg1.5.x/ratepic.php
+++ cpg1.5.x/ratepic.php
@@ -61,12 +61,30 @@ $result = cpg_db_query($sql);
 $user_md5_id = USER_ID ? md5(USER_ID) : $USER['ID'];
 $sql = "SELECT * " . "FROM {$CONFIG['TABLE_VOTES']} " . "WHERE pic_id = '$pic' AND user_md5_id = '$user_md5_id'";
 $result = cpg_db_query($sql);
-if (mysql_num_rows($result)) cpg_die(ERROR, $lang_rate_pic_php['already_rated'], __FILE__, __LINE__);
+if (mysql_num_rows($result)) { // user has already rated this file
+    $location = "displayimage.php?pid=" . ($pic).'&message_id='.cpgStoreTempMessage($lang_rate_pic_php['already_rated']).'#cpgMessageBlock';
+    $header_location = (@preg_match('/Microsoft|WebSTAR|Xitami/', getenv('SERVER_SOFTWARE'))) ? 'Refresh: 0; URL=' : 'Location: ';
+    header($header_location . $location);
+    pageheader($lang_common['information'], "<META http-equiv=\"refresh\" content=\"1;url=$location\">");
+    msg_box($lang_common['information'], $lang_rate_pic_php['already_rated'], $lang_common['continue'], $location);
+    pagefooter();
+    ob_end_flush();
+    // cpg_die(ERROR, $lang_rate_pic_php['already_rated'], __FILE__, __LINE__); // commented out in favor of message-block
+}
 //Test for Self-Rating
 $user=USER_ID;
 $owner=$row['owner_id'];
 
-if (!empty($user) && $user==$owner && !USER_IS_ADMIN) cpg_die(ERROR, $lang_rate_pic_php['forbidden'], __FILE__, __LINE__);
+if (!empty($user) && $user==$owner && !USER_IS_ADMIN) {
+    $location = "displayimage.php?pid=" . ($pic).'&message_id='.cpgStoreTempMessage($lang_rate_pic_php['forbidden']).'#cpgMessageBlock';
+    $header_location = (@preg_match('/Microsoft|WebSTAR|Xitami/', getenv('SERVER_SOFTWARE'))) ? 'Refresh: 0; URL=' : 'Location: ';
+    header($header_location . $location);
+    pageheader($lang_common['information'], "<META http-equiv=\"refresh\" content=\"1;url=$location\">");
+    msg_box($lang_common['information'], $lang_rate_pic_php['forbidden'], $lang_common['continue'], $location);
+    pagefooter();
+    ob_end_flush();
+    // cpg_die(ERROR, $lang_rate_pic_php['forbidden'], __FILE__, __LINE__); // commented out in favor of message-block
+}
 // Update picture rating
 $new_rating = round(($row['votes'] * $row['pic_rating'] + $rate * 2000) / ($row['votes'] + 1));
 $sql = "UPDATE {$CONFIG['TABLE_PICTURES']} " . "SET pic_rating = '$new_rating', votes = votes + 1 " . "WHERE pid = '$pic' LIMIT 1";
@@ -107,7 +125,7 @@ if ($CONFIG['vote_details']) {
         cpg_db_query($query);
 }
 
-$location = "displayimage.php?pid=" . ($pic);
+$location = "displayimage.php?pid=" . ($pic).'&message_id='.cpgStoreTempMessage($lang_rate_pic_php['rate_ok']).'#cpgMessageBlock';
 $header_location = (@preg_match('/Microsoft|WebSTAR|Xitami/', getenv('SERVER_SOFTWARE'))) ? 'Refresh: 0; URL=' : 'Location: ';
 header($header_location . $location);
 pageheader($lang_common['information'], "<META http-equiv=\"refresh\" content=\"1;url=$location\">");
