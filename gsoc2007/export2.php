d36cb07f203ced1e57c4823ee287d30228dd6913
drhammond <drhammond@62f5abee-3c11-0410-b416-987344bf19e2>
2007-08-14 06:58:49 +0000

    Single Page HTML Export, creates static html file along with dependencies to be used offline properly handling themes while removing links which are not accessible offline without php

--- gsoc2007/export2.php
+++ gsoc2007/export2.php
@@ -1,42 +1,102 @@
 <?php
-$album = 2;						 // Album to export
-$path  = 'testExport'; // Path to export to
+if(isset($_POST['exportSubmit']) && isset($_POST['directory']) && isset($_POST['album']))
+{
+  $album = $_POST['album'];						 // Album to export
+  $path  = $_POST['directory']; // Path to export to
 
-$filename = "thumbnails.php";
-$_GET['album'] = $album;
+  $filename = "thumbnails.php";
+  $_GET['album'] = $album;
+  if(is_file($filename)) {
+    include $filename;
+    $contents = ob_get_contents();
+    ob_end_clean();
 
-$src  =  "themes/classic";
-$dest =  "$path/themes/classic";
-recursive_copy($src,$dest);
+    // Create a DOM Object to parse the html (Removing links to dynamic content/functions that require php)
+    $doc = new DomDocument();
+    $doc->loadHtml($contents);
+    $divs = $doc->getElementsByTagName('div');
+    foreach($divs as $div)
+    {
+      if($div->getAttribute('id') == 'MENUS' || $div->getAttribute('class') == 'admin_menu_wrapper') {
+        $div->setAttribute('style','display:none');
+      }
+    }
+    $tds = $doc->getElementsByTagName('td');
+    foreach($tds as $td)
+    {
+      if($td->getAttribute('class') == 'sortorder_options') {
+        $td->setAttribute('style','display:none');
+      }
+    }
+    
+    
+    $contents = $doc->saveHTML();
+    $pictures = cpg_db_query("SELECT pid,title,filename,filepath FROM {$CONFIG['TABLE_PICTURES']} WHERE `aid` = '$album'");
 
-if(is_file($filename)) {
-	include $filename;
-	$contents = ob_get_contents();
-	ob_end_clean();
+    $picture_r = array();
+    while($picture = mysql_fetch_array($pictures))
+    {
+      $picture_r[$picture['pid']] = $picture['filepath'].$picture['filename'];
+      if(!is_dir("$path/albums/{$picture['filepath']}")) {
+        recursive_mkdir("$path/albums/{$picture['filepath']}");
+      }
+      copy("albums/{$picture['filepath']}{$picture['filename']}","$path/albums/{$picture['filepath']}{$picture['filename']}");
+      copy("albums/{$picture['filepath']}thumb_{$picture['filename']}","$path/albums/{$picture['filepath']}thumb_{$picture['filename']}");
 
-	$contents = preg_replace('/<!-- BEGIN [^\-]+ -->[^\!]+!-- END [^\-]+--> /',' ',$contents);
+      // Need to replace img lings to displayimage.php with direct links to photographs
+      $contents = preg_replace("/displayimage.php\?album=$album&amp;pid={$picture['pid']}/",'albums/'.$picture['filepath'].$picture['filename'],$contents);
+    }
 
-  $pictures = cpg_db_query("SELECT pid,title,filename,filepath FROM {$CONFIG['TABLE_PICTURES']} WHERE `aid` = '$album'");
+    // Find out the theme currently used and copy over necessary files to replicate this
+    $result = cpg_db_query("SELECT value FROM {$CONFIG['TABLE_CONFIG']} WHERE `name` = 'theme'");
+    $theme  = mysql_fetch_array($result);
 
-	$picture_r = array();
-  while($picture = mysql_fetch_array($pictures))
-  {
-  	$picture_r[$picture['pid']] = $picture['filepath'].$picture['filename'];
-		if(!is_dir('$path/albums/'.$picture['filepath']))
-			recursive_mkdir('$path/albums/'.$picture['filepath']);
-		copy('albums/'.$picture['filepath'].$picture['filename'],'$path/albums/'.$picture['filepath'].$picture['filename']);
-		copy('albums/'.$picture['filepath'].'thumb_'.$picture['filename'],'$path/albums/'.$picture['filepath'].'thumb_'.$picture['filename']);
-		
-		$contents = preg_replace("/displayimage.php\?album=$album&amp;pid={$picture['pid']}/",'albums/'.$picture['filepath'].$picture['filename'],$contents);
-  }
+    recursive_copy("themes/{$theme['value']}","$path/themes/{$theme['value']}");
 
-	file_put_contents("$path/index.html",$contents);
 
-	echo "success.<br><a href=$path/>Exported Album</a>";
-	
+    file_put_contents("$path/index.html",$contents);
+
+    header("location:$path");
+  } 
+} else {
+  define('IN_COPPERMINE',true);
+  require("include/init.inc.php");
+  if (!GALLERY_ADMIN_MODE) cpg_die(ERROR, $lang_errors['access_denied'], __FILE__, __LINE__);
+    pageheader($section, $meta_keywords);
+  starttable('60%', 'Chose an Album');
+  ?>
+    <tr><td><p>This is very experimental/early functionality demo.  This takes the album you specify from the drop down and copies the files necessary to render a static html file of the first page of thumbnails in the directory you specify in the export directory field (relative path from here). Note: this utilizes the current theme and settings but strips menus to inaccessible functions.  Debug info will show up if you have it configured to do so.</td></tr>
+    <tr><td>&nbsp;</td></tr>
+    <tr>
+    <td>
+    Select a Album:
+    <form action='<?php echo $_SERVER['PHP_SELF']?>' method='POST'>
+    <select name='album' class='listbox'>
+      <?php
+        $albums = cpg_db_query("SELECT * FROM {$CONFIG['TABLE_ALBUMS']} WHERE 1 ORDER BY `title`");
+        while($album = mysql_fetch_array($albums)) {
+          echo "<option value='$album[aid]'>$album[title]</option>";
+        }
+      ?>
+    </select><br/>
+    Export Directory:<br/>
+    <input type='text' value='export' name='directory'/><br/><br/>
+    <input type='submit' name='exportSubmit' value='Submit'/>
+    </form>
+    <br/><br/>
+  <?php
+  endtable();
+  echo "<br/><br/><br/><br/>";
+        
+  pagefooter();
+
+
 }
 
 
+
+
+// Does a recursive copy on a directory
 function recursive_copy($src,$dest)
 {
 	// Base case, is file so just copy
@@ -52,7 +112,7 @@ function recursive_copy($src,$dest)
 	
 		$dir = dir($src);
 		while (false !== $entry = $dir->read()) {		
-		if($entry== '.' || $entry == '..' || preg_match('/^\./',$entry))
+		if($entry== '.' || $entry == '..' || preg_match('/^\./',$entry)) // Skips the index '.' and '..' along with hidden files
 			continue;
 		if($dest !== "$src/$entry") {
 			recursive_copy("$src/$entry","$dest/$entry");
@@ -60,6 +120,7 @@ function recursive_copy($src,$dest)
 	}
 }
 
+// Recursively makes directories as necessary to create a given path:
 function recursive_mkdir( $folder )
 {
 $folder = explode( DIRECTORY_SEPARATOR , $folder );
