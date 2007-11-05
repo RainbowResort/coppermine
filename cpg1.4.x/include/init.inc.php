c8e94f726ba42250da81d512acd48af41c02ac34
gaugau <gaugau@62f5abee-3c11-0410-b416-987344bf19e2>
2007-11-05 08:20:20 +0000

    Changing version count as a preparation for the release of cpg1.4.14

--- cpg1.4.x/include/init.inc.php
+++ cpg1.4.x/include/init.inc.php
@@ -17,7 +17,7 @@
   $Date$
 **********************************************/
 
-define('COPPERMINE_VERSION', '1.4.13');
+define('COPPERMINE_VERSION', '1.4.14');
 define('COPPERMINE_VERSION_STATUS', 'stable');
 
 if (!defined('IN_COPPERMINE')) { die('Not in Coppermine...');}
