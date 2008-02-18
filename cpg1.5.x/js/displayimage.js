4823a638be2ceaa7eedb54c27779a5154d4aaf43
gaugau <gaugau@62f5abee-3c11-0410-b416-987344bf19e2>
2008-02-18 17:45:10 +0000

    Added file header (for version management purposes)

--- cpg1.5.x/js/displayimage.js
+++ cpg1.5.x/js/displayimage.js
@@ -11,9 +11,9 @@
   ********************************************
   Coppermine version: 1.5.0
   $HeadURL$
-  $Revision: 4220 $
-  $LastChangedBy: saweyyy $
-  $Date: 2008-01-24 23:08:30 +0100 (Do, 24 Jan 2008) $
+  $Revision: 4264 $
+  $LastChangedBy: gaugau $
+  $Date: 2008-02-11 19:11:06 +0100 (Mo, 11 Feb 2008) $
 **********************************************/
 
 /**
