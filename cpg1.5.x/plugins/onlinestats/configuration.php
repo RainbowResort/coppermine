029a67bed0398ce186bf3bdb9590561c1d28d4a7
gaugau <gaugau@62f5abee-3c11-0410-b416-987344bf19e2>
2008-06-10 06:14:11 +0000

    Fixed spelling and reflected documentation move.

--- cpg1.5.x/plugins/onlinestats/configuration.php
+++ cpg1.5.x/plugins/onlinestats/configuration.php
@@ -19,8 +19,8 @@
 
 $name = $lang_plugin_php['onlinestats_name'];
 $description = $lang_plugin_php['onlinestats_description'];
-$author = 'Originally created by <a href="http://coppermine-gallery.net/forum/index.php?action=profile;u=941">Nibbler</a>, internationalisation by <a href="http://coppermine-gallery.net/forum/index.php?action=profile;u=9980">Frantz</a>';
-$version = '2.0';
+$author = 'Originally created by <a href="http://coppermine-gallery.net/forum/index.php?action=profile;u=941">Nibbler</a>, <acronym title="internationalization">i18n</acronym> by <a href="http://coppermine-gallery.net/forum/index.php?action=profile;u=9980">Frantz</a>';
+$version = '2.1';
 $extra_info = $lang_plugin_php['onlinestats_config_extra'];
 $install_info = $lang_plugin_php['onlinestats_config_install'];
 ?>
