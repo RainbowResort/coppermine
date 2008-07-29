ca9c97e38e6e04487fd69f64886660c565a5cc02
rusticgeek <rusticgeek@62f5abee-3c11-0410-b416-987344bf19e2>
2008-07-29 08:17:31 +0000

    Editor functionality Added

--- gsoc2008/flashmanager.php
+++ gsoc2008/flashmanager.php
@@ -1,4 +1,31 @@
 <?php
+if($_GET['filename']=='snapshot.jpg'){
+
+//if (isset($superCage->server->getRAW('HTTP_RAW_POST_DATA'))
+{
+
+echo $GLOBALS["HTTP_RAW_POST_DATA"];
+echo  $HTTP_RAW_POST_DATA;
+echo "CALLED " ;
+	// get bytearray
+	$jpg = $GLOBALS["HTTP_RAW_POST_DATA"]; ;//$superCage->server->getRAW('HTTP_RAW_POST_DATA') ;//$GLOBALS["HTTP_RAW_POST_DATA"];
+
+	// add headers for download dialog-box
+//	header('Content-Type: image/jpeg');
+//	header('Content-Disposition: attachment; filename='.$superCage->get->getRAW('filename'));
+//	echo $jpg;
+	
+		// save image
+	$f = fopen("./albums/snapshot.jpg"/*$_GET['filename']*/, 'wb');
+	fwrite($f, $jpg);
+	fclose($f);
+	
+	echo "Written";
+
+}
+}
+else {
+
 define('IN_COPPERMINE', true);
 require('include/init.inc.php');
 $input = Inspekt::makeSuperCage();  
@@ -45,6 +72,48 @@ foreach ($arr as $value)
 }
 
 
+/* TREE BUIDLER 
+make root
+for each category
+	write category
+		for each element in category
+			write element
+		close element
+close category
+close root
+*/
+function treeBuilder(){
+global $CONFIG;
+cpg_db_connect();
+echo "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n";
+echo "<node label='GALLERY'>\n";
+$query = "SELECT cid, name  FROM {$CONFIG['TABLE_CATEGORIES']}";
+$categories = cpg_db_fetch_rowset(cpg_db_query($query));
+foreach ($categories as $category)
+{
+echo "<node label='$category[1]' t='C' i='$category[0]'>\n";
+//fetch subelements
+	$Albumquery = "SELECT aid, title FROM {$CONFIG['TABLE_ALBUMS']} WHERE category =".$category[0] ;
+	$albums = cpg_db_fetch_rowset(cpg_db_query($Albumquery));
+	foreach ($albums as $album)
+		{
+			echo "<node label='$album[1]' t='A' i='$album[0]'></node>\n";
+		}
+echo "</node>\n";
+}
+echo "</node>\n";
+}
+
+
+/* getimg - looks up the image in the DB and redirects the image  */
+function getimg($img){
+global $CONFIG;
+cpg_db_connect();
+$query = "SELECT * FROM {$CONFIG['TABLE_PICTURES']} where pid=".$img ;
+
+$image = cpg_db_fetch_rowset(cpg_db_query($query));
+header("Location: ".get_pic_url($image[0])) ;
+}
 
 
 
@@ -66,7 +135,7 @@ global $CONFIG;
 cpg_db_connect();
 $query = "SELECT cid, name  FROM {$CONFIG['TABLE_CATEGORIES']}";
 $categories = cpg_db_fetch_rowset(cpg_db_query($query));
-/* echo "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n"; */
+echo "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n";
 echo "<images>\n";
 foreach ($categories as $category)
 {
@@ -87,7 +156,7 @@ global $CONFIG;
 cpg_db_connect();
 $query = "SELECT * FROM {$CONFIG['TABLE_ALBUMS']} WHERE category =".$catid ;
 $albums = cpg_db_fetch_rowset(cpg_db_query($query));
-/* echo "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n"; */
+echo "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n";
 echo "<images>\n";
 foreach ($albums as $album)
 {
@@ -124,14 +193,26 @@ echo "</images>\n";
 }
 
 
+
+
+
+
 $get = Inspekt::makeGetCage();
-if($superCage->get->getRAW('getalb')==1)
-getImages($superCage->get->getRAW('alb'));
-else if ($superCage->get->getRAW('getroot')==1)
+if($superCage->get->getRAW('getalb')==1)  // Pictures from a particular album
+getImages($superCage->get->getRAW('alb'));  
+else if ($superCage->get->getRAW('getroot')==1) // get all root categories
 getroot();
-else if ($superCage->get->getRAW('getcat')==1)
+else if ($superCage->get->getRAW('getcat')==1) // get a particular category
 getAlbums($superCage->get->getRAW('cat'));
-else
+else if ($superCage->get->getRAW('albumlisting')==1)
+treeBuilder();
+else if ($superCage->get->getRAW('getimg')==1)
+getimg($superCage->get->getRAW('img'));
+
+
+
+
+else 
 {
 ?>
 <html>
@@ -140,7 +221,7 @@ else
 <object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=9,0,0,0" width="1024" height="600" id="player" align="middle">
 	<param name="allowScriptAccess" value="sameDomain" />
 	<param name="allowFullScreen" value="false" />
-	<param name="movie" value="player.swf" /><param name="quality" value="high" /><param name="bgcolor" value="#ffffff" />	<embed src="player.swf" quality="high" bgcolor="#0000000" width="1024" height="600" name="player" align="middle" allowScriptAccess="sameDomain" allowFullScreen="false" type="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/go/getflashplayer" />
+	<param name="movie" value="player.swf" /><param name="quality" value="high" /><param name="bgcolor" value="#000000" />	<embed src="player.swf" quality="high" bgcolor="#0000000" width="1024" height="600" name="player" align="middle" allowScriptAccess="sameDomain" allowFullScreen="false" type="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/go/getflashplayer" />
 	</object>
     </center>
 </body>
@@ -149,5 +230,5 @@ else
 
 <?php
 }
-
+}
 ?>
