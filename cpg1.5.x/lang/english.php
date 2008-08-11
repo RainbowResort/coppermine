cc602d2e97a1ce0933b12c65e009a3203ac82332
gaugau <gaugau@62f5abee-3c11-0410-b416-987344bf19e2>
2008-08-11 19:38:16 +0000

    Added more options to "enable menu icons" feature

--- cpg1.5.x/lang/english.php
+++ cpg1.5.x/lang/english.php
@@ -979,6 +979,8 @@ if (defined('ADMIN_PHP')) $lang_admin_php = array(
   'warning_change' => 'When changing this setting, only the files that are added from that point on are affected, so it\'s advisable that this setting must not be changed if there are already files in the gallery. You can, however, apply the changes to the existing files with the "admin tools (resize pictures)" utility from the admin menu.', // cpg1.5
   'warning_exist' => 'These settings mustn\'t be changed if you already have files in your database.', // cpg1.5
   'warning_dont_submit' => 'If you\\\'re not sure about the impact that changing this setting will have, don\\\'t submit the form and review the documentation first.', // cpg1.5 // js-alert
+  'menu_only' => 'menu only', // cpg1.5
+  'everywhere' => 'everywhere', // cpg1.5
 );
 
 
