f4a931577a56462c9ab37b3bf0e3c4c59e66fef5
rusticgeek <rusticgeek@62f5abee-3c11-0410-b416-987344bf19e2>
2008-08-18 15:51:26 +0000

    Added Save as Functionality

--- gsoc2008/flashmanager.php
+++ gsoc2008/flashmanager.php
@@ -1,24 +1,107 @@
+
 <?php
-if($_GET['filename']=='snapshot.jpg'){
 
-//if (isset($superCage->server->getRAW('HTTP_RAW_POST_DATA'))
-{
 
-echo $GLOBALS["HTTP_RAW_POST_DATA"];
-echo  $HTTP_RAW_POST_DATA;
-	// get bytearray
+//	define('SHOWTHUMB_PHP', true);
+
+
+	//BYPASS Inspekt to make upload work
+	if($_GET['upload']==1){
+
+	//if (isset($superCage->server->getRAW('HTTP_RAW_POST_DATA'))
+	print "image id ".$_GET['fileid'];
+	$img_id = $_GET['fileid'];
+	$img_savetype = $_GET['savetype'];
 	$jpg = $GLOBALS["HTTP_RAW_POST_DATA"]; ;
-
+	$size_x = $_GET['size_x'];
+	$size_y = $_GET['size_y'];
 // add headers for download dialog-box
 //	header('Content-Type: image/jpeg');
 //	header('Content-Disposition: attachment; filename='.$superCage->get->getRAW('filename'));
 //	echo $jpg;
 	
-		// save image
-	$f = fopen("./albums/snapshot.jpg"/*$_GET['filename']*/, 'wb');
+// save image to a temporary location
+	$f = fopen("./albums/snapshot.jpg", 'wb');
 	fwrite($f, $jpg);
 	fclose($f);
+
+	if ($img_savetype == 3)
+	print "DONE 1";
+
+	if ($img_savetype == 1 || $img_savetype == 2){
+	print "\n inside IF ... savetype =1 or 2? ";
+	//connect to the database get the filepath
+
+	define('IN_COPPERMINE', true);	
+	require('include/init.inc.php');
+
+//	require('include/picmgmt.inc.php');
+
+	global $CONFIG;
+	cpg_db_connect();
+	$query = "SELECT * FROM {$CONFIG['TABLE_PICTURES']} where pid=".$img_id ;
+	$image = cpg_db_fetch_rowset(cpg_db_query($query));
+	$image = $image[0];
+	// REPLACE OPTION --- OVERWRITE FILE
+	if ($img_savetype  == 1 ){
+	print "\n inside IF ... savetype =1 or 2? ";
+	copy('./albums/snapshot.jpg', './albums/'.$image[2].$image[3]); 
+	print "DONE 2 " . './albums/'.$image[2].$image[3];
+	}
+	else if($img_savetype  == 2){
+	//save it with a new name in the album Directory with currentdate prefix
+	//require('include/functions.inc.php');
+	$siz_e = cpg_getimagesize('./albums/snapshot.jpg', true);
+	echo "\n".$siz_e ; 
+	$file_name = date("Ymdgi").$image[3];
+	rename('./albums/snapshot.jpg', './albums/'.$image[2].$file_name); 
+	print "\n file name : ".$file_name;
+	//copy entires into the database
+	// All fields are duplicated except the id field, which
+	// is auto-incremented to the next available value.
+	//cpg_db_connect();	
+	$sql = 	"SELECT * FROM {$CONFIG['TABLE_PICTURES']} where pid=".$img_id ;
+	print "\n".$sql ;
+
+	$result = cpg_db_query($sql);
+	//print "\n fetched results ".echoArray($result);
+	
+	if ($result) {
+	$sql = "INSERT INTO {$CONFIG['TABLE_PICTURES']} SET ";
+	$row = mysql_fetch_array($result);
+	$RowKeys = array_keys($row);
+	$RowValues = array_values($row);
+
+	$sql .= $RowKeys[3] . " = '" . $RowValues[3] . "',";
+	$sql .= $RowKeys[5] . " = '" . $RowValues[5] . "',";
+	$sql .= $RowKeys[7] . " = '" . $file_name . "',";
+	$sql .= $RowKeys[9] . " = '" . $RowValues[9] . "',";
+	$sql .= $RowKeys[11] . " = '" . $RowValues[11] . "',";
+	$sql .= $RowKeys[13] . " = '" . $size_x  . "'," ;
+	$sql .= $RowKeys[15] . " = '" . $size_y  . "'," ;
+	
+	
+	for ($i=17;$i<count($RowKeys);$i+=2) {
+	if ($i!=17) { $sql .= ", "; }
+	$sql .= $RowKeys[$i] . " = '" . $RowValues[$i] . "'";
+	}
+	print "SQL QUERY LOOKS LIKE :\n".$sql."\n\n";
+	
+	$image_path = "./" . $image['filepath'] . $file_name;
+    $thumb = "./".$image['filepath'] . $CONFIG['thumb_pfx'] . $file_name;
+	print "\nIMG URL ".$image_path;
+	print "\n THUMB URL ".$thumb ;
+	define('UTIL_PHP', true);
+	require('include/picmgmt.inc.php');
+	$bool = resize_image($image, $thumb, $CONFIG['thumb_width'], $CONFIG['thumb_method'], $CONFIG['thumb_use'], "false", 1);
+    print "\nRESIZED ? : ". $bool. " ?";
+	print "DONE";
+
+	$result = cpg_db_query($sql);
+	}
 }
+}	
+
 }
 else {
 
@@ -139,6 +222,21 @@ function varFormat($id,$data){
 print "$id=$data" ;
 }
 
+function albumList(){
+global $CONFIG;
+cpg_db_connect();
+$query = "SELECT title FROM {$CONFIG['TABLE_ALBUMS']}" ;
+$albums = cpg_db_fetch_rowset(cpg_db_query($query));
+$albumStr = "";
+foreach ($albums as $album)
+$albumStr = $albumStr . "," . $album[0] ;
+
+$albumStr = substr($albumStr,1,$albumStr.length-1);
+varFormat("AlbumArray",$albumStr);
+
+
+}
+
 
 function getImagepath($image_id){
 cpg_db_connect();
@@ -204,6 +302,8 @@ foreach ($images as $image)
 		echo "<t>IMG</t>\n";
 		echo "<b>".get_pic_url($image)."</b>\n";
 		echo "<s>".get_pic_url($image,thumb)."</s>\n";
+	//	echo "<b>http://rgkdev.net/cpg/".get_pic_url($image)."</b>\n";
+	//	echo "<s>http://rgkdev.net/cpg/".get_pic_url($image,thumb)."</s>\n";
 		echo "</image>\n";
 }
 echo "</images>\n";
@@ -227,8 +327,8 @@ else if ($superCage->get->getRAW('getimg')==1)
 getimg($superCage->get->getRAW('img'));
 else if ($superCage->get->getRAW('photoinfo')==1)
 getPhotoInfo($superCage->get->getRAW('photo'));
-
-
+else if ($superCage->get->getRAW('albumList')==1)
+albumList();
 
 
 else 
@@ -250,4 +350,5 @@ else
 <?php
 }
 }
+
 ?>
