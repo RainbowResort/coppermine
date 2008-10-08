f3a12ac90b6bce72ef9bfb21febbc9fe3f9d1c6a
gaugau <gaugau@62f5abee-3c11-0410-b416-987344bf19e2>
2008-10-08 17:12:17 +0000

    Started to move JavaScript from admin.php into js/admin.js as per decision on dev board (thread id 49573)

--- cpg1.5.x/admin.php
+++ cpg1.5.x/admin.php
@@ -24,6 +24,15 @@ define('CONFIG_PHP', true); // added for backwards compatibility (language fallb
 require_once('include/init.inc.php');
 require_once('include/sql_parse.php');
 
+// define some vars that need to exist in JS
+set_js_var('lang_warning_dont_submit', $lang_admin_php['warning_dont_submit']);
+set_js_var('lang_reset_to_default', $lang_admin_php['reset_to_default']);
+set_js_var('lang_no_change_needed', $lang_admin_php['no_change_needed']);
+
+
+// Include the JS for admin.php
+js_include('js/admin.js');
+
 $admin_data_array = $CONFIG;
 $optionLoopCounter = 0;
 
@@ -520,122 +529,6 @@ echo <<< EOT
                 document.getElementById('collapse' + i).style.display = 'block';
             }
         }
-    }
-    function resetToDefault(theFieldId, fieldType, numberOfItems) 
-    {
-        //var foo = theFieldId + fieldType + numberOfItems;
-        //alert(numberOfItems);
-        //alert(fieldType);
-        if(fieldType == 'textfield' || fieldType == 'password') {
-            document.getElementById(theFieldId).value = document.getElementById('reset_default_' + theFieldId).value;
-            document.getElementById('reset_default_' + theFieldId).style.display = 'none';
-            document.getElementById('reset_default_' + theFieldId).checked = true;
-            return;
-        }
-        if(fieldType == 'checkbox') {
-            if (document.getElementById('reset_default_' + theFieldId).value == 1) {
-                document.getElementById(theFieldId).checked = true;
-            } else {
-                document.getElementById(theFieldId).checked = false;
-            }
-            document.getElementById('reset_default_' + theFieldId).style.display = 'none';
-            document.getElementById('reset_default_' + theFieldId).checked = true;
-            return;
-        }
-        if(fieldType == 'radio') {
-            document.getElementById(theFieldId + document.getElementById('reset_default_' + theFieldId).value).checked = true;
-            document.getElementById('reset_default_' + theFieldId).style.display = 'none';
-            document.getElementById('reset_default_' + theFieldId).checked = true;
-            return;
-        }
-        if(fieldType == 'select') {
-            for (var i = 0; i < numberOfItems; i++) {
-                //alert(document.getElementById(theFieldId).options[i].value);
-                if (document.getElementById(theFieldId).options[i].value == document.getElementById('reset_default_' + theFieldId).value) {
-                    document.getElementById(theFieldId).options[i].selected = true;
-                    document.getElementById('reset_default_' + theFieldId).style.display = 'none';
-                    document.getElementById('reset_default_' + theFieldId).checked = true;
-                    return; 
-                }
-            }
-        }
-    }
-    
-    function checkDefaultBox(theFieldId, fieldType, numberOfItems, warning) 
-    {
-        // Each time a config field is being changed (onblur/onchange), this JS is being run to enable/disable the default checkbox
-        if(warning != '') {
-            alert(warning + ' ' + '{$lang_admin_php['warning_dont_submit']}');
-        }
-        if(fieldType == 'textfield' || fieldType == 'password') {
-            if (document.getElementById(theFieldId).value != document.getElementById('reset_default_' + theFieldId).value) {
-                document.getElementById('reset_default_' + theFieldId).style.display = 'inline';
-                document.getElementById('reset_default_' + theFieldId).checked = false;
-                document.getElementById('reset_default_' + theFieldId).title = '{$lang_admin_php['reset_to_default']}';
-            } else {
-                document.getElementById('reset_default_' + theFieldId).style.display = 'none';
-                document.getElementById('reset_default_' + theFieldId).checked = true;
-                document.getElementById('reset_default_' + theFieldId).title = '{$lang_admin_php['reset_to_default']}: {$lang_admin_php['no_change_needed']} (' + document.getElementById('reset_default_' + theFieldId).value + ')';
-            }
-            return;
-        }
-        if(fieldType == 'checkbox') {
-            var checkboxNeedsChangeToChecked = 0;
-            if (document.getElementById(theFieldId).checked == true && document.getElementById('reset_default_' + theFieldId).value == 1) {
-                checkboxNeedsChangeToChecked = 1;
-            }
-            if (document.getElementById(theFieldId).checked == false && document.getElementById('reset_default_' + theFieldId).value == 0) {
-                checkboxNeedsChangeToChecked = 1;
-            }
-            if (checkboxNeedsChangeToChecked == 0) {
-                document.getElementById('reset_default_' + theFieldId).style.display = 'inline';
-                document.getElementById('reset_default_' + theFieldId).checked = false;
-                document.getElementById('reset_default_' + theFieldId).title = '{$lang_admin_php['reset_to_default']}';
-            } else {
-                document.getElementById('reset_default_' + theFieldId).style.display = 'none';
-                document.getElementById('reset_default_' + theFieldId).checked = true;
-                document.getElementById('reset_default_' + theFieldId).title = '{$lang_admin_php['reset_to_default']}: {$lang_admin_php['no_change_needed']} (' + document.getElementById('reset_default_' + theFieldId).value + ')';
-            }
-            return;
-        }
-        if(fieldType == 'radio') {
-            // theFieldId has got a number appended to it - let's strip it
-            theLoopCounterIndex = theFieldId.slice((theFieldId.length - 1),theFieldId.length); 
-            theFieldId = theFieldId.slice(0,(theFieldId.length - 1));
-            if (theLoopCounterIndex != document.getElementById('reset_default_' + theFieldId).value) {
-                document.getElementById('reset_default_' + theFieldId).style.display = 'inline';
-                document.getElementById('reset_default_' + theFieldId).checked = false;
-                document.getElementById('reset_default_' + theFieldId).title = '{$lang_admin_php['reset_to_default']}';
-            } else {
-                document.getElementById('reset_default_' + theFieldId).style.display = 'none';
-                document.getElementById('reset_default_' + theFieldId).checked = true;
-                document.getElementById('reset_default_' + theFieldId).title = '{$lang_admin_php['reset_to_default']}: {$lang_admin_php['no_change_needed']} (' + document.getElementById('reset_default_' + theFieldId).value + ')';
-            }
-            return;
-        }
-        if(fieldType == 'select') {
-            for (var i = 0; i < numberOfItems; i++) {
-                if (document.getElementById(theFieldId).options[i].selected == true) {
-                    if (document.getElementById(theFieldId).options[i].value == document.getElementById('reset_default_' + theFieldId).value) {
-                        document.getElementById('reset_default_' + theFieldId).style.display = 'none';
-                        document.getElementById('reset_default_' + theFieldId).checked = true;
-                        document.getElementById('reset_default_' + theFieldId).title = '{$lang_admin_php['reset_to_default']}: {$lang_admin_php['no_change_needed']} (' + document.getElementById('reset_default_' + theFieldId).value + ')';
-                        return;
-                    } else {
-                        document.getElementById('reset_default_' + theFieldId).style.display = 'inline';
-                        document.getElementById('reset_default_' + theFieldId).checked = false;
-                        document.getElementById('reset_default_' + theFieldId).title = '{$lang_admin_php['reset_to_default']}';
-                        return;
-                    }
-                }
-            }
-        }
-    }
-    
-    function deleteUnneededFields() 
-    {
-        $('.deleteOnSubmit').remove();
-        return true;
     }
 </script>
 EOT;
