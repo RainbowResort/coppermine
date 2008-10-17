fd53e30ee07df2b276a7b1b6e71cc540475b591e
gaugau <gaugau@62f5abee-3c11-0410-b416-987344bf19e2>
2008-10-17 07:20:49 +0000

    Changed header to match standards

--- cpg1.5.x/albums/index.php
+++ cpg1.5.x/albums/index.php
@@ -1,16 +1,27 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<?php
+/*************************
+  Coppermine Photo Gallery
+  ************************
+  Copyright (c) 2003-2008 Dev Team
+  v1.1 originally written by Gregory DEMAR
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License version 3
+  as published by the Free Software Foundation.
+
+  ********************************************
+  Coppermine version: 1.5.0
+  $Source: /cvsroot/coppermine/devel/admin.php,v $
+  $Revision$
+  $LastChangedBy$
+  $Date$
+**********************************************/
+?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en" dir="ltr">
 <head>
 <meta http-equiv="content-type" content="text/html; charset=utf-8" />
 <meta http-equiv="refresh" content="0; url=../index.php">
 <title>Coppermine Photo Gallery - Albums Folder</title>
-<!--
-  SVN version info:
-  Coppermine version: 1.5.0
-  $Revision$
-  $LastChangedBy$
-  $Date$
--->
 <style type="text/css">
 <!--
 body {
