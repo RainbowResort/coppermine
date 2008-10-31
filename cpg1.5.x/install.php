f098a26e2faec2bce668f13a87bfe5f409fe3f13
gaugau <gaugau@62f5abee-3c11-0410-b416-987344bf19e2>
2008-10-31 08:36:23 +0000

    Fixed encoding and static width issue

--- cpg1.5.x/install.php
+++ cpg1.5.x/install.php
@@ -499,14 +499,16 @@ function html_header()
 
     global $install;
 ?>
-<!doctype html public "-//W3C//DTD HTML 4.01 Transitional//EN">
-<html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en" dir="ltr">
 <head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta http-equiv="Pragma" content="no-cache" />
 <title>Coppermine - <?php echo $install->language['installation']; ?></title><link type="text/css" rel="stylesheet" href="installer.css">
 </head>
 <body>
  <div align="center">
-  <div style="width:600px;">
+  <div>
   <table width="100%" border="0" cellpadding="0" cellspacing="1" class="maintable">
     <tr>
       <td valign="top" style="background-color:#EFEFEF"><img src="images/coppermine-logo.png" width="260" height="60" border="0" alt="" /><br />
