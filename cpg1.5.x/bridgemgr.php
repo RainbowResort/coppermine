09b1e35b0f1b5e4b02d07c9ca38c6c6b10198cc9
pvanrompay <pvanrompay@62f5abee-3c11-0410-b416-987344bf19e2>
2008-12-16 08:49:37 +0000

    Fix restore groups in bridge manager (with new groups-based access level defaults)

--- cpg1.5.x/bridgemgr.php
+++ cpg1.5.x/bridgemgr.php
@@ -629,11 +629,11 @@ case "finalize":
                         // ok, then restore group table
                         cpg_db_query("DELETE FROM {$CONFIG['TABLE_USERGROUPS']} WHERE 1");
                         cpg_db_query("INSERT INTO {$CONFIG['TABLE_USERGROUPS']}
-                        VALUES (1, 'Administrators', 0, 1, 1, 1, 1, 1, 1, 0, 0, 3, 0, 5, 3)");
+                        VALUES (1, 'Administrators', 0, 1, 1, 1, 1, 1, 1, 0, 0, 3, 0, 5, 3, 3)");
                         cpg_db_query("INSERT INTO {$CONFIG['TABLE_USERGROUPS']}
-                        VALUES (2, 'Registered', 1024, 0, 1, 1, 1, 1, 1, 1, 0, 3, 0, 5, 3)");
+                        VALUES (2, 'Registered', 1024, 0, 1, 1, 1, 1, 1, 1, 0, 3, 0, 5, 3, 3)");
                         cpg_db_query("INSERT INTO {$CONFIG['TABLE_USERGROUPS']}
-                        VALUES (3, 'Anonymous', 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 5, 3)");
+                        VALUES (3, 'Anonymous', 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 5, 3, 3)");
                 }
         print '<form name="'.$step.'" id="cpgform" action="'.$CPG_PHP_SELF.'" method="post">';
         echo <<<EOT
@@ -835,11 +835,11 @@ else { // not in gallery admin mode --- start
                         // ok, then restore group table
                         cpg_db_query("DELETE FROM {$CONFIG['TABLE_USERGROUPS']} WHERE 1");
                         cpg_db_query("INSERT INTO {$CONFIG['TABLE_USERGROUPS']}
-                        VALUES (1, 'Administrators', 0, 1, 1, 1, 1, 1, 1, 0, 0, 3, 0, 5, 3)");
+                        VALUES (1, 'Administrators', 0, 1, 1, 1, 1, 1, 1, 0, 0, 3, 0, 5, 3, 3)");
                         cpg_db_query("INSERT INTO {$CONFIG['TABLE_USERGROUPS']}
-                        VALUES (2, 'Registered', 1024, 0, 1, 1, 1, 1, 1, 1, 0, 3, 0, 5, 3)");
+                        VALUES (2, 'Registered', 1024, 0, 1, 1, 1, 1, 1, 1, 0, 3, 0, 5, 3, 3)");
                         cpg_db_query("INSERT INTO {$CONFIG['TABLE_USERGROUPS']}
-                        VALUES (3, 'Anonymous', 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 5, 3)");
+                        VALUES (3, 'Anonymous', 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 5, 3, 3)");
 
             if (USER_ID) { //user already logged in
                 msg_box($lang_bridgemgr_php['recovery_success_title'], $lang_bridgemgr_php['recovery_success_content'], $lang_bridgemgr_php['goto_bridgemgr'], $CPG_PHP_SELF, "-1");
