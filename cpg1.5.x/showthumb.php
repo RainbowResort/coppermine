f05298b2a268b179dadab001ddd9c0ec50966ca6
nibbler999 <nibbler999@62f5abee-3c11-0410-b416-987344bf19e2>
2008-12-30 01:56:11 +0000

    Cleanup for coding standards

--- cpg1.5.x/showthumb.php
+++ cpg1.5.x/showthumb.php
@@ -22,7 +22,9 @@ define('SHOWTHUMB_PHP', true);
 
 require("include/init.inc.php");
 
-if (!GALLERY_ADMIN_MODE) cpg_die(ERROR, $lang_errors['access_denied'], __FILE__, __LINE__);
+if (!GALLERY_ADMIN_MODE) {
+    cpg_die(ERROR, $lang_errors['access_denied'], __FILE__, __LINE__);
+}
 
 define("GIS_GIF", 1);
 define("GIS_JPG", 2);
@@ -39,14 +41,16 @@ function makethumbnail($src_file, $newSize, $method)
     $content_type = array(
         GIS_GIF => 'gif',
         GIS_JPG => 'jpeg',
-        GIS_PNG => 'png'
-        );
+        GIS_PNG => 'png',
+    );
+    
     // Checks that file exists and is readable
     if (!filesize($src_file) || !is_readable($src_file)) {
         header("Content-type: image/png");
         fpassthru(fopen(READ_ERROR_ICON, 'rb'));
         exit;
     }
+    
     // find the image size, no size => unknow type
     $imginfo = cpg_getimagesize($src_file);
     if ($imginfo == null) {
@@ -54,6 +58,7 @@ function makethumbnail($src_file, $newSize, $method)
         fpassthru(fopen(UNKNOW_ICON, 'rb'));
         exit;
     }
+    
     // GD can't handle gif images
     //if ($imginfo[2] == GIS_GIF && ($method == 'gd1' || $method == 'gd2')) {
     if ($imginfo[2] == GIS_GIF && $CONFIG['GIF_support'] == 0) {
@@ -61,6 +66,7 @@ function makethumbnail($src_file, $newSize, $method)
         fpassthru(fopen(GIF_ICON, 'rb'));
         exit;
     }
+    
     // height/width
     $srcWidth = $imginfo[0];
     $srcHeight = $imginfo[1];
@@ -69,49 +75,68 @@ function makethumbnail($src_file, $newSize, $method)
     $ratio = max($ratio, 1.0);
     $destWidth = (int)($srcWidth / $ratio);
     $destHeight = (int)($srcHeight / $ratio);
+    
     // Choose method for thumb creation
     switch ($method) {
-        case "im" :
-            if (preg_match("#[A-Z]:|\\\\#Ai", __FILE__)) {
-                $cur_dir = dirname(__FILE__);
-                $src_file = '"' . $cur_dir . '\\' . strtr($src_file, '/', '\\') . '"';
-            } else {
-                $src_file = escapeshellarg($src_file);
-            }
-            header("Content-type: image/" . ($content_type[$imginfo[2]]));
-            passthru("{$CONFIG['impath']}convert -quality $CONFIG[jpeg_qual] -antialias -geometry {$destWidth}x{$destHeight} $src_file -");
-            break;
+    
+    case "im":
+    
+        if (preg_match("#[A-Z]:|\\\\#Ai", __FILE__)) {
+            $cur_dir = dirname(__FILE__);
+            $src_file = '"' . $cur_dir . '\\' . strtr($src_file, '/', '\\') . '"';
+        } else {
+            $src_file = escapeshellarg($src_file);
+        }
+        
+        header("Content-type: image/" . ($content_type[$imginfo[2]]));
+        passthru("{$CONFIG['impath']}convert -quality $CONFIG[jpeg_qual] -antialias -geometry {$destWidth}x{$destHeight} $src_file -");
 
-        case "gd2" :
-            if ($imginfo[2] == GIS_GIF && $CONFIG['GIF_support'] == 1)
-                $src_img = imagecreatefromgif($src_file);
-            elseif ($imginfo[2] == GIS_JPG)
-                $src_img = imagecreatefromjpeg($src_file);
-            else
-                $src_img = imagecreatefrompng($src_file);
-            if ($imginfo[2] == GIS_GIF)
-              $dst_img = imagecreate($destWidth, $destHeight);
-            else
-              $dst_img = imagecreatetruecolor($destWidth, $destHeight);
-            imagecopyresampled($dst_img, $src_img, 0, 0, 0, 0, $destWidth, (int)$destHeight, $srcWidth, $srcHeight);
-            header("Content-type: image/jpeg");
-            imagejpeg($dst_img);
-            imagedestroy($src_img);
-            imagedestroy($dst_img);
-            break;
+        break;
 
-        default :
-            if ($imginfo[2] == GIS_JPG)
-                $src_img = imagecreatefromjpeg($src_file);
-            else
-                $src_img = imagecreatefrompng($src_file);
+    case "gd2":
+    
+        if ($imginfo[2] == GIS_GIF && $CONFIG['GIF_support'] == 1) {
+            $src_img = imagecreatefromgif($src_file);
+        } elseif ($imginfo[2] == GIS_JPG) {
+            $src_img = imagecreatefromjpeg($src_file);
+        } else {
+            $src_img = imagecreatefrompng($src_file);
+        }
+        
+        if ($imginfo[2] == GIS_GIF) {
             $dst_img = imagecreate($destWidth, $destHeight);
-            imagecopyresized($dst_img, $src_img, 0, 0, 0, 0, $destWidth, (int)$destHeight, $srcWidth, $srcHeight);
-            header("Content-type: image/jpeg");
-            imagejpeg($dst_img);
-            imagedestroy($src_img);
-            imagedestroy($dst_img);
-            break;
+        } else {
+            $dst_img = imagecreatetruecolor($destWidth, $destHeight);
+        }
+        
+        imagecopyresampled($dst_img, $src_img, 0, 0, 0, 0, $destWidth, (int)$destHeight, $srcWidth, $srcHeight);
+
+        header("Content-type: image/jpeg");
+
+        imagejpeg($dst_img);
+        imagedestroy($src_img);
+        imagedestroy($dst_img);
+
+        break;
+
+    default:
+    
+        if ($imginfo[2] == GIS_JPG) {
+            $src_img = imagecreatefromjpeg($src_file);
+        } else {
+            $src_img = imagecreatefrompng($src_file);
+        }
+        
+        $dst_img = imagecreate($destWidth, $destHeight);
+        imagecopyresized($dst_img, $src_img, 0, 0, 0, 0, $destWidth, (int)$destHeight, $srcWidth, $srcHeight);
+
+        header("Content-type: image/jpeg");
+
+        imagejpeg($dst_img);
+        imagedestroy($src_img);
+        imagedestroy($dst_img);
+
+        break;
     }
 }
 
