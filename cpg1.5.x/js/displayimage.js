880a85c07db1afc148ba56f3726de1790832f3f3
abbas-ali <abbas-ali@62f5abee-3c11-0410-b416-987344bf19e2>
2008-12-30 05:07:54 +0000

    - Fixed the picinfo and slideshow buttons not getting shown if there are less than max_filmstrip_items in filmstrip

--- cpg1.5.x/js/displayimage.js
+++ cpg1.5.x/js/displayimage.js
@@ -23,157 +23,164 @@
         
 $(document).ready(function() {
 
-            var nextPosition    = parseInt(js_vars.position); 
-            var NumberOfPics    = parseInt(js_vars.count);
-            var album           = js_vars.album; 
-            var maxItems        = parseInt(js_vars.max_item);
-            var width           = js_vars.thumb_width;
-            var cat				= parseInt(js_vars.cat);
+    var nextPosition    = parseInt(js_vars.position); 
+    var NumberOfPics    = parseInt(js_vars.count);
+    var album           = js_vars.album; 
+    var maxItems        = parseInt(js_vars.max_item);
+    var width           = js_vars.thumb_width;
+    var cat				= parseInt(js_vars.cat);
+    // Display the stars        
+    displayStars();
+    // Display the slideshow buttons
+    printSlideshowButton();
+    // Display the pic info button
+	printPicInfoButton();
+
+    /** The code below this is filmstrip specific **/
+   // We need not execute the filmstrip js if there are not enough pictures in the album        
+    if(maxItems%2==0){
+        maxItems    = maxItems +1;
+    }
+    /**stop, if we don't have enugh images than maxItem*/
+    if(NumberOfPics <= maxItems){               
+        return false;
+    }
+    
+    //variables to handle the next - prev button
+    var picQueue        = (maxItems+1)/2;
+    var $go_next        = parseInt(maxItems/2);
+    //cache the images RULs 
+    //create a objects to keep an array
+    var url_cache       = new Array(NumberOfPics);
+    var link_cache      = new Array(NumberOfPics);
+    var img             = new Image();
+    //checking position is zero and assign $go_next to zero
+    if(nextPosition < picQueue) {
+        var cacheIndex      = 0;
+
+    } else if(nextPosition > (NumberOfPics-picQueue)) {
+        var cacheIndex = NumberOfPics - maxItems;
+        url_cache[0] = "";
+    } else {
+        var cacheIndex = (nextPosition-$go_next);
+        url_cache[0] = "";
+    }
+    
+    // checking position is last thumb image
+    for(var i=0; i<maxItems; i++) { 
+        url_cache[cacheIndex+i] = $("img.strip_image").eq(i).attr("src");
+        link_cache[cacheIndex+i]= $("a.thumbLink").eq(i).attr("href");
+    }
+
+    var prev_link = nextPosition > (picQueue-1) ? "<a id=\"filmstrip_prev\" rel=\"nofollow\" style=\"cursor: pointer;\"><img src=\"./images/icons/left.png\" border=\"0\" /></a>" : "<a id=\"filmstrip_prev\" rel=\"nofollow\" style=\"cursor: pointer;display:none\"><img src=\"./images/icons/left.png\" border=\"0\" /></a>";            
+    var next_link = nextPosition < (NumberOfPics - picQueue) ? "<a id=\"filmstrip_next\" rel=\"nofollow\" style=\"cursor: pointer;\"><img src=\"./images/icons/right.png\" border=\"0\" /></a>" : "<a id=\"filmstrip_next\" rel=\"nofollow\" style=\"cursor: pointer;display:none\"><img src=\"./images/icons/right.png\" border=\"0\" /></a>";
+
+    $('td.prev_strip').html(prev_link);
+    $('td.next_strip').html(next_link);
+
+    //set position if it is not zero 
+    if(nextPosition < $go_next) {
+        nextPosition    = $go_next;
+    }
+    
+    //set postion if it is at end 
+    if(nextPosition > (NumberOfPics-picQueue)) {
+        nextPosition    = (NumberOfPics-picQueue);
+    }
+    
+    // Bind a onclick event on element with id filmstrip_next
+    $('#filmstrip_next').click(function() {
+        // Get the url for next set of thumbnails. This will be the href of 'next' link; 
+        nextPosition = nextPosition +1;
+        
+        if(((NumberOfPics-1)-(picQueue-1)) <= nextPosition ) {
+            $('#filmstrip_next').hide();
+        }
+        //assign a variable to check initial position to next 
+        if(nextPosition < (picQueue-1)) {
+        // nextPosition = picQueue-1;
+        }
+
+        if(nextPosition > (picQueue-1)) {
+            $('#filmstrip_prev').show();            
+        }
             
-            if(maxItems%2==0){
-                maxItems    = maxItems +1;
-            }
-            /**stop, if we don't have enugh images than maxItem*/
-            if(NumberOfPics <= maxItems){               
-                return false;
+        if (!url_cache[nextPosition + $go_next]) {
+        	if (!isNaN(cat)) {
+                addCat = '&cat=' + cat;
+            } else {
+                addCat = "";
             }
             
-            //variables to handle the next - prev button
-            var picQueue        = (maxItems+1)/2;
-            var $go_next        = parseInt(maxItems/2);
-            //cache the images RULs 
-            //create a objects to keep an array
-            var url_cache       = new Array(NumberOfPics);
-            var link_cache      = new Array(NumberOfPics);
-            var img             = new Image();
-            //checking position is zero and assign $go_next to zero
-            if(nextPosition < picQueue){
-                var cacheIndex      = 0;
+			var next_url = "displayimage.php?film_strip=1&album=" + album + "&ajax_call=2&pos=" + nextPosition+addCat;
+            // Send the ajax request for getting next set of filmstrip thumbnails
+            $.getJSON(next_url, function(data) {
 
-            } else if(nextPosition > (NumberOfPics-picQueue)){
-                var cacheIndex      = NumberOfPics - maxItems;
-                    url_cache[0]="";
-            }else{
-                var cacheIndex      = (nextPosition-$go_next);
-                    url_cache[0]="";
-            }
-            // checking position is last thumb image
-
-            for(var i=0; i<maxItems; i++){ 
-                url_cache[cacheIndex+i] = $("img.strip_image").eq(i).attr("src");
-                link_cache[cacheIndex+i]= $("a.thumbLink").eq(i).attr("href");
-            }
-
-            var prev_link = nextPosition > (picQueue-1) ? "<a id=\"filmstrip_prev\" rel=\"nofollow\" style=\"cursor: pointer;\"><img src=\"./images/icons/left.png\" border=\"0\" /></a>" : "<a id=\"filmstrip_prev\" rel=\"nofollow\" style=\"cursor: pointer;display:none\"><img src=\"./images/icons/left.png\" border=\"0\" /></a>";            
-            var next_link = nextPosition < (NumberOfPics - picQueue) ? "<a id=\"filmstrip_next\" rel=\"nofollow\" style=\"cursor: pointer;\"><img src=\"./images/icons/right.png\" border=\"0\" /></a>" : "<a id=\"filmstrip_next\" rel=\"nofollow\" style=\"cursor: pointer;display:none\"><img src=\"./images/icons/right.png\" border=\"0\" /></a>";
-
-            $('td.prev_strip').html(prev_link);
-            $('td.next_strip').html(next_link);
-
-                //set position if it is not zero 
-                if(nextPosition < $go_next){
-                    nextPosition    = $go_next;
-                }
-                //set postion if it is at end 
-                if(nextPosition > (NumberOfPics-picQueue)){
-                    nextPosition    = (NumberOfPics-picQueue);
-                }
-            // Bind a onclick event on element with id filmstrip_next
-        $('#filmstrip_next').click(function() {
-            // Get the url for next set of thumbnails. This will be the href of 'next' link; 
-            nextPosition = nextPosition +1;
-            
-            if(((NumberOfPics-1)-(picQueue-1)) <= nextPosition ){
-                $('#filmstrip_next').hide();
-            }
-            //assign a variable to check initial position to next 
-            if(nextPosition < (picQueue-1)){
-            // nextPosition = picQueue-1;
-            }
-
-            if(nextPosition > (picQueue-1)){
-                $('#filmstrip_prev').show();            
-            }
+                url_cache[nextPosition+$go_next]  = data['url'];
+                link_cache[nextPosition+$go_next] = data['target'];
                 
-            if (!url_cache[nextPosition + $go_next]) {
-            	
-            	if(!isNaN(cat)){
-            		 addCat	= '&cat='+cat;
- 				}
- 				else addCat ="";
-                
-				var next_url = "displayimage.php?film_strip=1&album=" + album + "&ajax_call=2&pos=" + nextPosition+addCat;
-                // Send the ajax request for getting next set of filmstrip thumbnails
-                $.getJSON(next_url, function(data){
-
-                    url_cache[nextPosition+$go_next]  = data['url'];
-                    link_cache[nextPosition+$go_next] = data['target'];
-                    
-                    var itemLength = $(".tape tr > .thumb").length;
-                    var itemsToRemove = maxItems+1;
-                    if (itemLength == itemsToRemove) {
-                        $('.remove').remove();
-                    }
-                    $('.tape').css("marginLeft", '0px');
-                    var thumb = '<td align="center" class="thumb" ><a style="width: '+width+'px; float: left" href="' + data['target'] + '#top_display_media"><img border="0"  class="strip_image" src="' + data['url'] + '"/></a></td>';
-                    $('.tape tr').append(thumb);
-                        tempWidth = parseInt(width) +3;
-                    $('.tape').animate({
-                        marginLeft: "-"+tempWidth+"px"
-                    });
-                    
-                    $('.thumb').eq(0).addClass("remove");
-                    
-                    
-                });
-            }
-            else {
                 var itemLength = $(".tape tr > .thumb").length;
-                if (itemLength == (maxItems+1)) {
+                var itemsToRemove = maxItems+1;
+                if (itemLength == itemsToRemove) {
                     $('.remove').remove();
                 }
                 $('.tape').css("marginLeft", '0px');
-                var thumb = '<td align="center"  class="thumb" ><a style="width: '+width+'px; float: left" href="' + link_cache[nextPosition + $go_next] + '#top_display_media"><img border="0"  class="strip_image" src="' + url_cache[nextPosition + $go_next] + '"/></a></td>';
+                var thumb = '<td align="center" class="thumb" ><a style="width: '+width+'px; float: left" href="' + data['target'] + '#top_display_media"><img border="0"  class="strip_image" src="' + data['url'] + '"/></a></td>';
                 $('.tape tr').append(thumb);
-                    tempWidth = parseInt(width) +3;
+                tempWidth = parseInt(width) +3;
                 $('.tape').animate({
                     marginLeft: "-"+tempWidth+"px"
                 });
                 
                 $('.thumb').eq(0).addClass("remove");
+            });
+        } else {
+            var itemLength = $(".tape tr > .thumb").length;
+            if (itemLength == (maxItems+1)) {
+                $('.remove').remove();
             }
-        });
-
-
-        // Bind a onclick event on element with id filmstrip_prev
-        $('#filmstrip_prev').click(function() {
-            // Get the url for previous set of thumbnails. This will be the href of 'previous' link
-        
-            nextPosition = nextPosition -1;
-        
-            if(nextPosition >= ((NumberOfPics-1)-(picQueue-1))){
-            var nextPosition_to = (NumberOfPics-1)-(picQueue-1);
-            }
-            else{
-            var nextPosition_to = nextPosition; 
-            }
-            if(nextPosition_to <= (NumberOfPics-(picQueue))){
-                $('#filmstrip_next').show();    
-            }
-            if(nextPosition_to < (picQueue)){
-                $('#filmstrip_prev').hide();            
-            }
-                        
-        if(!url_cache[nextPosition-$go_next]) {
-    	
-    	    if(!isNaN(cat)){
-        		 addCat	= '&cat='+cat;
-			}
-			else addCat ="";
- 				
-            var prev_url = "displayimage.php?film_strip=1&album="+album+"&ajax_call=1&pos="+nextPosition+addCat;  
-            $.getJSON(prev_url, function(data){
+            $('.tape').css("marginLeft", '0px');
+            var thumb = '<td align="center"  class="thumb" ><a style="width: '+width+'px; float: left" href="' + link_cache[nextPosition + $go_next] + '#top_display_media"><img border="0"  class="strip_image" src="' + url_cache[nextPosition + $go_next] + '"/></a></td>';
+            $('.tape tr').append(thumb);
+            tempWidth = parseInt(width) +3;
+            $('.tape').animate({
+                marginLeft: "-"+tempWidth+"px"
+            });
             
+            $('.thumb').eq(0).addClass("remove");
+        }
+    });
+
+
+    // Bind a onclick event on element with id filmstrip_prev
+    $('#filmstrip_prev').click(function() {
+        // Get the url for previous set of thumbnails. This will be the href of 'previous' link
+        nextPosition = nextPosition -1;
+    
+        if(nextPosition >= ((NumberOfPics-1)-(picQueue-1))) {
+            var nextPosition_to = (NumberOfPics-1)-(picQueue-1);
+        } else {
+            var nextPosition_to = nextPosition; 
+        }
+        
+        if(nextPosition_to <= (NumberOfPics-(picQueue))) {
+            $('#filmstrip_next').show();
+        }
+        
+        if(nextPosition_to < (picQueue)) {
+            $('#filmstrip_prev').hide();            
+        }
+                    
+        if(!url_cache[nextPosition-$go_next]) {
+	
+    	    if (!isNaN(cat)) {
+                addCat = '&cat=' + cat;
+            } else {
+                addCat = "";
+            }
+				
+            var prev_url = "displayimage.php?film_strip=1&album="+album+"&ajax_call=1&pos="+nextPosition+addCat;  
+            $.getJSON(prev_url, function(data) {
                 url_cache[nextPosition-$go_next]  = data['url'];
                 link_cache[nextPosition-$go_next] = data['target'];
             
@@ -185,37 +192,31 @@ $(document).ready(function() {
                 $('.tape').css("marginLeft", '-'+width+'px');
                 var thumb_prev = '<td align="center" class="thumb" ><a style="width: '+width+'px; float: left" href="'+data['target']+'#top_display_media"><img border="0" class="strip_image" src="'+data['url']+'"/></a></td>';
                 $('.tape tr').prepend(thumb_prev);
-
+    
                 $('.tape').animate({
                     marginLeft: "0px"
                 });
- 
-
+    
                 $('.thumb').eq((maxItems)).addClass("remove");
-                
             });
-        }
-        else{
-                var itemLength = $(".tape tr > .thumb").length;
-                if (itemLength == (maxItems+1)) {
-                    $('.remove').remove();
-                }
-        
-                $('.tape').css("marginLeft", '-'+width+'px');
-                var thumb_prev = '<td align="center" class="thumb" ><a style="width: '+width+'px; float: left" href="'+link_cache[nextPosition-$go_next]+'#top_display_media"><img border="0"  class="strip_image" src="'+url_cache[nextPosition-$go_next]+'"/></a></td>';
-                $('.tape tr').prepend(thumb_prev);
-                
-                $('.tape').animate({
-                    marginLeft: "0px"
-                });
+        } else {
+            var itemLength = $(".tape tr > .thumb").length;
+            if (itemLength == (maxItems+1)) {
+                $('.remove').remove();
+            }
+    
+            $('.tape').css("marginLeft", '-'+width+'px');
+            var thumb_prev = '<td align="center" class="thumb" ><a style="width: '+width+'px; float: left" href="'+link_cache[nextPosition-$go_next]+'#top_display_media"><img border="0"  class="strip_image" src="'+url_cache[nextPosition-$go_next]+'"/></a></td>';
+            $('.tape tr').prepend(thumb_prev);
+            
+            $('.tape').animate({
+                marginLeft: "0px"
+            });
 
-                $('.thumb').eq(maxItems).addClass("remove");
+            $('.thumb').eq(maxItems).addClass("remove");
         }
         
-        })
-        displayStars();
-        printSlideshowButton();
-		printPicInfoButton();
+    });
 });
 
 
