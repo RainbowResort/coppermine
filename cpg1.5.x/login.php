567c7c37a58db80b466ed4919229d70824ed47b6
nibbler999 <nibbler999@62f5abee-3c11-0410-b416-987344bf19e2>
2009-01-03 17:09:19 +0000

    Cleanup for coding standards

--- cpg1.5.x/login.php
+++ cpg1.5.x/login.php
@@ -22,56 +22,66 @@ define('LOGIN_PHP', true);
 
 require('include/init.inc.php');
 
-if (USER_ID) cpg_die(ERROR, $lang_login_php['err_already_logged_in'], __FILE__, __LINE__);
+if (USER_ID) {
+    cpg_die(ERROR, $lang_login_php['err_already_logged_in'], __FILE__, __LINE__);
+}
 
-if (defined('UDB_INTEGRATION')) $cpg_udb->login_page();
+if (defined('UDB_INTEGRATION')) {
+    $cpg_udb->login_page();
+}
 
 //$referer = $superCage->get->keyExists('referer') ? $superCage->get->getRaw('referer') : 'index.php';
 if (strpos($CPG_REFERER, "logout.php") !== false) {
-  $CPG_REFERER = "index.php";
+    $CPG_REFERER = "index.php";
 }
-$login_failed = '';
+
+$login_failed   = '';
 $cookie_warning = '';
 
 if ($superCage->post->keyExists('submitted')) {
+
     if ($superCage->server->testip('REMOTE_ADDR')) {
-    	$ip = $superCage->server->getRaw('REMOTE_ADDR');
+        $ip = $superCage->server->getRaw('REMOTE_ADDR');
     } else {
-    	$ip = 'Unknown';
+        $ip = 'Unknown';
     }
-    if ( $USER_DATA = $cpg_udb->login( $superCage->post->getEscaped('username'), $superCage->post->getEscaped('password'), $superCage->post->getInt('remember_me') ) ) {
+    
+    if ($USER_DATA = $cpg_udb->login($superCage->post->getEscaped('username'), $superCage->post->getEscaped('password'), $superCage->post->getInt('remember_me'))) {
         //$referer=preg_replace("'&amp;'","&",$referer);
         // Write the log entry
-        log_write("Successful login by Username: ".$superCage->post->getEscaped('username')." from IP $ip on " . localised_date(-1,$lang_date['log']),CPG_SECURITY_LOG);
+        log_write("Successful login by Username: " . $superCage->post->getEscaped('username') . " from IP $ip on " . localised_date(-1, $lang_date['log']), CPG_SECURITY_LOG);
+
         // Set the language preference
-        $sql = "UPDATE {$CONFIG['TABLE_USERS']} SET " . "user_language = '" . $USER['lang'] . "' WHERE user_id = '" . $USER_DATA[user_id] . "'";
+        $sql = "UPDATE {$CONFIG['TABLE_USERS']} SET user_language = '{$USER['lang']}' WHERE user_id = {$USER_DATA['user_id']}";
         $result = cpg_db_query($sql);
-        cpgRedirectPage($CPG_REFERER, $lang_login_php['login'], sprintf($lang_login_php['welcome'], $USER_DATA['user_name']),3);
+
+        cpgRedirectPage($CPG_REFERER, $lang_login_php['login'], sprintf($lang_login_php['welcome'], $USER_DATA['user_name']), 3);
         exit;
     } else {
         // Write the log entry
-        log_write("Failed login attempt with Username: ".$superCage->post->getEscaped('username')." from IP $ip on " . localised_date(-1,$lang_date['log']),CPG_SECURITY_LOG);
+        log_write("Failed login attempt with Username: " . $superCage->post->getEscaped('username') . " from IP $ip on " . localised_date(-1, $lang_date['log']), CPG_SECURITY_LOG);
 
         $login_failed = <<<EOT
                   <tr>
-                          <td colspan="2" align="center" class="tableh2">
-                        <span style="color:red"><strong>{$lang_login_php['err_login']}<strong></span>
-                        </td>
+                      <td colspan="2" align="center" class="tableh2">
+                          <span style="color:red"><strong>{$lang_login_php['err_login']}</strong></span>
+                      </td>
                   </tr>
 EOT;
 
         // get IP address of the person who tried to log in, look it up on the banning table and increase the brute force counter. If the brute force counter has reached a critical limit, set a regular banning record
-        $result = cpg_db_query("SELECT ban_id, brute_force FROM {$CONFIG['TABLE_BANNED']} WHERE ip_addr='$raw_ip' OR ip_addr='$hdr_ip' LIMIT 1");
+        $result = cpg_db_query("SELECT ban_id, brute_force FROM {$CONFIG['TABLE_BANNED']} WHERE ip_addr = '$raw_ip' OR ip_addr = '$hdr_ip' LIMIT 1");
         $failed_logon_counter = mysql_fetch_assoc($result);
         mysql_free_result($result);
-        $expiry_date = date("Y-m-d H:i:s", mktime(date('H'), date('i') + $CONFIG['login_expiry'], date('s'), date('m'), date('d'),date('Y')));
+
+        $expiry_date = date("Y-m-d H:i:s", mktime(date('H'), date('i') + $CONFIG['login_expiry'], date('s'), date('m'), date('d'), date('Y')));
 
         if ($failed_logon_counter['brute_force']) {
             $failed_logon_counter['brute_force'] = $failed_logon_counter['brute_force'] - 1;
-            $query_string = "UPDATE {$CONFIG['TABLE_BANNED']} SET brute_force='".$failed_logon_counter['brute_force']."',  expiry='".$expiry_date."' WHERE ban_id=".$failed_logon_counter['ban_id'];
+            $query_string = "UPDATE {$CONFIG['TABLE_BANNED']} SET brute_force = {$failed_logon_counter['brute_force']}, expiry = '$expiry_date' WHERE ban_id = {$failed_logon_counter['ban_id']}";
         } else {
             $failed_logon_counter['brute_force'] = $CONFIG['login_threshold'];
-            $query_string = "INSERT INTO {$CONFIG['TABLE_BANNED']} (ip_addr, expiry, brute_force) VALUES ('$raw_ip', '$expiry_date','".$failed_logon_counter['brute_force']."')";
+            $query_string = "INSERT INTO {$CONFIG['TABLE_BANNED']} (ip_addr, expiry, brute_force) VALUES ('$raw_ip', '$expiry_date', {$failed_logon_counter['brute_force']})";
         }
 
         //write the logon counter to the database
@@ -79,59 +89,62 @@ EOT;
     }
 }
 
-//if (!isset($_COOKIE[$CONFIG['cookie_name'] . '_data'])) {
 if (!$superCage->cookie->keyExists($CONFIG['cookie_name'] . '_data')) {
     $cookie_warning = <<<EOT
                   <tr>
-                          <td colspan="2" align="center" class="tableh2">
-                        <span style="color:red"><strong>{$lang_login_php['cookie_warning']}</strong></span>
-                        </td>
+                      <td colspan="2" align="center" class="tableh2">
+                          <span style="color:red"><strong>{$lang_login_php['cookie_warning']}</strong></span>
+                      </td>
                   </tr>
 
 EOT;
 }
 
 if ($CONFIG['reg_requires_valid_email'] == 1) {
-  $send_activation_link = '<br /><a href="send_activation.php" class="topmenu">'.$lang_login_php['send_activation_link'].'</a>';
+    $send_activation_link = '<br /><a href="send_activation.php" class="topmenu">'.$lang_login_php['send_activation_link'].'</a>';
 }
 
 pageheader($lang_login_php['login']);
+
 if ($superCage->get->getInt('force_login')) {
     msg_box($lang_login_php['force_login_title'], $lang_login_php['force_login']);
 }
+
 //$referer = urlencode($referer);
 $username_icon = cpg_fetch_icon('my_profile', 2);
 $password_icon = cpg_fetch_icon('key_enter', 2);
 $ok_icon = cpg_fetch_icon('ok', 2);
-echo '<form action="login.php?referer='.urlencode($CPG_REFERER).'" method="post" name="loginbox" id="cpgform">';
 
-starttable('-1', cpg_fetch_icon('login', 2) . $lang_login_php['enter_login_pswd'], 2);
+echo '<form action="login.php?referer=' . urlencode($CPG_REFERER) . '" method="post" name="loginbox" id="cpgform">';
+
+starttable(-1, cpg_fetch_icon('login', 2) . $lang_login_php['enter_login_pswd'], 2);
 
 //see how users are allowed to login, can be username, email address or both
 $login_method = $lang_login_php[$CONFIG['login_method']];
+
 echo <<< EOT
                   $login_failed
                   $cookie_warning
                   <tr>
-                        <td class="tableb" width="40%">{$username_icon}{$login_method}</td>
-                        <td class="tableb" width="60%"><input type="text" class="textinput" name="username" style="width: 100%" tabindex="1" /></td>
+                      <td class="tableb" width="40%">{$username_icon}{$login_method}</td>
+                      <td class="tableb" width="60%"><input type="text" class="textinput" name="username" style="width: 100%" tabindex="1" /></td>
                   </tr>
                   <tr>
-                          <td class="tableb">{$password_icon}{$lang_login_php['password']}</td>
-                        <td class="tableb"><input type="password" class="textinput" name="password" style="width: 100%" tabindex="2" /></td>
+                      <td class="tableb">{$password_icon}{$lang_login_php['password']}</td>
+                      <td class="tableb"><input type="password" class="textinput" name="password" style="width: 100%" tabindex="2" /></td>
                   </tr>
                   <tr>
-                    <td colspan="2" align="center" class="tableb">{$lang_login_php['remember_me']} <input name="remember_me" type="checkbox" class="checkbox" value="1" tabindex="3" /></td>
+                      <td colspan="2" align="center" class="tableb">{$lang_login_php['remember_me']} <input name="remember_me" type="checkbox" class="checkbox" value="1" tabindex="3" /></td>
                   </tr>
                   <tr>
-                    <td align="center" class="tablef">
-                      <a href="forgot_passwd.php" class="topmenu">{$lang_login_php['forgot_password_link']}</a>
-                      $send_activation_link
-                    </td>
-                    <td align="left" class="tablef">
-	                    <!--<input name="submitted" type="submit" class="button" value="{$lang_login_php['login']}" tabindex="4" />-->
-	                    <button type="submit" class="button" name="submitted" value="{$lang_common['ok']}"  tabindex="4">{$ok_icon}{$lang_common['ok']}</button>
-                    </td>
+                      <td align="center" class="tablef">
+                          <a href="forgot_passwd.php" class="topmenu">{$lang_login_php['forgot_password_link']}</a>
+                          $send_activation_link
+                      </td>
+                      <td align="left" class="tablef">
+                        <!--<input name="submitted" type="submit" class="button" value="{$lang_login_php['login']}" tabindex="4" />-->
+                        <button type="submit" class="button" name="submitted" value="{$lang_common['ok']}"  tabindex="4">{$ok_icon}{$lang_common['ok']}</button>
+                      </td>
                   </tr>
 
 EOT;
@@ -149,6 +162,5 @@ document.loginbox.username.focus();
 EOT;
 
 pagefooter();
-ob_end_flush();
 
 ?>
