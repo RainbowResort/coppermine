3da75e49124081a42aa1ad4271bbe804b2259def
nibbler999 <nibbler999@62f5abee-3c11-0410-b416-987344bf19e2>
2009-01-15 22:36:09 +0000

    Cleanup for coding standards

--- cpg1.5.x/include/exif_php.inc.php
+++ cpg1.5.x/include/exif_php.inc.php
@@ -17,91 +17,82 @@
   $Date$
 **********************************************/
 
+if (!defined('IN_COPPERMINE')) {
+    die('Not in Coppermine...');
+}
 
-
-if (!defined('IN_COPPERMINE')) die('Not in Coppermine...');
-
-define("EXIF_CACHE_FILE","exif.dat");
 require("include/exif.php");
 
 function exif_parse_file($filename, $pid)
 {
-        global $CONFIG, $lang_picinfo;
+    global $CONFIG, $lang_picinfo;
 
-        //String containing all the available exif tags.
-        $exif_info = "AFFocusPosition|Adapter|ColorMode|ColorSpace|ComponentsConfiguration|CompressedBitsPerPixel|Contrast|CustomerRender|DateTimeOriginal|DateTimedigitized|DigitalZoom|DigitalZoomRatio|ExifImageHeight|ExifImageWidth|ExifInteroperabilityOffset|ExifOffset|ExifVersion|ExposureBiasValue|ExposureMode|ExposureProgram|ExposureTime|FNumber|FileSource|Flash|FlashPixVersion|FlashSetting|FocalLength|FocusMode|GainControl|IFD1Offset|ISOSelection|ISOSetting|ISOSpeedRatings|ImageAdjustment|ImageDescription|ImageSharpening|LightSource|Make|ManualFocusDistance|MaxApertureValue|MeteringMode|Model|NoiseReduction|Orientation|Quality|ResolutionUnit|Saturation|SceneCaptureMode|SceneType|Sharpness|Software|WhiteBalance|YCbCrPositioning|xResolution|yResolution";
+    if (!is_readable($filename)) {
+        return false;
+    }
+    
+    $size = cpg_getimagesize($filename);
 
-        if (!is_readable($filename)) return false;
+    if ($size[2] != 2) {
+        return false; // Not a JPEG file
+    }
 
-        $size = cpg_getimagesize($filename);
-        if ($size[2] != 2) return false; // Not a JPEG file
+    //Check if we have the data of the said file in the table
+    $sql = "SELECT exifData FROM {$CONFIG['TABLE_EXIF']} WHERE pid = $pid";
 
-        $exifRawData = explode ("|",$exif_info);
-        $exifCurrentData = explode("|",$CONFIG['show_which_exif']);
+    $result = cpg_db_query($sql);
 
-        //Let's build the string of current exif values to be shown
-        $showExifStr = "";
-        foreach ($exifRawData as $key => $val) {
-          if ($exifCurrentData[$key] == 1) {
-            $showExifStr .= "|".$val;
-          }
-        }
+    if (mysql_num_rows($result) > 0) {
 
-        //Check if we have the data of the said file in the table
-        $sql = "SELECT exifData FROM {$CONFIG['TABLE_EXIF']} WHERE pid = $pid";
+        $row = mysql_fetch_assoc($result);
+        mysql_free_result($result);
+        $exifRawData = unserialize($row['exifData']);
+        
+    } else { // No data in the table - read it from the image file
+    
+        $exifRawData = read_exif_data_raw($filename, 0);
 
+        // Insert it into table for future reference
+        $sql = "INSERT INTO {$CONFIG['TABLE_EXIF']} (pid, exifData) VALUES ($pid, '".addslashes(serialize($exifRawData))."')";
         $result = cpg_db_query($sql);
+    }
 
-        if (mysql_num_rows($result) > 0){
-                $row = mysql_fetch_array($result);
-                mysql_free_result($result);
-                $exifRawData = unserialize($row["exifData"]);
-        } else { // No data in the table - read it from the image file
-          // Get the file name
-          $file = basename($filename);
-          // Get the path
-          $path = dirname($filename);
-          // Path to original file (without watermark)
-          $orig_file = $path.'/'.$CONFIG['orig_pfx'].$file;
+    $exif = array();
 
-          // Check whether original file exists
-          if (file_exists($orig_file)) {
-            // If original file is there then read exif data from it.
-            $exifRawData = read_exif_data_raw($orig_file,0);
-          } else {
-            $exifRawData = read_exif_data_raw($filename,0);
-          }
+    if (is_array($exifRawData['IFD0'])) {
+        $exif = array_merge($exif, $exifRawData['IFD0']);
+    }
+    
+    if (is_array($exifRawData['SubIFD'])) {
+        $exif = array_merge($exif, $exifRawData['SubIFD']);
+    }
+    
+    if (is_array($exifRawData['SubIFD']['MakerNote'])) {
+        $exif = array_merge($exif, $exifRawData['SubIFD']['MakerNote']);
+    }
 
-          // Insert it into table for future reference
-          $sql = "INSERT INTO {$CONFIG['TABLE_EXIF']} (pid, exifData) VALUES ($pid, '".addslashes(serialize($exifRawData))."')";
-          $result = cpg_db_query($sql);
-        }
-
-        $exif = array();
-
-        if (is_array($exifRawData['IFD0'])) {
-          $exif = array_merge ($exif,$exifRawData['IFD0']);
-        }
-        if (is_array($exifRawData['SubIFD'])) {
-          $exif = array_merge ($exif,$exifRawData['SubIFD']);
-        }
-        if (is_array($exifRawData['SubIFD']['MakerNote'])) {
-          $exif = array_merge ($exif,$exifRawData['SubIFD']['MakerNote']);
-        }
-
+    if (isset($exifRawData['IFD1OffSet'])) {
         $exif['IFD1OffSet'] = $exifRawData['IFD1OffSet'];
+    }
+    
+    $exifParsed = array();
 
-        $exifParsed = array();
-
-        foreach ($exif as $key => $val) {
-          if (strpos($showExifStr,"|".$key) && isset($val)){
-                $exifParsed[$lang_picinfo[$key]] = $val;
-                //$exifParsed[$key] = $val;
-          }
+    //String containing all the available exif tags.
+    $exif_info = "AFFocusPosition|Adapter|ColorMode|ColorSpace|ComponentsConfiguration|CompressedBitsPerPixel|Contrast|CustomerRender|DateTimeOriginal|DateTimedigitized|DigitalZoom|DigitalZoomRatio|ExifImageHeight|ExifImageWidth|ExifInteroperabilityOffset|ExifOffset|ExifVersion|ExposureBiasValue|ExposureMode|ExposureProgram|ExposureTime|FNumber|FileSource|Flash|FlashPixVersion|FlashSetting|FocalLength|FocusMode|GainControl|IFD1Offset|ISOSelection|ISOSetting|ISOSpeedRatings|ImageAdjustment|ImageDescription|ImageSharpening|LightSource|Make|ManualFocusDistance|MaxApertureValue|MeteringMode|Model|NoiseReduction|Orientation|Quality|ResolutionUnit|Saturation|SceneCaptureMode|SceneType|Sharpness|Software|WhiteBalance|YCbCrPositioning|xResolution|yResolution";
+    $exif_names = explode("|", $exif_info);
+    
+    $exifCurrentData = array_keys(array_filter(explode("|", $CONFIG['show_which_exif'])));
+    
+    foreach ($exifCurrentData as $i) { 
+        $name = $exif_names[$i];
+        if (isset($exif[$name])) {
+            $exifParsed[$lang_picinfo[$name]] = $exif[$name];
         }
+    }
 
-        ksort($exifParsed);
+    ksort($exifParsed);
 
-        return $exifParsed;
+    return $exifParsed;
 }
+
 ?>
