dff4fd8b57d00a03ac74de2bd7a2c77385ac9c9c
gaugau <gaugau@62f5abee-3c11-0410-b416-987344bf19e2>
2009-01-28 09:52:43 +0000

    Updated header to reflect new year

--- cpg1.4.x/getlang.php
+++ cpg1.4.x/getlang.php
@@ -2,7 +2,7 @@
 /*************************
   Coppermine Photo Gallery
   ************************
-  Copyright (c) 2003-2008 Dev Team
+  Copyright (c) 2003-2009 Coppermine Dev Team
   v1.1 originally written by Gregory DEMAR
 
   This program is free software; you can redistribute it and/or modify
