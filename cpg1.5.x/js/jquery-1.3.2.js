89c915cb24863bbe8f51bacfa2ead956cc2006b0
abbas-ali <abbas-ali@62f5abee-3c11-0410-b416-987344bf19e2>
2009-03-30 07:44:00 +0000

    - Added the cpg copyright notice

--- cpg1.5.x/js/jquery-1.3.2.js
+++ cpg1.5.x/js/jquery-1.3.2.js
@@ -1,3 +1,21 @@
+/*************************
+  Coppermine Photo Gallery
+  ************************
+  Copyright (c) 2003-2009 Coppermine Dev Team
+  v1.1 originally written by Gregory DEMAR
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License version 3
+  as published by the Free Software Foundation.
+
+  ********************************************
+  Coppermine version: 1.5.1
+  $HeadURL: https://coppermine.svn.sourceforge.net/svnroot/coppermine/trunk/cpg1.5.x/js/jquery.js $
+  $Revision: 5576 $
+  $LastChangedBy: gaugau $
+  $Date: 2009-01-05 11:31:56 +0530 (Mon, 05 Jan 2009) $
+**********************************************/
+
 /*
  * jQuery JavaScript Library v1.3.2
  * http://jquery.com/
