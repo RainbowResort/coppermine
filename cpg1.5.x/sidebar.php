36b0eeec8a75784bcd9fd0e2f63dde598bc017a5
nibbler999 <nibbler999@62f5abee-3c11-0410-b416-987344bf19e2>
2009-06-02 10:40:14 +0000

    Correct filename

--- cpg1.5.x/sidebar.php
+++ cpg1.5.x/sidebar.php
@@ -31,14 +31,14 @@ if (USER_ID) {
   // the visitor is logged in
   if ($CONFIG['display_sidebar_user'] == 0 && !GALLERY_ADMIN_MODE) {
     if ($CONFIG['log_mode'] != 0) {
-            log_write('Denied privileged access to stat_details.php for user '.$USER_DATA['user_name'].' at ' . $hdr_ip .' on '.date("F j, Y, g:i a"),CPG_SECURITY_LOG);
+            log_write('Denied privileged access to sidebar.php for user '.$USER_DATA['user_name'].' at ' . $hdr_ip .' on '.date("F j, Y, g:i a"),CPG_SECURITY_LOG);
     }
     cpg_die($lang_common['error'], $lang_errors['access_denied'], __FILE__, __LINE__);
   }
 } else { // the visitor is not logged in
   if ($CONFIG['display_sidebar_guest'] == 0) {
     if ($CONFIG['log_mode'] != 0) {
-            log_write('Denied privileged access to stat_details.php for user '.$USER_DATA['user_name'].' at ' . $hdr_ip .' on '.date("F j, Y, g:i a"),CPG_SECURITY_LOG);
+            log_write('Denied privileged access to sidebar.php for user '.$USER_DATA['user_name'].' at ' . $hdr_ip .' on '.date("F j, Y, g:i a"),CPG_SECURITY_LOG);
     }
     cpg_die($lang_common['error'], $lang_errors['access_denied'], __FILE__, __LINE__);
   }
