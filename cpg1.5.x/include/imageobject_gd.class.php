adf319f5bac213707cafd5e197b2d47ce356e75f
nibbler999 <nibbler999@62f5abee-3c11-0410-b416-987344bf19e2>
2009-06-12 19:57:26 +0000

    Cleanup imageobject classes, fix rotate on IM

--- cpg1.5.x/include/imageobject_gd.class.php
+++ cpg1.5.x/include/imageobject_gd.class.php
@@ -17,137 +17,175 @@
   $Date$
 **********************************************/
 
-class imageObject{
-         // image resource
-         var $imgRes;
-         // px
-         var $height=0;
-         var $width=0;
-         // for img height/width tags
-         var $string;
-         // output report or error message
-         var $message;
-         // file + dir
-         var $directory;
-         var $filename;
-         // output quality, 0 - 100
-         var $quality;
-         // truecolor available, boolean
-         var $truecolor;
+class imageObject {
+    
+    // image resource
+    var $imgRes;
+    
+    // px
+    var $height = 0;
+    var $width = 0;
+    
+    // for img height/width tags
+    var $string;
+    
+    // output report or error message
+    var $message;
+    
+    // file + dir
+    var $directory;
+    var $filename;
+    
+    // output quality, 0 - 100
+    var $quality;
+    
+    // truecolor available, boolean
+    var $truecolor;
 
-         //constructor
-         function imageObject($directory,$filename,$previous=null)
-        {
+    //constructor
+    function imageObject($directory, $filename, $previous = null) {
+
         $this->directory = $directory;
-        $this->filename = $filename;
-        $this->previous = $previous;
-        $this->imgRes = $previous->imgRes;
-        if (file_exists($directory.$filename)){
-                        $this->filesize = round(filesize($directory.$filename)/1000);
-                        if($this->filesize>0){
-                                $size = @GetImageSize($directory.$filename);
-                                if ($size && !$this->imgRes) {
-                                        $this->imgRes = $this->getimgRes($directory.$filename,$size[2]);
-                                }
-                                if (function_exists("imagecreatetruecolor")){
-                                        $this->truecolor = true;
-                                }
-                                $this->width = $size[0];
-                                $this->height = $size[1];
-                                $this->string = $size[3];
-                                }
-                        }// if
-        }// constructor
+        $this->filename  = $filename;
+        $this->previous  = $previous;
+        $this->imgRes    = $previous->imgRes;
 
-         // private methods
-         function getimgRes($name,&$type)
-         {
-           switch ($type){
-              case 1:
-              $im = imagecreatefromgif($name);
-              break;
-              case 2:
-              $im = imagecreatefromjpeg($name);
-              break;
-              case 3:
-              $im = imagecreatefrompng($name);
-              break;
+        if (file_exists($directory . $filename)) {
+            
+            $this->filesize = round(filesize($directory . $filename) / 1000);
+            
+            if ($this->filesize > 0) {
+                
+                $size = cpg_getimagesize($directory . $filename);
+                
+                if ($size && !$this->imgRes) {
+                    $this->imgRes = $this->getimgRes($directory . $filename, $size[2]);
+                }
+                
+                if (function_exists('imagecreatetruecolor')) {
+                    $this->truecolor = true;
+                }
+                
+                $this->width  = $size[0];
+                $this->height = $size[1];
+                $this->string = $size[3];
             }
-           return $im;
-         }
+        }
+    }
 
-         function createUnique(&$imgnew)
-         {
-           srand((double)microtime()*100000);
-           $unique_str = "temp_".md5(rand(0,999999)).".jpg";
-           @imagejpeg($imgnew,$this->directory.$unique_str,$this->quality);
-           @imagedestroy($this->imgRes);
-           //Don't clutter with old images
-           @unlink($this->directory.$this->filename);
-           //Create a new ImageObject
-           return new imageObject($this->directory,$unique_str,$imgnew);
-         }
+    // private methods
+    function getimgRes($name, $type) {
 
-         function createImage($new_w,$new_h)
-         {
-           if (function_exists("imagecreatetruecolor")){
-             $retval = @imagecreatetruecolor($new_w,$new_h);
-           }
-           if (!$retval) $retval = imagecreate($new_w,$new_h);
-           return $retval;
-         }
+        switch ($type) {
+            
+        case 1:
+            $im = imagecreatefromgif($name);
+            break;
+         
+        case 2:
+            $im = imagecreatefromjpeg($name);
+            break;
+     
+        case 3:
+            $im = imagecreatefrompng($name);
+            break;
+        }
 
-         function cropImage(&$clipval)
-         {
-             $cliparray = split(",",$clipval);
-             $clip_top = $cliparray[0];
-             $clip_right = $cliparray[1];
-             $clip_bottom = $cliparray[2];
-             $clip_left = $cliparray[3];
-             $new_w = $clip_right - $clip_left;
-             $new_h = $clip_bottom - $clip_top;
-             $dst_img = $this->createImage($new_w,$new_h);
-             $result = @imagecopyresampled($dst_img, $this->imgRes, 0,0,$clip_left, $clip_top,$new_w, $new_h, $new_w, $new_h);
-             if (!$result) $result = @imagecopyresized($dst_img, $this->imgRes, 0,0,$clip_left, $clip_top,$new_w, $new_h, $new_w, $new_h);
-             return $this->createUnique($dst_img);
-         }
+        return $im;
+    }
 
-         function rotateImage(&$angle){
-          if ($angle == 180){
-              $dst_img = @imagerotate($this->imgRes, $angle, 0);
-          } else {
-                  $width = imagesx($this->imgRes);
-                  $height = imagesy($this->imgRes);
-                  if ($width > $height){
-                      $size = $width;
-                      } else {
-                      $size = $height;
-                  }
-                  $dst_img = $this->createImage($size, $size);
-                  imagecopy($dst_img, $this->imgRes, 0, 0, 0, 0, $width, $height);
-                  $dst_img = @imagerotate($dst_img, $angle, 0);
-                  $this->imgRes = $dst_img;
-                  $dst_img = $this->createImage($height, $width);
-                  if ((($angle == 90) && ($width > $height)) || (($angle == 270) && ($width < $height))){
-                          imagecopy($dst_img, $this->imgRes, 0, 0, 0, 0, $size, $size);
-                  }
-                  if ((($angle == 270) && ($width > $height)) || (($angle == 90) && ($width < $height))){
-                          imagecopy($dst_img, $this->imgRes, 0, 0, $size - $height, $size - $width, $size, $size);
-                  }
-          }
-           return $this->createUnique($dst_img);
-         }
+    function createUnique($imgnew) {
+        
+        $unique_str = 'temp_' . md5(rand(0, 999999)) . '.jpg';
+        
+        imagejpeg($imgnew, $this->directory . $unique_str, $this->quality);
+        imagedestroy($this->imgRes);
+        
+        // Don't clutter with old images
+        @unlink($this->directory . $this->filename);
+       
+        // Create a new ImageObject
+        return new imageObject($this->directory, $unique_str, $imgnew);
+    }
 
-         function resizeImage($new_w=0,$new_h=0){
-             $dst_img = $this->createImage($new_w,$new_h);
-             $result = @imagecopyresampled($dst_img, $this->imgRes, 0, 0, 0, 0, $new_w, $new_h, $this->width,$this->height);
-             if (!$result) $result = @imagecopyresized($dst_img, $this->imgRes, 0, 0, 0, 0, $new_w, $new_h, $this->width,$this->height);
-             return $this->createUnique($dst_img);
-         }
+    function createImage($new_w, $new_h) {
+        
+        if (function_exists('imagecreatetruecolor')) {
+            $retval = imagecreatetruecolor($new_w, $new_h);
+        }
+        
+        if (!$retval) {
+            $retval = imagecreate($new_w, $new_h);
+        }
+        
+        return $retval;
+    }
 
+    function cropImage($clipval) {
+        
+        $cliparray = split(",",$clipval);
+        
+        $clip_top = $cliparray[0];
+        $clip_right = $cliparray[1];
+        $clip_bottom = $cliparray[2];
+        $clip_left = $cliparray[3];
+        
+        $new_w = $clip_right - $clip_left;
+        $new_h = $clip_bottom - $clip_top;
+        
+        $dst_img = $this->createImage($new_w, $new_h);
+        
+        $result = imagecopyresampled($dst_img, $this->imgRes, 0, 0, $clip_left, $clip_top, $new_w, $new_h, $new_w, $new_h);
+        
+        if (!$result) {
+            $result = imagecopyresized($dst_img, $this->imgRes, 0, 0, $clip_left, $clip_top, $new_w, $new_h, $new_w, $new_h);
+        }
+        
+        return $this->createUnique($dst_img);
+    }
 
-         function saveImage(){
-         }
+    function rotateImage($angle) {
+             
+        if ($angle == 180){
+            $dst_img = imagerotate($this->imgRes, $angle, 0);
+        } else {
+            $width = imagesx($this->imgRes);
+            $height = imagesy($this->imgRes);
+            
+            if ($width > $height) {
+                $size = $width;
+            } else {
+                $size = $height;
+            }
+            
+            $dst_img = $this->createImage($size, $size);
+            imagecopy($dst_img, $this->imgRes, 0, 0, 0, 0, $width, $height);
+            $dst_img = imagerotate($dst_img, $angle, 0);
+            $this->imgRes = $dst_img;
+            $dst_img = $this->createImage($height, $width);
+            
+            if ((($angle == 90) && ($width > $height)) || (($angle == 270) && ($width < $height))) {
+                imagecopy($dst_img, $this->imgRes, 0, 0, 0, 0, $size, $size);
+            }
+            
+            if ((($angle == 270) && ($width > $height)) || (($angle == 90) && ($width < $height))){
+                imagecopy($dst_img, $this->imgRes, 0, 0, $size - $height, $size - $width, $size, $size);
+            }
+        }
 
-   }
- ?>
+        return $this->createUnique($dst_img);
+    }
+
+    function resizeImage($new_w = 0, $new_h = 0) {
+        
+        $dst_img = $this->createImage($new_w, $new_h);
+        
+        $result = imagecopyresampled($dst_img, $this->imgRes, 0, 0, 0, 0, $new_w, $new_h, $this->width, $this->height);
+        
+        if (!$result) {
+            $result = @imagecopyresized($dst_img, $this->imgRes, 0, 0, 0, 0, $new_w, $new_h, $this->width,$this->height);
+        }
+    
+        return $this->createUnique($dst_img);
+    }
+}
--- cpg1.5.x/include/imageobject_im.class.php
+++ cpg1.5.x/include/imageobject_im.class.php
@@ -17,125 +17,158 @@
   $Date$
 **********************************************/
 
-class imageObject{
-         // image resource
-         var $imgRes;
-         // px
-         var $height=0;
-         var $width=0;
-         // for img height/width tags
-         var $string;
-         // output report or error message
-         var $message;
-         // file + dir
-         var $directory;
-         var $filename;
-         // output quality, 0 - 100
-         var $quality;
+class imageObject {
+    
+    // image resource
+    var $imgRes;
+    
+    // px
+    var $height = 0;
+    var $width = 0;
+    
+    // for img height/width tags
+    var $string;
+     
+    // output report or error message
+    var $message;
+    
+    // file + dir
+    var $directory;
+    var $filename;
+    
+    // output quality, 0 - 100
+    var $quality;
 
-         //constructor
-         function imageObject($directory,$filename,$previous=null)
-        {
+    //constructor
+    function imageObject($directory, $filename, $previous = null) {
+        
         $this->directory = $directory;
-        $this->filename = $filename;
-        $this->previous = $previous;
+        $this->filename  = $filename;
+        $this->previous  = $previous;
         $this->truecolor = true;
 
-        if (file_exists($directory.$filename)){
-                        $this->filesize = round(filesize($directory.$filename)/1000);
-                        if($this->filesize>0){
-                                $size = @GetImageSize($directory.$filename);
-                                // For IM we don't need an Image Resource (work directly on file :)
-                                if ($size && !$this->imgRes) {
-                                        $this->imgRes = true;
-                                }
-                                $this->width = $size[0];
-                                $this->height = $size[1];
-                                $this->string = $size[3];
-                                }
-                        }// if
-        }// constructor
+        if (file_exists($directory.$filename)) {
+            
+            $this->filesize = round(filesize($directory . $filename) / 1000);
+            
+            if ($this->filesize > 0) {
+                
+                $size = cpg_getimagesize($directory . $filename);
+                
+                // For IM we don't need an Image Resource (work directly on file :)
+                if ($size && !$this->imgRes) {
+                    $this->imgRes = true;
+                }
+                
+                $this->width  = $size[0];
+                $this->height = $size[1];
+                $this->string = $size[3];
+            }
+        }
+    }
 
-         function cropImage(&$clipval)
-         {
-             global $CONFIG;
-             $cliparray = split(",",$clipval);
-             $clip_top = $cliparray[0];
-             $clip_right = $cliparray[1];
-             $clip_bottom = $cliparray[2];
-             $clip_left = $cliparray[3];
-             $new_w = $clip_right - $clip_left;
-             $new_h = $clip_bottom - $clip_top;
-             $imgFile = escapeshellarg("$this->directory$this->filename");
-             $output = array();
-                                 /*
-                                * Hack for working with ImageMagick on WIndows even if IM is installed in C:\Program Files.
-                                * Also the options for -crop should not have space in between them.
-                                * By Aditya Mooley <aditya@sanisoft.com>
-                                */
-                                if ($superCage->env->getMatched('OS', '/win/i')) {
-                                    $imgFile = str_replace("'","\"" ,$imgFile );
-                                         $cmd = "\"".str_replace("\\","/", $CONFIG['impath'])."convert\" -quality {$this->quality} {$CONFIG['im_options']} -crop {$new_w}x{$new_h}+{$clip_left}+{$clip_top} ".str_replace("\\","/" ,$imgFile )." ".str_replace("\\","/" ,$imgFile );
-                                         exec ("\"$cmd\"", $output, $retval);
-                                } else {
-                                    $cmd = "{$CONFIG['impath']}convert -quality {$this->quality} {$CONFIG['im_options']} -crop {$new_w}x{$new_h}+{$clip_left}+{$clip_top} $imgFile $imgFile";
-                                         exec ($cmd, $output, $retval);
-                                }
-                                 //$cmd = "{$CONFIG['impath']}convert -quality {$this->quality} {$CONFIG['im_options']} -crop '{$new_w}x{$new_h} +{$clip_left} +{$clip_top}' $imgFile $imgFile";
-             //exec ($cmd, $output, $retval);
-             //To Do check for errors in execution etc
-                 // Call the constructor again to repopulate the dimensions etc
-             $this->imageObject($this->directory,$this->filename);
-             return $this;
-         }
+    function cropImage($clipval) {
+        
+        global $CONFIG;
+         
+        $superCage = Inspekt::makeSuperCage();
+         
+        $cliparray = split(",",$clipval);
+        
+        $clip_top = $cliparray[0];
+        $clip_right = $cliparray[1];
+        $clip_bottom = $cliparray[2];
+        $clip_left = $cliparray[3];
+        
+        $new_w = $clip_right - $clip_left;
+        $new_h = $clip_bottom - $clip_top;
+        
+        $imgFile = escapeshellarg("$this->directory$this->filename");
+        $output = array();
+        
+        /*
+        * Hack for working with ImageMagick on WIndows even if IM is installed in C:\Program Files.
+        * Also the options for -crop should not have space in between them.
+        * By Aditya Mooley <aditya@sanisoft.com>
+        */
+        
+        if ($superCage->env->getMatched('OS', '/win/i')) {
+            $imgFile = str_replace("'","\"" , $imgFile);
+            $cmd = "\"".str_replace("\\","/", $CONFIG['impath'])."convert\" -quality {$this->quality} {$CONFIG['im_options']} -crop {$new_w}x{$new_h}+{$clip_left}+{$clip_top} ".str_replace("\\","/" ,$imgFile )." ".str_replace("\\","/" ,$imgFile );
+            exec ("\"$cmd\"", $output, $retval);
+        } else {
+            $cmd = "{$CONFIG['impath']}convert -quality {$this->quality} {$CONFIG['im_options']} -crop {$new_w}x{$new_h}+{$clip_left}+{$clip_top} $imgFile $imgFile";
+            exec($cmd, $output, $retval);
+        }
 
-         function rotateImage(&$angle){
-             global $CONFIG;
-             $imgFile = escapeshellarg("$this->directory$this->filename");
-             $output = array();
-                                 /*
-                                * Hack for working with ImageMagick on Windows even if IM is installed in C:\Program Files.
-                                * By Aditya Mooley <aditya@sanisoft.com>
-                                */
-                                if ($superCage->env->getMatched('OS', '/win/i')) {
-                                    $imgFile = str_replace("'","\"" ,$imgFile );
-                                         $cmd = "\"".str_replace("\\","/", $CONFIG['impath'])."convert\" -quality {$this->quality} {$CONFIG['im_options']} -rotate $angle ".str_replace("\\","/" ,$imgFile )." ".str_replace("\\","/" ,$imgFile );
-                                         exec ("\"$cmd\"", $output, $retval);
-                                } else {
-                                    $cmd = "{$CONFIG['impath']}convert -quality {$this->quality} {$CONFIG['im_options']} -rotate $angle $imgFile $imgFile";
-                                         exec ($cmd, $output, $retval);
-                                }
-             //$cmd = "{$CONFIG['impath']}convert -quality {$this->quality} {$CONFIG['im_options']} -rotate '$angle' $imgFile $imgFile";
-             //exec ($cmd, $output, $retval);
-             //To Do check for errors in execution etc
-             // Call the constructor again to repopulate the dimensions etc
-             $this->imageObject($this->directory,$this->filename);
-             return $this;
-         }
+        // To Do check for errors in execution etc
+        
+        // Call the constructor again to repopulate the dimensions etc
+        $this->imageObject($this->directory, $this->filename);
+        
+        return $this;
+    }
 
+     function rotateImage($angle) {
+         
+        global $CONFIG;
+        
+        $superCage = Inspekt::makeSuperCage();
+         
+        $imgFile = escapeshellarg("$this->directory$this->filename");
+        $output = array();
 
-         function resizeImage($new_w=0,$new_h=0){
-             global $CONFIG;
-                                 $imgFile = escapeshellarg("$this->directory$this->filename");
-             $output = array();
-                                 /*
-                                * Hack for working with ImageMagick on Windows even if IM is installed in C:\Program Files.
-                                * By Aditya Mooley <aditya@sanisoft.com>
-                                */
-                                if ($superCage->env->getMatched('OS', '/win/i')) {
-                                    $imgFile = str_replace("'","\"" ,$imgFile );
-                                         $cmd = "\"".str_replace("\\","/", $CONFIG['impath'])."convert\" -quality {$this->quality} {$CONFIG['im_options']} -geometry {$new_w}x{$new_h} ".str_replace("\\","/" ,$imgFile )." ".str_replace("\\","/" ,$imgFile );
-                                         exec ("\"$cmd\"", $output, $retval);
-                                } else {
-                                    $cmd = "{$CONFIG['impath']}convert -quality {$this->quality} {$CONFIG['im_options']} -geometry {$new_w}x{$new_h} $imgFile $imgFile";
-                                         exec ($cmd, $output, $retval);
-                                }
-            //$cmd = "{$CONFIG['impath']}convert -quality {$this->quality} {$CONFIG['im_options']} -geometry '{$new_w}x{$new_h}' $imgFile $imgFile";
-            //exec ($cmd, $output, $retval);
-             //To Do check for errors in execution etc
-             // Call the constructor again to repopulate the dimensions etc
-             $this->imageObject($this->directory,$this->filename);
-             return $this;
-         }
-   }
- ?>
+        /*
+        * Hack for working with ImageMagick on Windows even if IM is installed in C:\Program Files.
+        * By Aditya Mooley <aditya@sanisoft.com>
+        */
+        
+        if ($superCage->env->getMatched('OS', '/win/i')) {
+            $imgFile = str_replace("'","\"" ,$imgFile );
+            $cmd = "\"".str_replace("\\","/", $CONFIG['impath'])."convert\" -quality {$this->quality} {$CONFIG['im_options']} -rotate $angle ".str_replace("\\","/" ,$imgFile )." ".str_replace("\\","/" ,$imgFile );
+            exec("\"$cmd\"", $output, $retval);
+        } else {
+            $cmd = "{$CONFIG['impath']}convert -quality {$this->quality} {$CONFIG['im_options']} -rotate $angle $imgFile $imgFile";
+            exec($cmd, $output, $retval);
+        }
+        
+        // To Do check for errors in execution etc
+        
+        // Call the constructor again to repopulate the dimensions etc
+        $this->imageObject($this->directory, $this->filename);
+
+        return $this;
+    }
+
+     function resizeImage($new_w = 0, $new_h = 0) {
+
+        global $CONFIG;
+        
+        $superCage = Inspekt::makeSuperCage();
+        
+        $imgFile = escapeshellarg("$this->directory$this->filename");
+        
+        $output = array();
+        
+        /*
+        * Hack for working with ImageMagick on Windows even if IM is installed in C:\Program Files.
+        * By Aditya Mooley <aditya@sanisoft.com>
+        */
+        
+        if ($superCage->env->getMatched('OS', '/win/i')) {
+            $imgFile = str_replace("'","\"" ,$imgFile );
+            $cmd = "\"".str_replace("\\","/", $CONFIG['impath'])."convert\" -quality {$this->quality} {$CONFIG['im_options']} -geometry {$new_w}x{$new_h} ".str_replace("\\","/" ,$imgFile )." ".str_replace("\\","/" ,$imgFile );
+            exec("\"$cmd\"", $output, $retval);
+        } else {
+            $cmd = "{$CONFIG['impath']}convert -quality {$this->quality} {$CONFIG['im_options']} -geometry {$new_w}x{$new_h} $imgFile $imgFile";
+            exec($cmd, $output, $retval);
+        }
+
+        // To Do check for errors in execution etc
+        
+        // Call the constructor again to repopulate the dimensions etc
+        $this->imageObject($this->directory, $this->filename);
+
+        return $this;
+    }
+}
