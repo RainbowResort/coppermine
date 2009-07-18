c63dd8eeaf2c631b3f1b430f6afd24d6ef994bdb
nibbler999 <nibbler999@62f5abee-3c11-0410-b416-987344bf19e2>
2009-07-18 14:44:35 +0000

    Wrap the js_vars in CDATA to aid validation

--- cpg1.5.x/themes/sample/theme.php
+++ cpg1.5.x/themes/sample/theme.php
@@ -1651,26 +1651,36 @@ EOT;
 ** Section <<<theme_javascript_head>>> - START
 ******************************************************************************/
 // Function for the JavaScript inside the <head>-section
-function theme_javascript_head() {
-    global $CONFIG, $JS, $LINEBREAK;
+function theme_javascript_head()
+{
+    global $JS, $LINEBREAK;
 
-  $return = '';
-  // Check if we have any variables being set using set_js_vars function
-  if (isset($JS['vars']) && count($JS['vars'])) {
-    // Convert the $JS['vars'] array to json object string
-    $json_vars = json_encode($JS['vars']);
-    // Output the json object
-    $return .= "<script type=\"text/javascript\">var js_vars = $json_vars;</script>" . $LINEBREAK;
-  }
+    $return = '';
+    
+    // Check if we have any variables being set using set_js_vars function
+    if (!empty($JS['vars'])) {
+        // Convert the $JS['vars'] array to json object string
+        $json_vars = json_encode($JS['vars']);
+        // Output the json object
+        $return = <<< EOT
+<script type="text/javascript">
+/* <![CDATA[ */
+    var js_vars = $json_vars;
+/* ]]> */
+</script>
 
-  // Check if we have any js includes
-  if (isset($JS['includes']) && count($JS['includes'])) {
-    // Include all the file which were set using js_include() function
-    foreach ($JS['includes'] as $js_file) {
-      $return .= '<script type="text/javascript" src="' . $js_file . '"></script>' . $LINEBREAK;
+EOT;
     }
-  }
-  return $return;
+
+    // Check if we have any js includes
+    if (!empty($JS['includes'])) {
+        // Include all the files which were set using js_include() function
+        foreach ($JS['includes'] as $js_file) {
+            $return .= js_include($js_file, true) . $LINEBREAK;
+        }
+    }
+    
+    return $return;
 }
 /******************************************************************************
 ** Section <<<theme_javascript_head>>> - END
