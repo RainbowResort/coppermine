553206f618a1e1f5389cf2b3fefd8d25a34bc5d1
gaugau <gaugau@62f5abee-3c11-0410-b416-987344bf19e2>
2009-08-18 08:58:16 +0000

    Inspectification (work in progress)

---
+++ plugins/mass_import/configuration.php
@@ -0,0 +1,28 @@
+<?php
+/*************************
+  mass_import Plugin for cpg1.5.x
+  ************************
+  Copyright (c) 2003-2009 Coppermine Dev Team
+  v1.1 originally written by Gregory DEMAR
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License version 3
+  as published by the Free Software Foundation.
+
+  ********************************************
+  Coppermine version: 1.5.x
+  $HeadURL$
+  $Revision$
+  $LastChangedBy$
+  $Date$
+**********************************************/
+
+$name = 'Mass Import';
+$description = 'Mass Import gives the admin the ability to import large numbers of pictures organized by directory structure.';
+$author = '<a href="http://forum.coppermine-gallery.net/index.php?action=profile;u=941">Nibbler</a>, ';
+$author.= '<a href="http://forum.coppermine-gallery.net/index.php?action=profile;u=10059">Donnoman</a> (<a href="http://cpg-contrib.org">cpg-contrib.org</a>), ';
+$author.= '<a href="http://forum.coppermine-gallery.net/index.php?action=profile;u=15025">Flux</a>, ';
+$author.= '<a href="http://forum.coppermine-gallery.net/index.php?action=profile;u=23938">Paul Van Rompay</a>, ';
+$author.= '<a href="http://forum.coppermine-gallery.net/index.php?action=profile;u=2">Joachim Müller</a>';
+$version = '3.0';
+?>
