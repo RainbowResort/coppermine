87f46c9e8cde61fcd8acdde3c5d443cc669c61d2
gaugau <gaugau@62f5abee-3c11-0410-b416-987344bf19e2>
2009-10-02 11:16:08 +0000

    Renamed index.html to index.php to reduce the number of extensions.

--- cpg1.5.x/albums/userpics/index.php
+++ cpg1.5.x/albums/userpics/index.php
@@ -1,61 +1,26 @@
-<?php
-/*************************
-  Coppermine Photo Gallery
-  ************************
-  Copyright (c) 2003-2009 Coppermine Dev Team
-  v1.1 originally written by Gregory DEMAR
-
-  This program is free software; you can redistribute it and/or modify
-  it under the terms of the GNU General Public License version 3
-  as published by the Free Software Foundation.
-
-  ********************************************
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+   "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en" dir="ltr">
+<head>
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<meta http-equiv="refresh" content="0; url=../../index.php">
+<title>Coppermine Photo Gallery - Userpics Folder</title>
+<link rel="stylesheet" href="../../css/installer.css" type="text/css" />
+<link rel="shortcut icon" href="../../favicon.ico" />
+<!--
+  SVN version info:
   Coppermine version: 1.5.2
   $HeadURL$
   $Revision$
   $LastChangedBy$
   $Date$
-**********************************************/
-?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="content-type" content="text/html; charset=utf-8" />
-<meta http-equiv="refresh" content="0; url=../../index.php">
-<title>Coppermine Photo Gallery - Userpics Folder</title>
-<style type="text/css">
-<!--
-body {
-        font-family : Verdana, Arial, Helvetica, sans-serif;
-        font-size: 12px;
-        background : #F7F7F7 ;
-        color : Black;
-        margin: 20px;
-}
-
-h1{
-        font-weight: bold;
-        font-size: 22px;
-        font-family: "Trebuchet MS", Verdana, Arial, Helvetica, sans-serif;
-        text-decoration: none;
-        line-height : 120%;
-        color : #000000;
-}
-
-p {
-        font-family : Verdana, Arial, Helvetica, sans-serif;
-        font-size: 12px;
-        margin: 10px 10px 0px 0px;
-}
-
 -->
-</style>
 </head>
-
 <body>
-<img src="../../images/coppermine-logo.png" width="260" height="60" />
+<img src="../images/coppermine-logo.png" width="260" height="60" border="0" alt="" title="Coppermine Photo Gallery" />
 <h1>Coppermine Photo Gallery - Userpics Folder</h1>
 <p align="center">The contents of this folder aren't meant to be browsed. Visit the Coppermine Photo Gallery instead - you'll be redirected.
-If you don't want to wait (or your browser doesn't support redirect), click <a href="../../index.php">here</a>.<br />If you're the gallery admin: do not ftp-upload to this folder - refer to the documentation where to upload instead.</p>
-
+If you don't want to wait (or your browser doesn't support redirect), click <a href="../../index.php">here</a>.</p>
+<p>If you're the gallery admin: do not ftp-upload to this folder - refer to the documentation where to upload instead.</p>
 </body>
 </html>
