6b19d745c061608c0329189016669d931ec42ffc
eenemeenemuu <eenemeenemuu@62f5abee-3c11-0410-b416-987344bf19e2>
2009-10-09 06:13:15 +0000

    Replaced sort icons as suggested in thread 62080.

--- cpg1.5.x/js/thumbnails.js
+++ cpg1.5.x/js/thumbnails.js
@@ -20,33 +20,33 @@ $(document).ready(function() {
 });
 
 function addSortOptions(){
-	var extra_att = '';
-	var sortable = '';
-	// Change the separator to '<br />' for vertical stacking
-	var separator = '&nbsp;&bull;&nbsp;';
-	sortable += '<span class="sortorder_options">';
-	sortable += js_vars.sort_vars.sort_title;
-	sortable += '</span>';
-	sortable += '&nbsp;';
-	sortable += '<span class="statlink">';
-	sortable += '<a href="thumbnails.php?album=' + js_vars.sort_vars.aid + '&amp;page=' + js_vars.sort_vars.page + '&amp;sort=ta" title="' + js_vars.sort_vars.sort_ta + '">+</a>';
-	sortable += '</span>';
-	sortable += '&nbsp;';
-	sortable += '<span class="statlink">';
-	sortable += '<a href="thumbnails.php?album=' + js_vars.sort_vars.aid + '&amp;page=' + js_vars.sort_vars.page + '&amp;sort=td" title="' + js_vars.sort_vars.sort_td + '">-</a>';
-	sortable += '</span>';
-	sortable += separator;
-	
+    var extra_att = '';
+    var sortable = '';
+    // Change the separator to '<br />' for vertical stacking
+    var separator = '&nbsp;&bull;&nbsp;';
+    sortable += '<span class="sortorder_options">';
+    sortable += js_vars.sort_vars.sort_title;
+    sortable += '</span>';
+    sortable += '&nbsp;';
+    sortable += '<span class="statlink">';
+    sortable += '<a href="thumbnails.php?album=' + js_vars.sort_vars.aid + '&amp;page=' + js_vars.sort_vars.page + '&amp;sort=ta" title="' + js_vars.sort_vars.sort_ta + '"><img src="images/ascending.gif" width="9" height="9" border="0" alt="+" /></a>';
+    sortable += '</span>';
+    sortable += '&nbsp;';
+    sortable += '<span class="statlink">';
+    sortable += '<a href="thumbnails.php?album=' + js_vars.sort_vars.aid + '&amp;page=' + js_vars.sort_vars.page + '&amp;sort=td" title="' + js_vars.sort_vars.sort_td + '"><img src="images/descending.gif" width="9" height="9" border="0" alt="-" /></a>';
+    sortable += '</span>';
+    sortable += separator;
+    
     sortable += '<span class="sortorder_options">';
     sortable += js_vars.sort_vars.sort_name;
     sortable += '</span>';
     sortable += '&nbsp;';
     sortable += '<span class="statlink">';
-    sortable += '<a href="thumbnails.php?album=' + js_vars.sort_vars.aid + '&amp;page=' + js_vars.sort_vars.page + '&amp;sort=na" title="' + js_vars.sort_vars.sort_na + '">+</a>';
+    sortable += '<a href="thumbnails.php?album=' + js_vars.sort_vars.aid + '&amp;page=' + js_vars.sort_vars.page + '&amp;sort=na" title="' + js_vars.sort_vars.sort_na + '"><img src="images/ascending.gif" width="9" height="9" border="0" alt="+" /></a>';
     sortable += '</span>';
     sortable += '&nbsp;';
     sortable += '<span class="statlink">';
-    sortable += '<a href="thumbnails.php?album=' + js_vars.sort_vars.aid + '&amp;page=' + js_vars.sort_vars.page + '&amp;sort=nd" title="' + js_vars.sort_vars.sort_nd + '">-</a>';
+    sortable += '<a href="thumbnails.php?album=' + js_vars.sort_vars.aid + '&amp;page=' + js_vars.sort_vars.page + '&amp;sort=nd" title="' + js_vars.sort_vars.sort_nd + '"><img src="images/descending.gif" width="9" height="9" border="0" alt="-" /></a>';
     sortable += '</span>';
     sortable += separator;
     
@@ -55,11 +55,11 @@ function addSortOptions(){
     sortable += '</span>';
     sortable += '&nbsp;';
     sortable += '<span class="statlink">';
-    sortable += '<a href="thumbnails.php?album=' + js_vars.sort_vars.aid + '&amp;page=' + js_vars.sort_vars.page + '&amp;sort=da" title="' + js_vars.sort_vars.sort_da + '">+</a>';
+    sortable += '<a href="thumbnails.php?album=' + js_vars.sort_vars.aid + '&amp;page=' + js_vars.sort_vars.page + '&amp;sort=da" title="' + js_vars.sort_vars.sort_da + '"><img src="images/ascending.gif" width="9" height="9" border="0" alt="+" /></a>';
     sortable += '</span>';
     sortable += '&nbsp;';
     sortable += '<span class="statlink">';
-    sortable += '<a href="thumbnails.php?album=' + js_vars.sort_vars.aid + '&amp;page=' + js_vars.sort_vars.page + '&amp;sort=dd" title="' + js_vars.sort_vars.sort_dd + '">-</a>';
+    sortable += '<a href="thumbnails.php?album=' + js_vars.sort_vars.aid + '&amp;page=' + js_vars.sort_vars.page + '&amp;sort=dd" title="' + js_vars.sort_vars.sort_dd + '"><img src="images/descending.gif" width="9" height="9" border="0" alt="-" /></a>';
     sortable += '</span>';
     sortable += separator;
     
@@ -68,11 +68,11 @@ function addSortOptions(){
     sortable += '</span>';
     sortable += '&nbsp;';
     sortable += '<span class="statlink">';
-    sortable += '<a href="thumbnails.php?album=' + js_vars.sort_vars.aid + '&amp;page=' + js_vars.sort_vars.page + '&amp;sort=pa" title="' + js_vars.sort_vars.sort_pa + '">+</a>';
+    sortable += '<a href="thumbnails.php?album=' + js_vars.sort_vars.aid + '&amp;page=' + js_vars.sort_vars.page + '&amp;sort=pa" title="' + js_vars.sort_vars.sort_pa + '"><img src="images/ascending.gif" width="9" height="9" border="0" alt="+" /></a>';
     sortable += '</span>';
     sortable += '&nbsp;';
     sortable += '<span class="statlink">';
-    sortable += '<a href="thumbnails.php?album=' + js_vars.sort_vars.aid + '&amp;page=' + js_vars.sort_vars.page + '&amp;sort=pd" title="' + js_vars.sort_vars.sort_pd + '">-</a>';
+    sortable += '<a href="thumbnails.php?album=' + js_vars.sort_vars.aid + '&amp;page=' + js_vars.sort_vars.page + '&amp;sort=pd" title="' + js_vars.sort_vars.sort_pd + '"><img src="images/descending.gif" width="9" height="9" border="0" alt="-" /></a>';
     sortable += '</span>';
     
     $('#sortorder_cell').append(sortable); 
