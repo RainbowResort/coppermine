f685dd01b7e568299b4a559d7f8812c16ef61505
eenemeenemuu <eenemeenemuu@62f5abee-3c11-0410-b416-987344bf19e2>
2009-12-01 15:14:21 +0000

    Added Flash Media Player plugin to svn.

---
+++ plugins/fmp/codebase.php
@@ -0,0 +1,176 @@
+<?php
+/*************************
+  Coppermine Photo Gallery
+  ************************
+  Copyright (c) 2003-2008 Dev Team
+  v1.1 originally written by Gregory DEMAR
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License version 3
+  as published by the Free Software Foundation.
+
+  ********************************************
+  Coppermine version: 1.5.x
+  $HeadURL$
+  $Revision$
+  $LastChangedBy$
+  $Date$
+**********************************************/
+/*********************************************
+  Coppermine Plugin - Flash Media Player
+  ********************************************
+  Copyright (c) 2009 eenemeenemuu
+**********************************************/
+
+if (!defined('IN_COPPERMINE')) die('Not in Coppermine...');
+
+$thisplugin->add_action('plugin_install', 'fmp_install');
+$thisplugin->add_action('plugin_uninstall', 'fmp_uninstall');
+$thisplugin->add_action('plugin_cleanup', 'fmp_cleanup');
+
+$thisplugin->add_filter('html_other_media','fmp_other_media');
+
+
+function fmp_other_media($pic_html) {
+    global $CURRENT_PIC_DATA;
+    if (in_array($CURRENT_PIC_DATA['extension'], Array('flv', 'mp4', 'mp3', 'aac'))) {
+        global $CONFIG, $USER;
+        $CURRENT_PIC_DATA['pheight'] += 20;
+        $thumb = get_pic_url($CURRENT_PIC_DATA, 'thumb');
+        $file = $CONFIG['ecards_more_pic_target'].get_pic_url($CURRENT_PIC_DATA, 'fullsize');
+
+        $autostart = $CONFIG['media_autostart'] == 1 ? true : false;
+
+        $theme = $USER['theme'] ? $USER['theme'] : $CONFIG['theme'];
+        $stylesheet = file_get_contents("themes/{$theme}/style.css");
+        preg_match_all('/\.tableh2.*{(.*)}/Us', $stylesheet, $matches);
+        $style = array();
+        foreach($matches[1] as $value) {
+            $split = explode(";", $value);
+            foreach($split as $value2) {
+                $split2 = explode(":", $value2);
+                if (isset($split2[0]) && isset($split2[1])) {
+                    $style[trim($split2[0])] = trim($split2[1]);
+                }
+            }
+        }
+
+        $player = "plugins/fmp/player.swf";
+        $player .= "?file=$file";
+        $player .= "&image=$thumb";
+        $player .= "&autostart=$autostart";
+        $player .= "&backcolor={$style['background']}";
+        $player .= "&frontcolor={$style['color']}";
+        $player .= "&lightcolor={$style['color']}";
+
+        $pic_html = <<< EOT
+            <object type="{$CURRENT_PIC_DATA['mime']}" width="{$CURRENT_PIC_DATA['pwidth']}" height="{$CURRENT_PIC_DATA['pheight']}" data="$player">
+            <param name="movie" value="$player" />
+            <param name="allowfullscreen" value="true" />
+            <param name="wmode" value="opaque" />
+            </object>
+        
+EOT;
+    }
+    return $pic_html;
+}
+
+
+function fmp_update_filetype($extension, $type) {
+    global $CONFIG;
+    if (!mysql_result(cpg_db_query("SELECT COUNT(*) FROM {$CONFIG['TABLE_FILETYPES']} WHERE extension = '$extension'"), 0)) {
+        cpg_db_query("INSERT INTO {$CONFIG['TABLE_FILETYPES']} (extension,mime,content,player) VALUES ('$extension', 'application/x-shockwave-flash', '$type', 'SWF')");
+    } else {
+        cpg_db_query("UPDATE {$CONFIG['TABLE_FILETYPES']} SET mime = 'application/x-shockwave-flash', player = 'SWF' WHERE extension = '$extension'");
+    }
+
+    if ($type == "movie") {
+        $config_value = "allowed_mov_types";
+    } elseif ($type == "audio") {
+        $config_value = "allowed_snd_types";
+    }
+    if (strpos($CONFIG[$config_value], '$extension') === FALSE) {
+        cpg_db_query("UPDATE {$CONFIG['TABLE_CONFIG']} SET value = CONCAT(value, '/$extension') WHERE name = '$config_value'");
+    }
+    return;
+}
+
+
+function fmp_reset_filetype($extension, $mime) {
+    global $CONFIG;
+    if (substr_count($mime, 'video') > 0) {
+        $config_value = "allowed_mov_types";
+    } elseif (substr_count($mime, 'audio') > 0) {
+        $config_value = "allowed_snd_types";
+    }
+    $CONFIG[$config_value] = str_replace('/'.$extension, '', $CONFIG[$config_value]);
+    $CONFIG[$config_value] = str_replace($extension.'/', '', $CONFIG[$config_value]);
+    $CONFIG[$config_value] = str_replace($extension, '', $CONFIG[$config_value]);
+    cpg_db_query("UPDATE {$CONFIG['TABLE_CONFIG']} SET value = '{$CONFIG[$config_value]}' WHERE name = '$config_value'");
+    cpg_db_query("UPDATE {$CONFIG['TABLE_FILETYPES']} SET mime = '$mime', player = 'WMP' WHERE extension = '$extension'");
+}
+
+
+function fmp_install() {
+    fmp_update_filetype('flv', 'movie');
+    fmp_update_filetype('mp4', 'movie');
+    fmp_update_filetype('mp3', 'audio');
+    fmp_update_filetype('aac', 'audio');
+    return true;
+}
+
+
+function fmp_uninstall() {
+    $superCage = Inspekt::makeSuperCage();
+
+    if (!$superCage->post->keyExists('drop')) {
+        return 1;
+    }
+
+    if (!checkFormToken()) {
+        global $lang_errors;
+        cpg_die(ERROR, $lang_errors['invalid_form_token'], __FILE__, __LINE__);
+    }
+
+    if ($superCage->post->getInt('drop') == 1) {
+        fmp_reset_filetype('flv', 'video/x-flv');
+        fmp_reset_filetype('mp4', 'video/mp4');
+        fmp_reset_filetype('mp3', 'audio/mpeg3');
+        fmp_reset_filetype('aac', 'audio/aac');
+        return true;
+    }
+}
+
+
+function fmp_cleanup($action) {
+    $superCage = Inspekt::makeSuperCage();
+    $cleanup = $superCage->server->getEscaped('REQUEST_URI');
+    if ($action == 1) {
+        global $lang_common;
+        list($timestamp, $form_token) = getFormToken();
+        echo <<< EOT
+            <table border="0" cellspacing="0" cellpadding="0">
+                <tr>
+                    <td class="tableb">
+                        Playback of existing flv files won't work anymore!
+                    </td>
+                    <td class="tableb">
+                        <form action="pluginmgr.php" method="post">
+                            <input type="submit" name="submit" value="{$lang_common['back']}" class="button" />
+                        </form>
+                    </td>
+                    <td class="tableb">
+                        <form action="{$cleanup}" method="post">
+                            <input type="hidden" name="drop" value="1" />
+                            <input type="hidden" name="form_token" value="{$form_token}" />
+                            <input type="hidden" name="timestamp" value="{$timestamp}" />
+                            <input type="submit" name="submit" value="{$lang_common['continue']}" class="button" />
+                        </form>
+                    </td>
+                </tr>
+            </table>
+EOT;
+    }
+}
+
+?>
---
+++ plugins/fmp/configuration.php
@@ -0,0 +1,39 @@
+<?php
+/*************************
+  Coppermine Photo Gallery
+  ************************
+  Copyright (c) 2003-2008 Dev Team
+  v1.1 originally written by Gregory DEMAR
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License version 3
+  as published by the Free Software Foundation.
+
+  ********************************************
+  Coppermine version: 1.5.x
+  $HeadURL$
+  $Revision$
+  $LastChangedBy$
+  $Date$
+**********************************************/
+/*********************************************
+  Coppermine Plugin - Flash Media Player
+  ********************************************
+  Copyright (c) 2009 eenemeenemuu
+**********************************************/
+
+$name = 'Flash Media Player';
+$description = 'Use <a href="http://www.longtailvideo.com/players/jw-flv-player/" rel="external" class="external">JW Media Player</a> to play flv, mp4, mp3 &amp; aac files. Consider the <a href="http://creativecommons.org/licenses/by-nc-sa/3.0/" class="external">noncommercial license</a>!';
+$author = '<a href="http://forum.coppermine-gallery.net/index.php?action=profile;u=24278" rel="external" class="external">eenemeenemuu</a>';
+$version = '1.0';
+$extra_info = $install_info = <<<EOT
+    <table border="0" cellspacing="0" cellpadding="0">
+        <tr>
+            <td class="admin_menu">
+                <a href="http://forum.coppermine-gallery.net/index.php/topic,62704.0.html" rel="external" class="external">Announcement thread for <strong>$name</strong> plugin</a>
+            </td>
+        </tr>
+    </table>
+EOT;
+
+?>
