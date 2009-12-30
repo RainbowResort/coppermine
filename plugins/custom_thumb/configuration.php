5ab170512bab56a4ae546254fb679d19c3d2a9df
gaugau <gaugau@62f5abee-3c11-0410-b416-987344bf19e2>
2009-12-30 16:02:58 +0000

    Counted version up from 1.2 to 1.3. Added changelog file.

--- plugins/custom_thumb/configuration.php
+++ plugins/custom_thumb/configuration.php
@@ -1,26 +1,19 @@
 <?php
-/*************************
-  Coppermine Photo Gallery
-  ************************
-  Copyright (c) 2003-2008 Dev Team
-  v1.1 originally written by Gregory DEMAR
-
+/**************************************************
+  Coppermine 1.5.x Plugin - Custom Thumbnail
+  *************************************************
+  Copyright (c) 2009 eenemeenemuu
+  *************************************************
   This program is free software; you can redistribute it and/or modify
-  it under the terms of the GNU General Public License version 3
-  as published by the Free Software Foundation.
-
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 3 of the License, or
+  (at your option) any later version.
   ********************************************
-  Coppermine version: 1.5.x
   $HeadURL$
   $Revision$
   $LastChangedBy$
   $Date$
-**********************************************/
-/*********************************************
-  Coppermine Plugin - Custom Thumbnail
-  ********************************************
-  Copyright (c) 2009 eenemeenemuu
-**********************************************/
+**************************************************/
 
 require_once "./plugins/custom_thumb/lang/english.php";
 if ($CONFIG['lang'] != 'english' && file_exists("./plugins/custom_thumb/lang/{$CONFIG['lang']}.php")) {
@@ -30,7 +23,7 @@ if ($CONFIG['lang'] != 'english' && file_exists("./plugins/custom_thumb/lang/{$C
 $name = $lang_plugin_custom_thumb['custom_thumbnail'];
 $description = $lang_plugin_custom_thumb['description'];
 $author = '<a href="http://forum.coppermine-gallery.net/index.php?action=profile;u=24278" rel="external" class="external">eenemeenemuu</a>';
-$version = '1.2';
+$version = '1.3';
 $extra_info = $install_info = '<a href="http://forum.coppermine-gallery.net/index.php/topic,60272.0.html" rel="external" class="admin_menu">'.cpg_fetch_icon('announcement', 1).$lang_plugin_custom_thumb['announcement_thread'].'</a>';
 
 ?>
