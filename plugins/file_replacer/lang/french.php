dc65d98004a1a4ba69abd1fd2d8a844bee326576
eenemeenemuu <eenemeenemuu@62f5abee-3c11-0410-b416-987344bf19e2>
2009-12-30 12:54:56 +0000

    Added French language files for custom_thumb and file_replacer plugins.

---
+++ plugins/file_replacer/lang/french.php
@@ -0,0 +1,32 @@
+<?php
+/**************************************************
+  Picture Annotation (annotate) plugin for cpg1.5.x
+  *************************************************
+  Copyright (c) 2003-2009 Coppermine Dev Team
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License version 3
+  as published by the Free Software Foundation.
+
+  *************************************************
+  Coppermine version: 1.5.x
+  $HeadURL$
+  $Revision$
+  $LastChangedBy$
+  $Date$
+**************************************************/
+
+if (!defined('IN_COPPERMINE')) {
+    die('Not in Coppermine...');
+}
+
+$lang_plugin_file_replacer['replace_file'] = 'Remplacer le fichier';
+$lang_plugin_file_replacer['file_replacer'] = 'Remplacement de fichier';
+$lang_plugin_file_replacer['upload_file'] = 'Télécharger le fichier de remplacement';
+$lang_plugin_file_replacer['browse'] = 'Parcourir';
+$lang_plugin_file_replacer['upload'] = 'Télécharger';
+$lang_plugin_file_replacer['description'] = 'Autoriser les fichiers à être remplacés par un fichier de remplacement';
+$lang_plugin_file_replacer['announcement_thread'] = 'Sujet d\'annonce';
+$lang_plugin_file_replacer['author'] = 'Créer par %s pour cpg1.4.x - Porté sur cpg1.5.x et internationnalisé par %s';
+
+?>
