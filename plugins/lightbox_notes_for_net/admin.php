402dad32a70a811b0e216b1085c8a757a8656266
gaugau <gaugau@62f5abee-3c11-0410-b416-987344bf19e2>
2009-12-31 14:34:53 +0000

    Fixed indentation of PHP code

--- plugins/lightbox_notes_for_net/admin.php
+++ plugins/lightbox_notes_for_net/admin.php
@@ -10,64 +10,64 @@
   $Date$
   **************************************************/
 
-	pageheader('LightBox Configuration');
-	
-	if (!GALLERY_ADMIN_MODE) cpg_die(ERROR, $lang_errors['perm_denied'], __FILE__, __LINE__);
-	
-    global $lang_plugin_php, $CONFIG, $lang_common, $lang_pluginmgr_php, $lang_admin_php, $icon_array;
-	
-	// variables for plugin
-		$lb_bor  = $CONFIG['plugin_lightbox_nfn_border'];	
-		$lb_tim  = $CONFIG['plugin_lightbox_nfn_slidetimer'];
-		$lb_spd  = $CONFIG['plugin_lightbox_nfn_sizespeed'];
-		$lb_not  = $CONFIG['plugin_lightbox_nfn_notimer'];			
-		$lb_ext  = $CONFIG['plugin_lightbox_nfn_image_exit'];
-		$lb_cap  = $CONFIG['plugin_lightbox_nfn_caption'];
-		$lb_fol  = $CONFIG['plugin_lightbox_nfn_nofollow'];			
-		$lb_max  = $CONFIG['plugin_lightbox_nfn_maxpics'];
-	
-		$sel_not = '';	
-		if ($lb_not  == '1') {
-	   	$sel_not = 'selected="selected"';
-		}
-		$sel_notb = '';	
-		if ($lb_not  == '0') {
-	   	$sel_notb = 'selected="selected"';
-		}
+pageheader('LightBox Configuration');
 
-		$sel_ext = '';	
-		if ($lb_ext  == '1') {
-	   	$sel_ext = 'selected="selected"';
-		}
-		$sel_extb = '';	
-		if ($lb_ext  == '0') {
-	   	$sel_extb = 'selected="selected"';
-		}
+if (!GALLERY_ADMIN_MODE) cpg_die(ERROR, $lang_errors['perm_denied'], __FILE__, __LINE__);
 
-		$sel_cap = '';	
-		if ($lb_cap  == '1') {
-	   	$sel_cap = 'selected="selected"';
-		}
-		$sel_capb = '';	
-		if ($lb_cap  == '0') {
-	   	$sel_capb = 'selected="selected"';
-		}
-	
-		$sel_fol = '';	
-		if ($lb_fol  == '1') {
-	   	$sel_fol = 'selected="selected"';
-		}
-		$sel_folb = '';	
-		if ($lb_fol  == '0') {
-	   	$sel_folb = 'selected="selected"';
-		}
+global $lang_plugin_php, $CONFIG, $lang_common, $lang_pluginmgr_php, $lang_admin_php, $icon_array;
 
-	
-	// Make form 
-		$lb_superCage = Inspekt::makeSuperCage();
-	
-	starttable("85%");
-	echo <<< EOT
+// variables for plugin
+$lb_bor  = $CONFIG['plugin_lightbox_nfn_border'];	
+$lb_tim  = $CONFIG['plugin_lightbox_nfn_slidetimer'];
+$lb_spd  = $CONFIG['plugin_lightbox_nfn_sizespeed'];
+$lb_not  = $CONFIG['plugin_lightbox_nfn_notimer'];			
+$lb_ext  = $CONFIG['plugin_lightbox_nfn_image_exit'];
+$lb_cap  = $CONFIG['plugin_lightbox_nfn_caption'];
+$lb_fol  = $CONFIG['plugin_lightbox_nfn_nofollow'];			
+$lb_max  = $CONFIG['plugin_lightbox_nfn_maxpics'];
+
+$sel_not = '';	
+if ($lb_not  == '1') {
+   	$sel_not = 'selected="selected"';
+}
+$sel_notb = '';	
+if ($lb_not  == '0') {
+   	$sel_notb = 'selected="selected"';
+}
+
+$sel_ext = '';	
+if ($lb_ext  == '1') {
+   	$sel_ext = 'selected="selected"';
+}
+$sel_extb = '';	
+if ($lb_ext  == '0') {
+   	$sel_extb = 'selected="selected"';
+}
+
+$sel_cap = '';	
+if ($lb_cap  == '1') {
+   	$sel_cap = 'selected="selected"';
+}
+$sel_capb = '';	
+if ($lb_cap  == '0') {
+   	$sel_capb = 'selected="selected"';
+}
+
+$sel_fol = '';	
+if ($lb_fol  == '1') {
+   	$sel_fol = 'selected="selected"';
+}
+$sel_folb = '';	
+if ($lb_fol  == '0') {
+   	$sel_folb = 'selected="selected"';
+}
+
+
+// Make form 
+$lb_superCage = Inspekt::makeSuperCage();
+
+starttable("85%");
+echo <<< EOT
 	<tr><td>
 	<form name="cpgform" id="cpgform" action="{$_SERVER['REQUEST_URI']}" method="post">
 	<table border="0" cellspacing="2" cellpadding="2" width= "85%">
@@ -207,45 +207,57 @@
 		</form>
 EOT;
 
-	//change values in the configuration table - minimal data filtering assumes admin use only
-	if ($lb_superCage->post->keyExists('update')) {
+//change values in the configuration table - minimal data filtering assumes admin use only
+if ($lb_superCage->post->keyExists('update')) {
+    
+    $lb_bor = $lb_superCage->post->getInt('border');
+    $lb_tim = $lb_superCage->post->getInt('slidetimer');
+    $lb_spd = $lb_superCage->post->getInt('sizespeed');
+    $lb_not = $lb_superCage->post->getInt('notimer');
+    $lb_ext = $lb_superCage->post->getRaw('exit');
+    $lb_cap = $lb_superCage->post->getInt('caption');
+    $lb_fol = $lb_superCage->post->getInt('nofollow');
+    $lb_max = $lb_superCage->post->getInt('maxpics');  
+      
+    if ($lb_bor == '') {
+        cpg_die(ERROR, 'ERROR - BORDER FIELD EMPTY', __FILE__, __LINE__);
+    }
+    if ($lb_tim == '') {
+        cpg_die(ERROR, 'ERROR - TIMER FIELD EMPTY', __FILE__, __LINE__);
+    }
+    if ($lb_spd == '') {
+        cpg_die(ERROR, 'ERROR - RESIZE FIELD EMPTY', __FILE__, __LINE__);
+    }
+    if ($lb_max == '') {
+        cpg_die(ERROR, 'ERROR - MAX FILES FIELD EMPTY', __FILE__, __LINE__);
+    }
+    if ($lb_fol < 0) {
+        cpg_die(ERROR, 'ERROR - FOLLOW FIELD EMPTY', __FILE__, __LINE__);
+    }
+    if ($lb_cap < 0) {
+        cpg_die(ERROR, 'ERROR - CAPTION FIELD EMPTY', __FILE__, __LINE__);
+    }	
+    
+                
+    cpg_db_query("UPDATE {$CONFIG['TABLE_CONFIG']} SET value = '$lb_bor' WHERE name = 'plugin_lightbox_nfn_border'");
+    cpg_db_query("UPDATE {$CONFIG['TABLE_CONFIG']} SET value = '$lb_tim' WHERE name = 'plugin_lightbox_nfn_slidetimer'");	
+    cpg_db_query("UPDATE {$CONFIG['TABLE_CONFIG']} SET value = '$lb_spd' WHERE name = 'plugin_lightbox_nfn_sizespeed'");
+    cpg_db_query("UPDATE {$CONFIG['TABLE_CONFIG']} SET value = '$lb_not' WHERE name = 'plugin_lightbox_nfn_notimer'");
+    cpg_db_query("UPDATE {$CONFIG['TABLE_CONFIG']} SET value = '$lb_ext' WHERE name = 'plugin_lightbox_nfn_image_exit'");
+    cpg_db_query("UPDATE {$CONFIG['TABLE_CONFIG']} SET value = '$lb_cap' WHERE name = 'plugin_lightbox_nfn_caption'");
+    cpg_db_query("UPDATE {$CONFIG['TABLE_CONFIG']} SET value = '$lb_fol' WHERE name = 'plugin_lightbox_nfn_nofollow'");
+    cpg_db_query("UPDATE {$CONFIG['TABLE_CONFIG']} SET value = '$lb_max' WHERE name = 'plugin_lightbox_nfn_maxpics'");		
+    
+    
+    cpgRedirectPage('index.php?file=lightbox_notes_for_net/admin', $lang_common['information'], 'LightBox plugin settings saved successfully', 1);
+    
+    exit;
+}
 
-  $lb_bor = $lb_superCage->post->getInt('border');
-  $lb_tim = $lb_superCage->post->getInt('slidetimer');
-  $lb_spd = $lb_superCage->post->getInt('sizespeed');
-  $lb_not = $lb_superCage->post->getInt('notimer');
-  $lb_ext = $lb_superCage->post->getRaw('exit');
-  $lb_cap = $lb_superCage->post->getInt('caption');
-  $lb_fol = $lb_superCage->post->getInt('nofollow');
-  $lb_max = $lb_superCage->post->getInt('maxpics');  
-  
-  	if ($lb_bor == '') cpg_die(ERROR, 'ERROR - BORDER FIELD EMPTY', __FILE__, __LINE__);
-	if ($lb_tim == '') cpg_die(ERROR, 'ERROR - TIMER FIELD EMPTY', __FILE__, __LINE__);
-	if ($lb_spd == '') cpg_die(ERROR, 'ERROR - RESIZE FIELD EMPTY', __FILE__, __LINE__);
-	if ($lb_max == '') cpg_die(ERROR, 'ERROR - MAX FILES FIELD EMPTY', __FILE__, __LINE__);
-	if ($lb_fol < 0) cpg_die(ERROR, 'ERROR - FOLLOW FIELD EMPTY', __FILE__, __LINE__);
-	if ($lb_cap < 0) cpg_die(ERROR, 'ERROR - CAPTION FIELD EMPTY', __FILE__, __LINE__);	
-	
-		            
-	    cpg_db_query("UPDATE {$CONFIG['TABLE_CONFIG']} SET value = '$lb_bor' WHERE name = 'plugin_lightbox_nfn_border'");
-	    cpg_db_query("UPDATE {$CONFIG['TABLE_CONFIG']} SET value = '$lb_tim' WHERE name = 'plugin_lightbox_nfn_slidetimer'");	
-	    cpg_db_query("UPDATE {$CONFIG['TABLE_CONFIG']} SET value = '$lb_spd' WHERE name = 'plugin_lightbox_nfn_sizespeed'");
-	    cpg_db_query("UPDATE {$CONFIG['TABLE_CONFIG']} SET value = '$lb_not' WHERE name = 'plugin_lightbox_nfn_notimer'");
-		cpg_db_query("UPDATE {$CONFIG['TABLE_CONFIG']} SET value = '$lb_ext' WHERE name = 'plugin_lightbox_nfn_image_exit'");
-		cpg_db_query("UPDATE {$CONFIG['TABLE_CONFIG']} SET value = '$lb_cap' WHERE name = 'plugin_lightbox_nfn_caption'");
-		cpg_db_query("UPDATE {$CONFIG['TABLE_CONFIG']} SET value = '$lb_fol' WHERE name = 'plugin_lightbox_nfn_nofollow'");
-		cpg_db_query("UPDATE {$CONFIG['TABLE_CONFIG']} SET value = '$lb_max' WHERE name = 'plugin_lightbox_nfn_maxpics'");		
-		
-		
-        cpgRedirectPage('index.php?file=lightbox_notes_for_net/admin', $lang_common['information'], 'LightBox plugin settings saved successfully', 1);
-		
-        exit;
-	}
+endtable();
 
-	endtable();
+pagefooter();
 
-	pagefooter();
-	
-	ob_end_flush();
+ob_end_flush();
 
 ?>
