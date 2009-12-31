26f83c1b259e3f9dd79c21541a21b92f76c4da7c
timoswelt <timoswelt@62f5abee-3c11-0410-b416-987344bf19e2>
2009-12-31 14:32:21 +0000

    added SVN keywords

--- plugins/mirror/codebase.php
+++ plugins/mirror/codebase.php
@@ -1,14 +1,19 @@
 <?php
 /**************************************************
-  Coppermine 1.5.x Plugin - Add mirror to images $VERSION$=1.0
+  Coppermine 1.5.x Plugin - Mirror
   *************************************************
   Copyright (c) 2009 Timos-Welt (www.timos-welt.de)
   *************************************************
   This program is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
-  the Free Software Foundation; either version 2 of the License, or
+  the Free Software Foundation; either version 3 of the License, or
   (at your option) any later version.
-  ***************************************************/
+  ********************************************
+  $HeadURL$
+  $Revision$
+  $LastChangedBy$
+  $Date$
+  **************************************************/
 
   
 if (!defined('IN_COPPERMINE')) die('Not in Coppermine...');
