1464e599632295a2fed38639b3b10e0ba319f78b
timoswelt <timoswelt@62f5abee-3c11-0410-b416-987344bf19e2>
2009-12-31 14:21:41 +0000

    added svn keywords

--- plugins/imsharpen/configuration.php
+++ plugins/imsharpen/configuration.php
@@ -1,14 +1,19 @@
 <?php
 /**************************************************
-  Coppermine 1.5.x Plugin - Sharpen Intermediate $VERSION$=0.2
+  Coppermine 1.5.x Plugin - Sharpen Intermediate
   *************************************************
   Copyright (c) 2009 Timos-Welt (www.timos-welt.de)
   *************************************************
   This program is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
-  the Free Software Foundation; either version 2 of the License, or
+  the Free Software Foundation; either version 3 of the License, or
   (at your option) any later version.
-  ***************************************************/
+  ********************************************
+  $HeadURL$
+  $Revision$
+  $LastChangedBy$
+  $Date$
+  **************************************************/
 
 $name='Sharpen Intermediate';
 $description='If this plugin is installed, the generated intermediate images will be sharpened with the thumb sharpening settings from the config page. Thumb sharpening has to be enabled for this to work.';
