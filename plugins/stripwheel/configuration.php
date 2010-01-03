3c6117f8292b578d6bab43de9edcf7b6e294a947
timoswelt <timoswelt@62f5abee-3c11-0410-b416-987344bf19e2>
2010-01-03 17:56:49 +0000

    update to v0.3

--- plugins/stripwheel/configuration.php
+++ plugins/stripwheel/configuration.php
@@ -2,7 +2,7 @@
 /**************************************************
   Coppermine 1.5.x Plugin - Mouse wheel support for filmstrip
   *************************************************
-  Copyright (c) 2010 Timos-Welt (www.timos-welt.de)
+  Copyright (c) 2010 Timo Schewe (www.timos-welt.de)
   *************************************************
   This program is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
@@ -19,7 +19,7 @@
 $name='Mousewheel support for filmstrip';
 $description='Moving the mouse pointer above the filmstrip, the visitor may use the mouse wheel to move one pic back/forward.';
 $author='<a href="http://www.timos-welt.de" rel="external" class="external">Timos-Welt</a>';
-$version='0.2';
+$version='0.3';
 $install_info = "This plugin requires no configuration.";
 $extra_info = '<span class="admin_menu external"><a href="http://forum.coppermine-gallery.net/index.php/topic,62454.0.html" rel="external" title="Mouse wheel for filmstrip support">Mouse wheel for filmstrip support</a></span>';
 ?>
