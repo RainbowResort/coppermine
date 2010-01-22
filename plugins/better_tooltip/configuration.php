3a51877ee563d6d5902cedc982d5f3975e95b177
timoswelt <timoswelt@62f5abee-3c11-0410-b416-987344bf19e2>
2010-01-22 14:52:29 +0000

    first release of plugin that replaces standard thumb title with pic title using the new plugin hook 'thumb_html_title'

---
+++ plugins/better_tooltip/configuration.php
@@ -0,0 +1,24 @@
+<?php
+/**************************************************
+  Coppermine 1.5.x Plugin - Better Tooltip
+  *************************************************
+  Copyright (c) 2010 Timos-Welt (www.timos-welt.de)
+  *************************************************
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 3 of the License, or
+  (at your option) any later version.
+  ********************************************
+  $HeadURL$
+  $Revision$
+  $LastChangedBy$
+  $Date$
+  **************************************************/
+
+$name='Better Tooltip';
+$description='Replaces the standard thumb title (the tooltip that appears if you hold your mouse pointer over a thumbnail) with the image title.';
+$author='<a href="http://www.timos-welt.de">Timos-Welt</a>';
+$version='0.1';
+$install_info = "This plugin requires no configuration.";
+$extra_info = '';
+?>
