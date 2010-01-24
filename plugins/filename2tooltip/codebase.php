b664d7ecb4b9d3c5a9ca434dde71d2aac8da228b
gaugau <gaugau@62f5abee-3c11-0410-b416-987344bf19e2>
2010-01-24 21:17:36 +0000

    Renamed plugin from better_tooltip (too biased) to filename2tooltip

---
+++ plugins/filename2tooltip/codebase.php
@@ -0,0 +1,37 @@
+<?php
+/**************************************************
+  Coppermine 1.5.x Plugin - filename2tooltip
+  *************************************************
+  Copyright (c) 2010 Timos-Welt (www.timos-welt.de)
+  *************************************************
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 3 of the License, or
+  (at your option) any later version.
+  ********************************************
+  $HeadURL: https://coppermine.svn.sourceforge.net/svnroot/coppermine/branches/cpg1.5.x/plugins/better_tooltip/codebase.php $
+  $Revision: 7120 $
+  $LastChangedBy: gaugau $
+  $Date: 2010-01-24 21:57:04 +0100 (So, 24. Jan 2010) $
+  **************************************************/
+
+  
+if (!defined('IN_COPPERMINE')) 
+    die('Not in Coppermine...');
+}
+
+// Add filter for html thumb title
+$thisplugin->add_filter('thumb_html_title','modify_title');
+
+function modify_title($row_data)
+{
+    // replace standard tooltip with pic title
+    if ($row_data[1]['title']) {
+        return $row_data[1]['title'];
+    } else {
+        return $row_data[1]['filename'];
+    }
+}
+
+
+?>
---
+++ plugins/filename2tooltip/configuration.php
@@ -0,0 +1,25 @@
+<?php
+/**************************************************
+  Coppermine 1.5.x Plugin - filename2tooltip
+  *************************************************
+  Copyright (c) 2010 Timos-Welt (www.timos-welt.de)
+  *************************************************
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 3 of the License, or
+  (at your option) any later version.
+  ********************************************
+  $HeadURL: https://coppermine.svn.sourceforge.net/svnroot/coppermine/branches/cpg1.5.x/plugins/better_tooltip/configuration.php $
+  $Revision: 7120 $
+  $LastChangedBy: gaugau $
+  $Date: 2010-01-24 21:57:04 +0100 (So, 24. Jan 2010) $
+  **************************************************/
+
+$name='Better Tooltip';
+$description='Replaces the standard thumb title (the tooltip that appears if you hold your mouse pointer over a thumbnail) with the image title.';
+$author='<a href="http://www.timos-welt.de">Timos-Welt</a>';
+$version='0.4';
+$plugin_cpg_version = array('min' => '1.5.3');
+$install_info = "This plugin requires no configuration.";
+$extra_info = '<a href="http://forum.coppermine-gallery.net/index.php/topic,63369.0.html" rel="external" class="admin_menu">Announcement thread</a>';
+?>
