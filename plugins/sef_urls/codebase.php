234a411733249ce5039fc3ca070ae673498ac52f
timoswelt <timoswelt@62f5abee-3c11-0410-b416-987344bf19e2>
2010-02-02 15:16:44 +0000

    update to v1.6 fixing a ton of bugs and adding german language support

--- plugins/sef_urls/codebase.php
+++ plugins/sef_urls/codebase.php
@@ -37,60 +37,102 @@ $thisplugin->add_filter('page_html','sef_urls_convert');
  * Convert urls to search-engine friendly (SEF) urls
  */
 function sef_urls_convert($html) {
+    
+    $sef_language = 'english';
+    
+    // Language translation
+    if ($sef_language == 'german')
+    {
+        $str_thumbnails = 'uebersicht';
+        $str_displayimage = 'bild';
+        $str_slideshow = 'diaschau';
+        $str_toprated = 'beste';
+        $str_topn = 'beliebteste';
+        $str_lastcomby = 'kommentiertvon';
+        $str_lastcom = 'kommentierte';
+        $str_page = 'seite';
+        $str_profile = 'benutzer';
+        $str_lastupby = 'neuestevon';
+        $str_lastup = 'neueste';
+        $str_search = 'suche';
+        $str_contact = 'kontakt';
+        $str_tdm = 'oben';
+        $str_usermgr = 'benutzerliste';
+        $str_sort = 'sortierung';
+    }
+    else
+    {
+        $str_thumbnails = 'thumbnails';
+        $str_displayimage = 'displayimage';
+        $str_slideshow = 'slideshow';
+        $str_toprated = 'toprated';
+        $str_topn = 'topn';
+        $str_lastcomby = 'lastcomby';
+        $str_lastcom = 'lastcom';
+        $str_page = 'page';
+        $str_profile = 'profile';
+        $str_lastupby = 'lastupby';
+        $str_lastup = 'lastup';
+        $str_search = 'search';
+        $str_contact = 'contact';
+        $str_tdm = 'top_display_media';
+        $str_usermgr = 'usermgr';
+        $str_sort = 'sort';
+    }
 
-    // Rewrite index.php?cat=0 URLs to index.html
+    // Rewrite usermgr.php
+    $html = preg_replace('/usermgr\.php\?page=([0-9]+)/i',$str_usermgr.'-'.$str_page.'-$1.html',$html);
+    $html = preg_replace('/usermgr\.php/i',$str_usermgr.'.html',$html);
+
+    // Rewrite index.php
+    $html = preg_replace('/index\.php\?cat=([0-9]+)(\&|\&amp;)page=([0-9]+)/i','index-$1-'.$str_page.'-$3.html',$html);
     $html = preg_replace('/index\.php\?cat=0/i','/index.html',$html);
-
-    // Rewrite index.php?cat=[category]&page=[page] URLs to index-[category]-page-[page].html
-    $html = preg_replace('/index\.php\?cat=([0-9]+)(\&|\&amp;)page=([0-9]+)/i','index-$1-page-$3.html',$html);
-
-    // Rewrite index.php?cat=[category] URLs to index-[category].html
     $html = preg_replace('/index\.php\?cat=([0-9]+)/i','index-$1.html',$html);
+    $html = preg_replace('/index\.php/i','/index.html',$html);
     
-    // Rewrite thumbnails.php?album=lastupby&uid=[userid] URLs to thumbnails-lastupby-[userid].html
-    $html = preg_replace('/thumbnails\.php\?album=lastupby(\&|\&amp;)uid=([0-9]+)/i','thumbnails-lastupby-$2.html',$html);
- 
-    // Rewrite thumbnails.php?album=lastcomby&uid=[userid] URLs to thumbnails-lastcomby-[userid].html
-    $html = preg_replace('/thumbnails\.php\?album=lastcomby(\&|\&amp;)uid=([0-9]+)/i','thumbnails-lastcomby-$2.html',$html);
+    // Rewrite thumbnails.php
+    $html = preg_replace('/thumbnails\.php\?album=lastupby(\&|\&amp;)uid=([0-9]+)/i',$str_thumbnails.'-'.$str_lastupby.'-$2.html',$html);
+    $html = preg_replace('/thumbnails\.php\?album=lastcomby(\&|\&amp;)uid=([0-9]+)/i',$str_thumbnails.'-'.$str_lastcomby.'-$2.html',$html);
+    $html = preg_replace('/thumbnails\.php\?album=([a-z0-9]+)(\&|\&amp;)cat=([\-0-9]+)(\&|\&amp;)page=([0-9]+)/i',$str_thumbnails.'-$1-$3-'.$str_page.'-$5.html',$html);
+    $html = preg_replace('/thumbnails\.php\?album=([a-z0-9]+)(\&|\&amp;)cat=([\-0-9]+)/i',$str_thumbnails.'-$1-$3.html',$html);
+    $html = preg_replace('/thumbnails\.php\?album=([a-z0-9]+)(\&|\&amp;)page=([0-9]+)/i',$str_thumbnails.'-$1-'.$str_page.'-$3.html',$html);
+    $html = preg_replace('/thumbnails\.php\?search=([^"]+)(\&|\&amp;)album=search/i',$str_thumbnails.'-'.$str_search.'-$1.html',$html);
+    $html = preg_replace('/thumbnails\.php\?album=search(\&|\&amp;)search=([^"]+)/i',$str_thumbnails.'-'.$str_search.'-$2.html',$html);
+    $html = preg_replace('/thumbnails\.php\?album=([a-z0-9]+)/i',$str_thumbnails.'-$1.html',$html);
 
-    // Rewrite thumbnails.php?album=[album]&cat=[category]&page=[category] URLs to thumbnails-[album]-[category]-page-[page].html
-    $html = preg_replace('/thumbnails\.php\?album=([a-z0-9]+)(\&|\&amp;)cat=([\-0-9]+)(\&|\&amp;)page=([0-9]+)/i','thumbnails-$1-$3-page-$5.html',$html);
-
-    // Rewrite thumbnails.php?album=[album]&cat=[category] URLs to thumbnails-[album]-[category].html
-    $html = preg_replace('/thumbnails\.php\?album=([a-z0-9]+)(\&|\&amp;)cat=([\-0-9]+)/i','thumbnails-$1-$3.html',$html);
-
-     // Rewrite thumbnails.php?album=[album]&page=[category]&sort=[type] URLs to thumbnails-[album]-page-[page]-sort-[type].html
-    $html = preg_replace('/thumbnails\.php\?album=([a-z0-9]+)(\&|\&amp;)page=([0-9]+)(\&|\&amp;)sort=([a-z]+)/i','thumbnails-$1-page-$3-sort-$5.html',$html);
-
-    // Rewrite thumbnails.php?album=[album]&page=[category] URLs to thumbnails-[album]-page-[page].html
-    $html = preg_replace('/thumbnails\.php\?album=([a-z0-9]+)(\&|\&amp;)page=([0-9]+)/i','thumbnails-$1-page-$3.html',$html);
-
-    // Rewrite thumbnails.php?album=search&search=[searchterm] URLs to thumbnails-search-[searchterm].html
-    $html = preg_replace('/thumbnails\.php\?album=search(\&|\&amp;)search=([^"]+)/i','thumbnails-search-$2.html',$html);
-
-    // Rewrite thumbnails.php?album=[album] URLs to thumbnails-[album].html
-    $html = preg_replace('/thumbnails\.php\?album=([a-z0-9]+)/i','thumbnails-$1.html',$html);
-
-    // Rewrite displayimage.php?album=[album]&cat=[category]&pid=[pid] URLs to displayimage-[album]-[category]-[pid].html
-    $html = preg_replace('/displayimage\.php\?album=([a-z0-9]+)(\&|\&amp;)cat=([\-0-9]+)(\&|\&amp;)pid=([\-0-9]+)/i','displayimage-$1-$3-$5.html',$html);
-
-    // Rewrite displayimage.php?album=[album]&pid=[pid] URLs to displayimage-[album]-[pid].html
-    $html = preg_replace('/displayimage\.php\?album=([a-z0-9]+)(\&|\&amp;)pid=([\-0-9]+)/i','displayimage-$1-$3.html',$html);
-
-    // Rewrite displayimage.php?pid=[pid] URLs to displayimage-[pid].html
-    $html = preg_replace('/displayimage\.php\?pid=([0-9]+)/i','displayimage-$1.html',$html);
-
-    // Rewrite displayimage.php?album=[album]&pid=[pid]&slideshow=[interval] URLs to slideshow-[album]-[pid]-[interval].html
-    $html = preg_replace('/displayimage\.php\?album=([a-z0-9]+)(\&|\&amp;)pid=([0-9]+)(\&|\&amp;)slideshow=([0-9]+)/i','slideshow-$1-$3-$5.html',$html);
-
-    // Rewrite displayimage.php?album=[metaalbum]&cat=[category]&pid=[pid]&slideshow=[interval] URLs to slideshow-[album]-[category]-[pid]-[interval].html
-    $html = preg_replace('/displayimage\.php\?album=([a-z]+)(\&|\&amp;)cat=([\-0-9]+)(\&|\&amp;)pid=([0-9]+)(\&|\&amp;)slideshow=([0-9]+)/i','slideshow-$1-$3-$5-$7.html',$html);
+    // Rewrite displayimage.php
+    $html = preg_replace('/displayimage\.php\?album=lastcom(\&|\&amp;)cat=([\-0-9]+)(\&|\&amp;)pid=([\-0-9]+)(\&|\&amp;)msg_id=([\-0-9]+)(\&|\&amp;)page=([\-0-9]+)/i',$str_displayimage.'-lastcom-$2-$4-$6-'.$str_page.'-$8.html',$html);
+    $html = preg_replace('/displayimage\.php\?album=([a-z0-9]+)(\&|\&amp;)cat=([\-0-9]+)(\&|\&amp;)pid=([\-0-9]+)(\&|\&amp;)slideshow=([\-0-9]+)/i',$str_slideshow.'-$1-$3-$5-$7.html',$html);
+    $html = preg_replace('/displayimage\.php\?album=([a-z0-9]+)(\&|\&amp;)cat=([\-0-9]+)(\&|\&amp;)pid=([\-0-9]+)(\&|\&amp;)msg_id=([\-0-9]+)(\&|\&amp;)slideshow=([\-0-9]+)/i',$str_slideshow.'-$1-$3-$5-$7-$9.html',$html);
+    $html = preg_replace('/displayimage\.php\?album=([a-z0-9]+)(\&|\&amp;)pid=([\-0-9]+)(\&|\&amp;)slideshow=([\-0-9]+)/i',$str_slideshow.'-$1-$3-$5.html',$html);
+    $html = preg_replace('/displayimage\.php\?album=lastcomby(\&|\&amp;)cat=([\-0-9]+)(\&|\&amp;)pid=([\-0-9]+)(\&|\&amp;)uid=([\-0-9]+)(\&|\&amp;)msg_id=([\-0-9]+)(\&|\&amp;)page=([\-0-9]+)/i',$str_displayimage.'-'.$str_lastcomby.'-$2-$4-$6-$8-$10.html',$html);
+    $html = preg_replace('/displayimage\.php\?album=lastupby(\&|\&amp;)cat=([\-0-9]+)(\&|\&amp;)pid=([\-0-9]+)(\&|\&amp;)uid=([\-0-9]+)/i',$str_displayimage.'-'.$str_lastupby.'-$2-$4-$6.html',$html);
+    $html = preg_replace('/displayimage\.php\?album=([a-z0-9]+)(\&|\&amp;)cat=([\-0-9]+)(\&|\&amp;)pid=([\-0-9]+)/i',$str_displayimage.'-$1-$3-$5.html',$html);
+    $html = preg_replace('/displayimage\.php\?album=search(\&|\&amp;)pid=([\-0-9]+)/i',$str_displayimage.'-'.$str_search.'-$2.html',$html);
+    $html = preg_replace('/displayimage\.php\?album=([a-z0-9]+)(\&|\&amp;)pid=([\-0-9]+)/i',$str_displayimage.'-$1-$3.html',$html);
+    $html = preg_replace('/displayimage\.php\?pid=([0-9]+)/i',$str_displayimage.'-$1.html',$html);
     
-    // Rewrite profile.php?uid=[userid] URLs to profile-[userid].html
-    $html = preg_replace('/profile\.php\?uid=([0-9]+)/i','profile-$1.html',$html);
-  
-    // Rewrite ratepic.php?pic=[position]&rate=[rate] URLs to ratepic-[position]-[rate].html
-    $html = preg_replace('/ratepic\.php\?pic=([0-9]+)(\&|\&amp;)rate=([0-5])/i','ratepic-$1-$3.html',$html);
+    // Rewrite profile.php
+    $html = preg_replace('/profile\.php\?uid=([0-9]+)/i',$str_profile.'-$1.html',$html);
+    $html = preg_replace('/profile\.php\?op=([a-z0-9_]+)/i',$str_profile.'-op-$1.html',$html);
+    
+    // language specific replacements
+    if ($sef_language != 'english')
+    { 
+        $html = preg_replace('/-toprated/i','-'.$str_toprated,$html);
+        $html = preg_replace('/-topn/i','-'.$str_topn,$html);
+        $html = preg_replace('/-lastcom/i','-'.$str_lastcom,$html);
+        $html = preg_replace('/-lastup/i','-'.$str_lastup,$html);
+        $html = preg_replace('/top_display_media/i',$str_tdm,$html);
+        $html = preg_replace('/'.$str_displayimage.'-search-/i',$str_displayimage.'-'.$str_search.'-',$html);
+    }
+    
+    // contact and search.php
+    $html = preg_replace('/contact.php/i',$str_contact.'.html',$html);
+    $html = preg_replace('/search.php/i',$str_search.'.html',$html);
+    
+    // albums in cat=0
+    $html = preg_replace('/'.$str_thumbnails.'-([a-z0-9]+)-0\.html/i',$str_thumbnails.'-$1.html',$html);
     
     // Return modified HTML
     return $html;
@@ -186,7 +228,7 @@ EOT;
  */
 function sef_urls_install() {
     global $thisplugin;
-
+    $sef_language = 'english';
     $sef_superCage = Inspekt::makeSuperCage();
     if ($sef_superCage->post->keyExists('create')) 
     {
@@ -196,7 +238,7 @@ function sef_urls_install() {
 
     // There's no .htaccess file or user has clicked 'yes' on the create form
     if (!file_exists('.htaccess') || $create) {
-        copy($thisplugin->fullpath.'/ht.txt','.htaccess');
+        copy($thisplugin->fullpath.'/ht-'.$sef_language.'.txt','.htaccess');
         return true;
 
     // An htaccess file exists; display the configure form
