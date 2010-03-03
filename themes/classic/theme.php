6ee935313bd7dc976986e9e144dd79015f0942c2
gaugau <gaugau@62f5abee-3c11-0410-b416-987344bf19e2>
2010-03-03 18:33:40 +0000

    Set theme "curve" as default theme. Removed outdated core themes, adding them to the corresponding branches sub-folder.

---
+++ themes/classic/theme.php
@@ -0,0 +1,26 @@
+<?php
+/*************************
+  Coppermine Photo Gallery
+  ************************
+  Copyright (c) 2003-2010 Coppermine Dev Team
+  v1.0 originally written by Gregory Demar
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License version 3
+  as published by the Free Software Foundation.
+  
+  ********************************************
+  Coppermine version: 1.5.3
+  $HeadURL: https://coppermine.svn.sourceforge.net/svnroot/coppermine/trunk/cpg1.5.x/themes/classic/theme.php $
+  $Revision: 6985 $
+  $LastChangedBy: gaugau $
+  $Date: 2010-01-02 11:38:29 +0100 (Sa, 02 Jan 2010) $
+**********************************************/
+
+// ------------------------------------------------------------------------- //
+// This theme has all CORE items removed                                     //
+// ------------------------------------------------------------------------- //
+
+define('THEME_HAS_MENU_ICONS', 16);
+
+?>
---
+++ themes/fruity/theme.php
@@ -0,0 +1,41 @@
+<?php
+/*************************
+  Coppermine Photo Gallery
+  ************************
+  Copyright (c) 2003-2010 Coppermine Dev Team
+  v1.0 originally written by Gregory Demar
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License version 3
+  as published by the Free Software Foundation.
+  
+  ********************************************
+  Coppermine version: 1.5.3
+  $HeadURL: https://coppermine.svn.sourceforge.net/svnroot/coppermine/trunk/cpg1.5.x/themes/fruity/theme.php $
+  $Revision: 6985 $
+  $LastChangedBy: gaugau $
+  $Date: 2010-01-02 11:38:29 +0100 (Sa, 02 Jan 2010) $
+**********************************************/
+
+// ------------------------------------------------------------------------- //
+// The theme "Fruity" has been done by GauGau (http://gaugau.de/) based on   //
+// the framed template of studicasa.nl (their website has gone down, so I    //
+// guess no one will care). The usage of this theme is free for personal     //
+// use, not for commercial use (according to the disclaimer of studiocasa)!  //
+// ------------------------------------------------------------------------- //
+
+define('THEME_HAS_RATING_GRAPHICS', 1);
+define('THEME_HAS_NAVBAR_GRAPHICS', 1);
+define('THEME_HAS_PROGRESS_GRAPHICS', 1);
+
+// HTML template for main menu
+$template_sys_menu = <<< EOT
+                <div class="topmenu">
+                     {BUTTONS}
+                </div>
+EOT;
+
+// HTML template for template sys_menu spacer
+$template_sys_menu_spacer = '';
+
+?>
