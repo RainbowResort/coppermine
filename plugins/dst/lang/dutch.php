e913132ccc1a9708278b9aff0011f5f3fe8561b8
eenemeenemuu <eenemeenemuu@62f5abee-3c11-0410-b416-987344bf19e2>
2010-03-30 18:10:13 +0000

    Added Dutch language file.

---
+++ plugins/dst/lang/dutch.php
@@ -0,0 +1,26 @@
+﻿<?php
+/**************************************************
+  Coppermine 1.5.x Plugin - Daylight saving time
+  *************************************************
+  Copyright (c) 2010 eenemeenemuu
+  *************************************************
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 3 of the License, or
+  (at your option) any later version.
+  ********************************************
+  $HeadURL$
+  $Revision$
+  $LastChangedBy$
+  $Date$
+**************************************************/
+
+if (!defined('IN_COPPERMINE')) {
+    die('Not in Coppermine...');
+}
+
+$lang_plugin_dst['dst'] = 'Zomeruur';
+$lang_plugin_dst['description'] = 'Schakelt de galerij om naar het zomeruur. Deze plugin heeft <strong>geen</strong> configuratie. Uw galerij wordt dadelijk, nadat de plugin geïnstalleerd is, omgeschakeld naar het zomeruur. Na de-installatie wordt uw galerij terug omgezet naar de standaardtijd.';
+$lang_plugin_dst['announcement_thread'] = 'Aankondigingsthema';
+
+?>
