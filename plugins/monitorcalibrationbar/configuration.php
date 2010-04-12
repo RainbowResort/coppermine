aab44a905af5d98cc320b27aed359989dfbbf49a
gaugau <gaugau@62f5abee-3c11-0410-b416-987344bf19e2>
2010-04-12 07:56:11 +0000

    Fixed transparent button background

--- plugins/monitorcalibrationbar/configuration.php
+++ plugins/monitorcalibrationbar/configuration.php
@@ -19,15 +19,15 @@ $name = $lang_plugin_moncalb['config_name'];
 $description = $lang_plugin_moncalb['config_description'];
 
 $author='<a href="http://forum.coppermine-gallery.net/index.php?action=profile;u=2" rel="external" class="external">Joachim Müller</a>';
-$version='1.3';
+$version='1.4';
 $plugin_cpg_version = array('min' => '1.5');
 $extra_info = <<<EOT
     <a href="index.php?file=monitorcalibrationbar/index&amp;action=configure" class="admin_menu">{$moncal_icon_array['config']}{$lang_plugin_moncalb['config']}</a>&nbsp;
-    <a href="http://forum.coppermine-gallery.net/index.php/topic,58638.0.html" title="&laquo;{$lang_plugin_moncalb['config_name']}&raquo; - {$lang_plugin_moncalb['announcement_thread']}" class="admin_menu external">{$moncal_icon_array['announcement']}{$lang_plugin_moncalb['announcement_thread']}</a>&nbsp;
+    <a href="http://forum.coppermine-gallery.net/index.php/topic,58638.0.html" title="&laquo;{$lang_plugin_moncalb['config_name']}&raquo; - {$lang_plugin_moncalb['announcement_thread']}" rel="external" class="admin_menu">{$moncal_icon_array['announcement']}{$lang_plugin_moncalb['announcement_thread']}</a>&nbsp;
     <a href="index.php?file=monitorcalibrationbar/index&action=display" class="admin_menu" title="{$lang_plugin_moncalb['menu']}">{$moncal_icon_array['calibration']}{$lang_plugin_moncalb['picinfo_heading']}</a>
 EOT;
 
 $install_info=<<<EOT
-    <a href="http://forum.coppermine-gallery.net/index.php/topic,58638.0.html" title="&laquo;{$lang_plugin_moncalb['config_name']}&raquo; - {$lang_plugin_moncalb['announcement_thread']}" class="admin_menu external">{$moncal_icon_array['announcement']}{$lang_plugin_moncalb['announcement_thread']}</a>
+    <a href="http://forum.coppermine-gallery.net/index.php/topic,58638.0.html" title="&laquo;{$lang_plugin_moncalb['config_name']}&raquo; - {$lang_plugin_moncalb['announcement_thread']}" rel="external" class="admin_menu">{$moncal_icon_array['announcement']}{$lang_plugin_moncalb['announcement_thread']}</a>
 EOT;
 ?>
