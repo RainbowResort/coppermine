28b7a649f6ff411542e6c9fb41614e091806383b
gaugau <gaugau@62f5abee-3c11-0410-b416-987344bf19e2>
2010-05-04 16:32:50 +0000

    Added Geo IP lookup plugin to repository (initial release).

---
+++ plugins/geoip/codebase.php
@@ -0,0 +1,46 @@
+<?php
+/**************************************************
+  Coppermine 1.5.x Plugin - Geo IP Lookup (geoip)
+  *************************************************
+  Copyright (c) 2010 Joachim Müller
+  *************************************************
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 3 of the License, or
+  (at your option) any later version.
+  ********************************************
+  $HeadURL$
+  $Revision$
+  $LastChangedBy$
+  $Date$
+  **************************************************/
+
+if (!defined('IN_COPPERMINE')) {
+    die('Not in Coppermine...');
+}
+
+if (GALLERY_ADMIN_MODE) {
+	include_once('plugins/geoip/geoip.inc.php');
+	$thisplugin->add_filter('ip_information','plugin_geoip_flag');
+}
+
+
+function plugin_geoip_flag($ip) {  
+	$gi = geoip_open('plugins/geoip/GeoIP.dat',GEOIP_STANDARD);
+	if ($ip == '') {
+		$return = '';
+	} else {
+		$country_code = geoip_country_code_by_addr($gi, $ip);
+		if ($country_code != '' && file_exists('images/flags/' . strtolower($country_code) . '.png') == TRUE) {
+			$return = '<img src="images/flags/' . strtolower($country_code) . '.png" border="0" width="16" height="11" alt="" title="' . geoip_country_name_by_addr($gi, $ip) . ' (' . $country_code . ')" style="margin-left:1px;" />';
+		} else {
+			$return = '';
+		}
+	}
+	geoip_close($gi);
+	return $return;
+}
+
+
+
+?>
---
+++ plugins/geoip/configuration.php
@@ -0,0 +1,33 @@
+<?php
+/**************************************************
+  Coppermine 1.5.x Plugin - Geo IP Lookup (geoip)
+  *************************************************
+  Copyright (c) 2010 Joachim Müller
+  *************************************************
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 3 of the License, or
+  (at your option) any later version.
+  ********************************************
+  $HeadURL$
+  $Revision$
+  $LastChangedBy$
+  $Date$
+  **************************************************/
+
+if (!defined('IN_COPPERMINE')) {
+    die('Not in Coppermine...');
+}
+$geoip_icon_array['announcement'] = cpg_fetch_icon('announcement', 1);
+$name = 'Geo IP Lookup';
+$description = 'Looks up the countries to the IP addresses your visitors come from and displays a flag icon next to each IP address (admin-only feature).';
+$author = 'Joachim Müller';
+$version = '1.0';
+$plugin_cpg_version = array('min' => '1.5.4');
+$install_info = 'This plugin is using the free API and database from <a href="http://maxmind.com/" rel="external" class="external">maxmind.com</a> that has been released under GNU GPL. The lookup is not 100% accurate, but should work for most setups. The lookup will of course burn additional CPU cycles, so don\'t install this plugin it if you don\'t have the needed resources available on your server.<br />&nbsp;<br /><a href="http://forum.coppermine-gallery.net/index.php/topic,64820.0.html" rel="external" class="admin_menu">' . $geoip_icon_array['announcement'] . 'Announcement thread</a>'; 
+$extra_info .= $install_info;
+
+
+
+
+?>
