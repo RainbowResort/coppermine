8e215cf3f9ff579038e1ff32e4e8af07e691ce73
timoswelt <timoswelt@62f5abee-3c11-0410-b416-987344bf19e2>
2010-06-07 10:44:38 +0000

    fixed keyword search URLs

--- plugins/sef_urls/codebase.php
+++ plugins/sef_urls/codebase.php
@@ -42,7 +42,7 @@ function sef_urls_convert($html) {
     // Configure here
     $sef_language            = 'english';   // set to english, german, french, italian or spanish
     $speakingurl_placeholder = '-9b6o4';   // set to '' (empty string) to disable speaking URL functionality
-    $number_of_url_chars     = 40;         // max number of chars in speaking URL functionality
+    $number_of_url_chars     = 42;         // max number of chars in speaking URL functionality
     
     // Language translation
     if ($sef_language == 'german')
@@ -147,6 +147,7 @@ function sef_urls_convert($html) {
     $html = preg_replace('/thumbnails\.php\?album=([a-z0-9]+)(\&|\&amp;)cat=([\-0-9]+)(\&|\&amp;)page=([0-9]+)/i',$str_thumbnails.'-$1-$3-'.$str_page.'-$5.html',$html);
     $html = preg_replace('/thumbnails\.php\?album=([a-z0-9]+)(\&|\&amp;)cat=([\-0-9]+)/i',$str_thumbnails.'-$1-$3.html',$html);
     $html = preg_replace('/thumbnails\.php\?album=([a-z0-9]+)(\&|\&amp;)page=([0-9]+)/i',$str_thumbnails.'-$1-'.$str_page.'-$3.html',$html);
+	$html = preg_replace('/thumbnails\.php\?album=search(\&|\&amp;)keywords=on(\&|\&amp;)search=([^"]+)/i',$str_thumbnails.'-'.$str_search.'-keyword-$3.html',$html);
     $html = preg_replace('/thumbnails\.php\?search=([^"]+)(\&|\&amp;)album=search/i',$str_thumbnails.'-'.$str_search.'-$1.html',$html);
     $html = preg_replace('/thumbnails\.php\?album=search(\&|\&amp;)search=([^"]+)/i',$str_thumbnails.'-'.$str_search.'-$2.html',$html);
     $html = preg_replace('/thumbnails\.php\?album=([a-z0-9]+)/i',$str_thumbnails.'-$1.html',$html);
