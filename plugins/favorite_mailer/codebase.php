6fb5153929abf4a218f060f5407b210453f4d77c
eenemeenemuu <eenemeenemuu@62f5abee-3c11-0410-b416-987344bf19e2>
2010-06-11 12:14:16 +0000

    Updated favorite_mailer plugin.

--- plugins/favorite_mailer/codebase.php
+++ plugins/favorite_mailer/codebase.php
@@ -17,31 +17,34 @@
 
 if (!defined('IN_COPPERMINE')) die('Not in Coppermine...');
 
-$thisplugin->add_action('page_start','favorite_mailer');
+$thisplugin->add_action('page_start', 'favorite_mailer_button');
+$thisplugin->add_filter('page_html', 'favorite_mailer_form');
 
-function favorite_mailer() {
+function favorite_mailer_button() {
     $superCage = Inspekt::makeSuperCage();
 
     if ($superCage->get->getAlpha('album') == 'favpics') {
         global $lang_meta_album_names;
-        $lang_meta_album_names['favpics'] .= ' <a href="index.php?fm_mail" title="Send favorites to admin">'.cpg_fetch_icon('contact', 2).'</a>';
+        $lang_meta_album_names['favpics'] .= ' <a href="contact.php?fm_mail" title="Send favorites to admin">'.cpg_fetch_icon('contact', 2).'</a>';
     }
+}
+
+function favorite_mailer_form($html) {
+    $superCage = Inspekt::makeSuperCage();
 
     if ($superCage->get->keyExists('fm_mail')) {
         global $CONFIG, $FAVPICS;
 
         foreach ($FAVPICS as $pid) {
-            $fav_pics .= $CONFIG['ecards_more_pic_target']."/displayimage.php?pid=$pid \n";
+            $fav_pics .= $CONFIG['ecards_more_pic_target']."displayimage.php?pid=$pid \n\n";
         }
 
         if ($fav_pics) {
-            require('include/mailer.inc.php');
-            cpg_mail('admin', 'Favorite mailer test', $fav_pics);
+            $html = str_replace('<textarea name="message" cols="50" rows="10" class="textinput"></textarea>', '<textarea name="message" cols="50" rows="10" class="textinput">'.$fav_pics.'</textarea>', $html);
         }
     }
 
+    return $html;
 }
-
-
 
 ?>
--- plugins/favorite_mailer/configuration.php
+++ plugins/favorite_mailer/configuration.php
@@ -18,7 +18,7 @@
 $name = 'Favorite mailer';
 $description = 'E-Mail user\'s favorites to the gallery admin';
 $author = '<a href="http://forum.coppermine-gallery.net/index.php?action=profile;u=24278" rel="external" class="external">eenemeenemuu</a>';
-$version = '0.1';
+$version = '0.2';
 $plugin_cpg_version = array('min' => '1.5');
 //$extra_info = $install_info = '<a href="http://forum.coppermine-gallery.net/index.php/topic,TODO.0.html" rel="external" class="admin_menu">'.cpg_fetch_icon('announcement', 1).$lang_plugin_custom_thumb['announcement_thread'].'</a>';
 
