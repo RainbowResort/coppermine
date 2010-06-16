112a9e379a11e7953ae4262ac4aa2c36498efeff
eenemeenemuu <eenemeenemuu@62f5abee-3c11-0410-b416-987344bf19e2>
2010-06-16 09:18:54 +0000

    Added fullsize view support.

--- plugins/flash_media_player/codebase.php
+++ plugins/flash_media_player/codebase.php
@@ -19,9 +19,18 @@ $thisplugin->add_action('plugin_cleanup', 'fmp_cleanup');
 $thisplugin->add_filter('html_other_media','fmp_other_media');
 $thisplugin->add_action('page_start','fmp_page_start');
 
+if ($CONFIG['thumbnail_to_fullsize'] == 1) {
+    $thisplugin->add_filter('fullsize_html','fmp_fullsize');
+    $thisplugin->add_filter('theme_display_thumbnails_params','fmp_fullsize_thumbnail_params');
+}
 
-function fmp_get_html($CURRENT_PIC_DATA) {
-    if (in_array($CURRENT_PIC_DATA['extension'], Array('flv', 'mp4', 'mp3', 'aac'))) {
+
+function fmp_get_html($CURRENT_PIC_DATA, $check_only = false) {
+    if (in_array($CURRENT_PIC_DATA['extension'], array('flv', 'mp4', 'mp3', 'aac'))) {
+        if ($check_only == true) {
+            return true;
+        }
+
         global $CONFIG, $USER;
         $CURRENT_PIC_DATA['pheight'] += 24;
         $thumb = get_pic_url($CURRENT_PIC_DATA, 'thumb');
@@ -58,17 +67,100 @@ function fmp_get_html($CURRENT_PIC_DATA) {
             <param name="wmode" value="opaque" />
             </object>
 EOT;
+        return $pic_html;
+    } else {
+        return false;
     }
-    return $pic_html;
 }
 
 
 function fmp_other_media($pic_html) {
     global $CURRENT_PIC_DATA;
-    if (fmp_get_html($CURRENT_PIC_DATA)) {
+    if (fmp_get_html($CURRENT_PIC_DATA, true)) {
         $pic_html = fmp_get_html($CURRENT_PIC_DATA);
     }
     return $pic_html;
+}
+
+
+function fmp_fullsize($html) {
+    global $CONFIG, $FORBIDDEN_SET, $pid;
+
+    $sql = "SELECT * FROM {$CONFIG['TABLE_PICTURES']} AS p WHERE pid='$pid' $FORBIDDEN_SET";
+    $result = cpg_db_query($sql);
+    if (!mysql_num_rows($result)) {
+        cpg_die(ERROR, $lang_errors['non_exist_ap'], __FILE__, __LINE__);
+    }
+    $CURRENT_PIC_DATA = mysql_fetch_assoc($result);
+
+    $mime_content = cpg_get_type($CURRENT_PIC_DATA['filename']);
+    $CURRENT_PIC_DATA['extension'] = $mime_content['extension'];
+
+    if ($CURRENT_PIC_DATA['pwidth']==0 || $CURRENT_PIC_DATA['pheight']==0) {
+        $resize_method = $CONFIG['picture_use'] == "thumb" ? ($CONFIG['thumb_use'] == "ex" ? "any" : $CONFIG['thumb_use']) : $CONFIG['picture_use'];
+        if ($resize_method == 'ht') {
+            $pwidth = $CONFIG['picture_width']*4/3;
+            $pheight = $CONFIG['picture_width'];
+        } else { 
+            $pwidth = $CONFIG['picture_width'];
+            $pheight = $CONFIG['picture_width']*3/4;
+        }
+
+        $CURRENT_PIC_DATA['pwidth']  = $pwidth; // Default width
+
+        // Set default height; if file is a movie
+        if ($mime_content['content']=='movie') {
+            $CURRENT_PIC_DATA['pheight'] = $pheight; // Default height
+        }
+    }
+
+    if ($pic_html = fmp_get_html($CURRENT_PIC_DATA)) {
+        $html = preg_replace('/<body style="margin:0px; padding:0px; background-color: gray;">(.*)<\/body>/Usi', '<body style="margin:0px; padding:0px; background-color: gray;">'.$pic_html.'</body>', $html);
+    }
+
+    return $html;
+}
+
+
+function fmp_fullsize_thumbnail_params($data) {
+    preg_match('/alt="(.*)"/Ui', $data['{THUMB}'], $match_alt);
+    $mime_content = cpg_get_type($match_alt[1]);
+
+    if (fmp_get_html($mime_content, true)) {
+        global $CONFIG;
+
+        preg_match('/pid=([0-9]+)/i', $data['{LINK_TGT}'], $match_pid);
+        $pid = $match_pid[1];
+
+        $sql = "SELECT pwidth, pheight FROM {$CONFIG['TABLE_PICTURES']} WHERE pid='$pid'";
+        $result = cpg_db_query($sql);
+        $CURRENT_PIC_DATA = mysql_fetch_assoc($result);
+
+        if ($CURRENT_PIC_DATA['pwidth']==0 || $CURRENT_PIC_DATA['pheight']==0) {
+            $resize_method = $CONFIG['picture_use'] == "thumb" ? ($CONFIG['thumb_use'] == "ex" ? "any" : $CONFIG['thumb_use']) : $CONFIG['picture_use'];
+            if ($resize_method == 'ht') {
+                $pwidth = $CONFIG['picture_width']*4/3;
+                $pheight = $CONFIG['picture_width'];
+            } else { 
+                $pwidth = $CONFIG['picture_width'];
+                $pheight = $CONFIG['picture_width']*3/4;
+            }
+
+            $CURRENT_PIC_DATA['pwidth']  = $pwidth; // Default width
+
+            // Set default height; if file is a movie
+            if ($mime_content['content']=='movie') {
+                $CURRENT_PIC_DATA['pheight'] = $pheight; // Default height
+            }
+        }
+
+        $new_height = ceil($CURRENT_PIC_DATA['pheight'] + $CONFIG['fullsize_padding_y'] + 24);
+        $new_width = ceil($CURRENT_PIC_DATA['pwidth'] + $CONFIG['fullsize_padding_x']);
+
+        $data['{LINK_TGT}'] = preg_replace('/width=([0-9]+),height=([0-9]+)/i', 'width='.$new_width.',height='.$new_height, $data['{LINK_TGT}']);
+    }
+
+    return $data;
 }
 
 
