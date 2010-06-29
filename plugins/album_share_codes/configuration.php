a0620cb55a50c25dd3235e3080074c206918ea58
eenemeenemuu <eenemeenemuu@62f5abee-3c11-0410-b416-987344bf19e2>
2010-06-29 14:03:32 +0000

    Updated urls to announcement threads.

--- plugins/album_share_codes/configuration.php
+++ plugins/album_share_codes/configuration.php
@@ -16,7 +16,10 @@
   **************************************************/
 
 $name = 'Album Share Codes';
+$description = 'Adds share codes to the album view page (thumbnail.php).';
+$author = '<a href="http://forum.coppermine-gallery.net/index.php?action=profile;u=24278" rel="external" class="external">eenemeenemuu</a>';
 $version = '0.1';
 $plugin_cpg_version = array('min' => '1.5');
+$extra_info = $install_info = '<a href="http://forum.coppermine-gallery.net/index.php/topic,65779.0.html" rel="external" class="admin_menu">'.cpg_fetch_icon('announcement', 1).$lang_plugin_custom_thumb['announcement_thread'].'</a>';
 
 ?>
--- plugins/favorite_mailer/configuration.php
+++ plugins/favorite_mailer/configuration.php
@@ -20,6 +20,6 @@ $description = 'E-Mail user\'s favorites to the gallery admin';
 $author = '<a href="http://forum.coppermine-gallery.net/index.php?action=profile;u=24278" rel="external" class="external">eenemeenemuu</a>';
 $version = '0.2';
 $plugin_cpg_version = array('min' => '1.5');
-//$extra_info = $install_info = '<a href="http://forum.coppermine-gallery.net/index.php/topic,TODO.0.html" rel="external" class="admin_menu">'.cpg_fetch_icon('announcement', 1).$lang_plugin_custom_thumb['announcement_thread'].'</a>';
+$extra_info = $install_info = '<a href="http://forum.coppermine-gallery.net/index.php/topic,65780.0.html" rel="external" class="admin_menu">'.cpg_fetch_icon('announcement', 1).$lang_plugin_custom_thumb['announcement_thread'].'</a>';
 
 ?>
