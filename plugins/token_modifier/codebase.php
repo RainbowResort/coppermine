a1bab89ceeaf6f637a6114aeddf9f6b5b29c0cad
eenemeenemuu <eenemeenemuu@62f5abee-3c11-0410-b416-987344bf19e2>
2010-07-16 07:49:16 +0000

    Added form token modifier plugin.

---
+++ plugins/token_modifier/codebase.php
@@ -0,0 +1,30 @@
+<?php
+/**************************************************
+  Coppermine 1.5.x Plugin - Form token modifier
+  *************************************************
+  Copyright (c) 2010 eenemeenemuu
+  *************************************************
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 3 of the License, or
+  (at your option) any later version.
+  ********************************************
+  $HeadURL$
+  $Revision$
+  $LastChangedBy$
+  $Date$
+**************************************************/
+
+if (!defined('IN_COPPERMINE')) die('Not in Coppermine...');
+
+$thisplugin->add_filter('token_criteria', 'token_modifier_criteria');
+
+function token_modifier_criteria($criteria) {
+    $remove = array('ip_addr', 'browser');
+    foreach($remove as $key) {
+        unset($criteria[$key]);
+    }
+    return $criteria;
+}
+
+?>
---
+++ plugins/token_modifier/configuration.php
@@ -0,0 +1,25 @@
+<?php
+/**************************************************
+  Coppermine 1.5.x Plugin - Form token modifier
+  *************************************************
+  Copyright (c) 2010 eenemeenemuu
+  *************************************************
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 3 of the License, or
+  (at your option) any later version.
+  ********************************************
+  $HeadURL$
+  $Revision$
+  $LastChangedBy$
+  $Date$
+**************************************************/
+
+$name = 'Form token modifier';
+$description = 'Creates form tokens with only server side criteria.';
+$author = '<a href="http://forum.coppermine-gallery.net/index.php?action=profile;u=24278" rel="external" class="external">eenemeenemuu</a>';
+$version = '1.0';
+$plugin_cpg_version = array('min' => '1.5.7');
+$extra_info = $install_info = '<a href="http://forum.coppermine-gallery.net/index.php/topic,66044.0.html" rel="external" class="admin_menu">'.cpg_fetch_icon('announcement', 1).$lang_plugin_custom_thumb['announcement_thread'].'</a>';
+
+?>
