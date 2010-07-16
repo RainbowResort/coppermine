76a6d33e3beb30a78e664ccf7cb37f01ceedce66
eenemeenemuu <eenemeenemuu@62f5abee-3c11-0410-b416-987344bf19e2>
2010-07-16 07:59:24 +0000

    Replaced language string.

--- plugins/token_modifier/configuration.php
+++ plugins/token_modifier/configuration.php
@@ -20,6 +20,6 @@ $description = 'Creates form tokens with only server side criteria.';
 $author = '<a href="http://forum.coppermine-gallery.net/index.php?action=profile;u=24278" rel="external" class="external">eenemeenemuu</a>';
 $version = '1.0';
 $plugin_cpg_version = array('min' => '1.5.7');
-$extra_info = $install_info = '<a href="http://forum.coppermine-gallery.net/index.php/topic,66044.0.html" rel="external" class="admin_menu">'.cpg_fetch_icon('announcement', 1).$lang_plugin_custom_thumb['announcement_thread'].'</a>';
+$extra_info = $install_info = '<a href="http://forum.coppermine-gallery.net/index.php/topic,66044.0.html" rel="external" class="admin_menu">'.cpg_fetch_icon('announcement', 1).'Announcement thread</a>';
 
 ?>
