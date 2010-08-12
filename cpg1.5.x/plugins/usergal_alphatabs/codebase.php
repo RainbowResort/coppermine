07b470cc555fb73909919f15097444067cf8845f
eenemeenemuu <eenemeenemuu@62f5abee-3c11-0410-b416-987344bf19e2>
2010-08-12 06:46:35 +0000

    Fixed lower case user name issue with plugin usergal_alphatabs (thread ID 66485).

--- cpg1.5.x/plugins/usergal_alphatabs/codebase.php
+++ cpg1.5.x/plugins/usergal_alphatabs/codebase.php
@@ -262,7 +262,7 @@ if ($superCage->get->keyExists('cat') && $superCage->get->getInt('cat') == USER_
         $sql .= " INNER JOIN {$CONFIG[\'TABLE_PICTURES\']} AS pics ON pics.aid = p.aid ";
         $sql .= " INNER JOIN ".$this->usertable." AS u ON p.owner = u.user_id ";
         $sql .= "WHERE ( category > " . FIRST_USER_CAT . " $forbidden) ";
-        if ($l = $getLetter) $sql .= "AND {$f[\'username\']} LIKE \'$l%\' ";
+        if ($l = $getLetter) $sql .= "AND UPPER({$f[\'username\']}) LIKE \'$l%\' ";
         $sql .= "GROUP BY category;";
         $result = cpg_db_query($sql);
         $user_count = mysql_num_rows($result);
