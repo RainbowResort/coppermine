76f5c10a0ce392ab720403f4665403b0163f9fe5
eenemeenemuu <eenemeenemuu@62f5abee-3c11-0410-b416-987344bf19e2>
2010-10-13 13:12:07 +0000

    Updated usage of plugin hook 'ip_information' according to change of r7961.

--- plugins/geoip/codebase.php
+++ plugins/geoip/codebase.php
@@ -25,55 +25,56 @@ if (!defined('IN_COPPERMINE')) {
 
 
 if (GALLERY_ADMIN_MODE) {
-	if ($CONFIG['plugin_geoip_scope'] == '1' && file_exists("./plugins/geoip/GeoLiteCity.dat")) {
+    if ($CONFIG['plugin_geoip_scope'] == '1' && file_exists("./plugins/geoip/GeoLiteCity.dat")) {
         include_once('plugins/geoip/geoipcity.inc.php');
         include_once('plugins/geoip/geoipregionvars.php');
-	} else {
-	    include_once('plugins/geoip/geoip.inc.php');
-	}
-	$thisplugin->add_filter('ip_information','plugin_geoip_flag');
+    } else {
+        include_once('plugins/geoip/geoip.inc.php');
+    }
+    $thisplugin->add_filter('ip_information','plugin_geoip_flag');
 }
 
 // install
 function plugin_geoip_install() {
     global $CONFIG;
-	// Add the config options for the plugin	cpg_db_query("INSERT IGNORE INTO {$CONFIG['TABLE_CONFIG']} (`name`, `value`) VALUES ('plugin_geoip_scope', '0')");    return true;
+    // Add the config options for the plugin    cpg_db_query("INSERT IGNORE INTO {$CONFIG['TABLE_CONFIG']} (`name`, `value`) VALUES ('plugin_geoip_scope', '0')");    return true;
 }
 
 // uninstall and drop settingsfunction plugin_geoip_uninstall() {
     global $CONFIG;
-	$superCage = Inspekt::makeSuperCage();
+    $superCage = Inspekt::makeSuperCage();
     if (!checkFormToken()) {
         global $lang_errors;
         cpg_die(ERROR, $lang_errors['invalid_form_token'], __FILE__, __LINE__);
     }
-    // Delete the plugin config records	cpg_db_query("DELETE FROM {$CONFIG['TABLE_CONFIG']} WHERE name = 'plugin_geoip_scope'");    return true;
+    // Delete the plugin config records    cpg_db_query("DELETE FROM {$CONFIG['TABLE_CONFIG']} WHERE name = 'plugin_geoip_scope'");    return true;
 }
 
-function plugin_geoip_flag($ip) {  
-	global $CONFIG;
-	$return = '';
-	if ($ip != '') {
-		if ($CONFIG['plugin_geoip_scope'] == '1' && file_exists("./plugins/geoip/GeoLiteCity.dat")) {
-		    $gi = geoip_open('plugins/geoip/GeoLiteCity.dat',GEOIP_STANDARD);
-		    $record = geoip_record_by_addr($gi, $ip);
-    		if ($record->country_code != '' && file_exists('images/flags/' . strtolower($record->country_code) . '.png') == TRUE) {
-    			$return = '<img src="images/flags/' . strtolower($record->country_code) . '.png" border="0" width="16" height="11" alt="" title="' . geoip_country_name_by_addr($gi, $ip) . '" style="margin-left:1px;" />';
-    		}
-    		if ($record->city != '') {
-    			$return .= $record->city;
-    		}
-		    geoip_close($gi);
-		} else {
-    		$gi = geoip_open('plugins/geoip/GeoIP.dat',GEOIP_STANDARD);
-    		$country_code = geoip_country_code_by_addr($gi, $ip);
-    		if ($country_code != '' && file_exists('images/flags/' . strtolower($country_code) . '.png') == TRUE) {
-    			$return = '<img src="images/flags/' . strtolower($country_code) . '.png" border="0" width="16" height="11" alt="" title="' . geoip_country_name_by_addr($gi, $ip) . '" style="margin-left:1px;" />';
-    		}
-    		geoip_close($gi);
-		}
-	}
-	return $return;
+function plugin_geoip_flag($ip) {
+    global $CONFIG;
+    $ip = $ip[1];
+    $return = '';
+    if ($ip != '') {
+        if ($CONFIG['plugin_geoip_scope'] == '1' && file_exists("./plugins/geoip/GeoLiteCity.dat")) {
+            $gi = geoip_open('plugins/geoip/GeoLiteCity.dat',GEOIP_STANDARD);
+            $record = geoip_record_by_addr($gi, $ip);
+            if ($record->country_code != '' && file_exists('images/flags/' . strtolower($record->country_code) . '.png') == TRUE) {
+                $return = '<img src="images/flags/' . strtolower($record->country_code) . '.png" border="0" width="16" height="11" alt="" title="' . geoip_country_name_by_addr($gi, $ip) . '" style="margin-left:1px;" />';
+            }
+            if ($record->city != '') {
+                $return .= $record->city;
+            }
+            geoip_close($gi);
+        } else {
+            $gi = geoip_open('plugins/geoip/GeoIP.dat',GEOIP_STANDARD);
+            $country_code = geoip_country_code_by_addr($gi, $ip);
+            if ($country_code != '' && file_exists('images/flags/' . strtolower($country_code) . '.png') == TRUE) {
+                $return = '<img src="images/flags/' . strtolower($country_code) . '.png" border="0" width="16" height="11" alt="" title="' . geoip_country_name_by_addr($gi, $ip) . '" style="margin-left:1px;" />';
+            }
+            geoip_close($gi);
+        }
+    }
+    return array($return);
 }
 
 
