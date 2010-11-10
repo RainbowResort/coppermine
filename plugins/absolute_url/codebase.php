64dddd1df3b6411f91a6a2b7ef9fcae6730ac0c4
eenemeenemuu <eenemeenemuu@62f5abee-3c11-0410-b416-987344bf19e2>
2010-11-10 15:21:10 +0000

    Don't use for images/ folder.

--- plugins/absolute_url/codebase.php
+++ plugins/absolute_url/codebase.php
@@ -21,8 +21,12 @@ $thisplugin->add_filter('picture_url', 'absolute_url_picture_url');
 
 function absolute_url_picture_url($pic_row) {
     global $CONFIG;
-    $url = $CONFIG['plugin_absolute_url_url'] ? $CONFIG['plugin_absolute_url_url'] : $CONFIG['ecards_more_pic_target'];
-    $pic_row['url'] = $url.$pic_row['url'];
+    if (stripos($pic_row['url'], $CONFIG['fullpath']) === 0) {
+        $prefix = $CONFIG['plugin_absolute_url_url'] ? $CONFIG['plugin_absolute_url_url'] : $CONFIG['ecards_more_pic_target'];
+    } else {
+        $prefix = '';
+    }
+    $pic_row['url'] = $prefix.$pic_row['url'];
     return $pic_row;
 }
 
