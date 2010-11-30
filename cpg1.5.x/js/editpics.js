3eed4b708f955908730e815ed2e4842540dc3aa4
eenemeenemuu <eenemeenemuu@62f5abee-3c11-0410-b416-987344bf19e2>
2010-11-30 06:37:44 +0000

    Preparing release of cpg1.5.10.

--- cpg1.5.x/js/editpics.js
+++ cpg1.5.x/js/editpics.js
@@ -9,7 +9,7 @@
   as published by the Free Software Foundation.
 
   ********************************************
-  Coppermine version: 1.5.9
+  Coppermine version: 1.5.10
   $HeadURL$
   $Revision$
 **********************************************/
--- cpg1.5.x/update.php
+++ cpg1.5.x/update.php
@@ -11,7 +11,7 @@
   as published by the Free Software Foundation.
 
   ********************************************
-  Coppermine version: 1.5.9
+  Coppermine version: 1.5.10
   $HeadURL$
   $Revision$
 **********************************************/
