69a87e34dbc980b5fdc8cb6c368beec1a3ec69ae
Frantzk <Frantzk@62f5abee-3c11-0410-b416-987344bf19e2>
2010-12-19 10:15:50 +0000

    Added french lang file (Translation by FBleu)

---
+++ plugins/dst/lang/french.php
@@ -0,0 +1,39 @@
+<?php
+/**************************************************
+  Coppermine 1.5.x Plugin - Daylight saving time 
+  *************************************************
+  Copyright (c) 2010 eenemeenemuu
+  *************************************************
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 3 of the License, or
+  (at your option) any later version.
+  ********************************************
+  $HeadURL: https://coppermine.svn.sourceforge.net/svnroot/coppermine/branches/cpg1.5.x/plugins/dst/lang/english.php $
+  $Revision: 7458 $
+  $LastChangedBy: eenemeenemuu $
+  $Date: 2010-04-22 08:22:23 +0200 (jeu. 22 avr. 2010) $
+**************************************************/
+
+if (!defined('IN_COPPERMINE')) {
+    die('Not in Coppermine...');
+}
+
+$lang_plugin_dst['dst'] = 'Daylight saving time ( Sauvegarde de l\'heure d\'été )';
+$lang_plugin_dst['description'] = 'Commutateurs automatique de galerie à l\'heure d\'été.';
+$lang_plugin_dst['announcement_thread'] = 'Sujet d’annonce';
+$lang_plugin_dst['configuration'] = 'Configuration';
+$lang_plugin_dst['select_your_country_or_region'] = 'Sélectionnez votre pays ou région';
+$lang_plugin_dst['plugin_x_not_installed_yet'] = 'Le plugin %s n\'est pas encore installé.';
+$lang_plugin_dst['submit_form_to_install'] = 'Soumettre le formulaire d\'installation.';
+$lang_plugin_dst['changes_saved'] = 'Vos modifications ont été enregistrées';
+$lang_plugin_dst['no_changes'] = 'Il n\'y a eu aucun changement ou les valeurs que vous avez inscrites sont invalides';
+$lang_plugin_dst['dst_will_start_on'] = 'L\'heure d\'été commencera le %s.';
+$lang_plugin_dst['dst_will_end_on'] = 'L\'heure d\'été prendra fin le %s.';
+$lang_plugin_dst['time_will_auto_adjust'] = 'L’heure de votre galerie se trouvant sur votre serveur est ajustée automatiquement à cette date.';
+$lang_plugin_dst['you_have_selected_x_as_country'] = 'Vous avez choisi %s en tant que votre pays / région.';
+$lang_plugin_dst['submit'] = 'Soumettre';
+$lang_plugin_dst['no_daylight_saving_time'] = 'Aucune heure d\'été';
+$lang_plugin_dst['plugin_is_installed_and_interferes'] = 'Le plugin %s est actuellement installé et interfère avec cette option de configuration.';
+
+?>
