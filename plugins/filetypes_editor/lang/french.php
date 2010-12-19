d3e84e318c6d17c6e7bb6930049318f782fdc708
Frantzk <Frantzk@62f5abee-3c11-0410-b416-987344bf19e2>
2010-12-19 09:59:17 +0000

    Added french lang file (Translation by FBleu)

---
+++ plugins/filetypes_editor/lang/french.php
@@ -0,0 +1,50 @@
+<?php
+/**************************************************
+  Coppermine 1.5.x Plugin - filetypes_editor
+  *************************************************
+  Copyright (c) 2009 Nibbler
+  *************************************************
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 3 of the License, or
+  (at your option) any later version.
+  ********************************************
+  $HeadURL: https://coppermine.svn.sourceforge.net/svnroot/coppermine/branches/cpg1.5.x/plugins/filetypes_editor/lang/english.php $
+  $Revision: 7125 $
+  $LastChangedBy: gaugau $
+  $Date: 2010-01-25 18:03:41 +0100 (Mo, 25 Jan 2010) $
+  **************************************************/
+
+if (!defined('IN_COPPERMINE')) die('Not in Coppermine...');
+$lang_plugin_filetypes_editor['plugin_name'] = 'Filetypes editor (Éditeur de types de fichiers)';
+$lang_plugin_filetypes_editor['plugin_description'] = 'Un plugin pour permettre un accès facile à la table des types de fichiers';
+$lang_plugin_filetypes_editor['plugin_description_detail'] = 'Utile pour associer des types MIME avec des lecteurs particuliers ou pour ajouter le support d’extensions de fichier que Coppermine ne prend pas en charge.';
+$lang_plugin_filetypes_editor['plugin_extra'] = 'Ce plugin crée un nouveau bouton à la fin de votre menu administrateur appelé "Éditeur de types de fichiers". Cliquez sur ce bouton pour utiliser ce plugin ou cliquez sur le bouton "Information":';
+$lang_plugin_filetypes_editor['announcement_thread'] = 'Sujet d’annonce';
+$lang_plugin_filetypes_editor['config_title'] = 'Modifier les types de fichiers et la corrélation avec le lecteur';
+$lang_plugin_filetypes_editor['config_name'] = 'Éditeur de types de fichiers';
+$lang_plugin_filetypes_editor['file_extension'] = 'Extension de fichier';
+$lang_plugin_filetypes_editor['file_extension_detail'] = 'minuscules, pas de point';
+$lang_plugin_filetypes_editor['mime_type'] = 'Type MIME';
+$lang_plugin_filetypes_editor['mime_type_detail'] = 'doit être valide';
+$lang_plugin_filetypes_editor['content_type'] = 'Type de contenu';
+$lang_plugin_filetypes_editor['associated_player'] = 'Lecteur associé';
+$lang_plugin_filetypes_editor['delete_selected_filetypes'] = 'Supprimer le type de fichier sélectionné';
+$lang_plugin_filetypes_editor['add_new_filetype'] = 'Ajouter un nouveau type de fichier';
+$lang_plugin_filetypes_editor['update'] = 'mise à jour';
+$lang_plugin_filetypes_editor['add'] = 'Ajouter';
+$lang_plugin_filetypes_editor['enter_filetype_details'] = 'Entrez les détails du type du fichier';
+$lang_plugin_filetypes_editor['cancel'] = 'Annuler';
+$lang_plugin_filetypes_editor['image'] = 'Image';
+$lang_plugin_filetypes_editor['movie'] = 'Vidéo';
+$lang_plugin_filetypes_editor['audio'] = 'Audio';
+$lang_plugin_filetypes_editor['document'] = 'Document';
+$lang_plugin_filetypes_editor['no_player'] = 'Aucun lecteur';
+$lang_plugin_filetypes_editor['wmp'] = 'Windows Media Player';
+$lang_plugin_filetypes_editor['qt'] = 'Apple QuickTime';
+$lang_plugin_filetypes_editor['rmp'] = 'Real Media Player';
+$lang_plugin_filetypes_editor['swf'] = 'Flash';
+$lang_plugin_filetypes_editor['htmlaudio'] = 'Balise audio HTML5';
+$lang_plugin_filetypes_editor['htmlvideo'] = 'Balise vidéo HTML5';
+$lang_plugin_filetypes_editor['author'] = 'Créé par %s, modifiée par %s';
+?>
