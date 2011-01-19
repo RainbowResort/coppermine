be869881e0b1f6846034ce82029827e09114937d
eenemeenemuu <eenemeenemuu@62f5abee-3c11-0410-b416-987344bf19e2>
2011-01-19 15:04:03 +0000

    Fixed 'null' issue when menu icons are disabled.

--- plugins/annotate/init.inc.php
+++ plugins/annotate/init.inc.php
@@ -32,21 +32,30 @@ function annotate_initialize() {
         $annotate_icon_array['permission_read'] = '<img src="./plugins/annotate/images/icons/visible.png" border="0" width="16" height="16" alt="" class="icon" />';
         $annotate_icon_array['permission_write'] = '<img src="./plugins/annotate/images/icons/edit.png" border="0" width="16" height="16" alt="" class="icon" />';
         $annotate_icon_array['permission_delete'] = '<img src="./plugins/annotate/images/icons/delete.png" border="0" width="16" height="16" alt="" class="icon" />';
+        $annotate_icon_array['announcement'] = cpg_fetch_icon('announcement', 1);
+        $annotate_icon_array['configure'] = cpg_fetch_icon('config', 1);
+        $annotate_icon_array['update_database'] = cpg_fetch_icon('update_database', 1);
+        $annotate_icon_array['import'] = cpg_fetch_icon('download', 1);
+        $annotate_icon_array['manage'] = cpg_fetch_icon('edit', 1);
+        $annotate_icon_array['ok'] = cpg_fetch_icon('ok', 2);
+        $annotate_icon_array['cancel'] = cpg_fetch_icon('cancel', 2);
+        $annotate_icon_array['delete'] = cpg_fetch_icon('delete', 2);
     } else {
         $annotate_icon_array['annotate'] = '';
         $annotate_icon_array['permission_none'] = '';
         $annotate_icon_array['permission_read'] = '';
         $annotate_icon_array['permission_write'] = '';
         $annotate_icon_array['permission_delete'] = '';
+        $annotate_icon_array['announcement'] = '';
+        $annotate_icon_array['configure'] = '';
+        $annotate_icon_array['update_database'] = '';
+        $annotate_icon_array['import'] = '';
+        $annotate_icon_array['manage'] = '';
+        $annotate_icon_array['ok'] = '';
+        $annotate_icon_array['cancel'] = '';
+        $annotate_icon_array['delete'] = '';
     }
-    $annotate_icon_array['announcement'] = cpg_fetch_icon('announcement', 1);
-    $annotate_icon_array['configure'] = cpg_fetch_icon('config', 1);
-    $annotate_icon_array['update_database'] = cpg_fetch_icon('update_database', 1);
-    $annotate_icon_array['import'] = cpg_fetch_icon('download', 1);
-    $annotate_icon_array['manage'] = cpg_fetch_icon('edit', 1);
-    $annotate_icon_array['ok'] = cpg_fetch_icon('ok', 2);
-    $annotate_icon_array['cancel'] = cpg_fetch_icon('cancel', 2);
-    $annotate_icon_array['delete'] = cpg_fetch_icon('delete', 2);
+
     $return['language'] = $lang_plugin_annotate;
     $return['icon'] = $annotate_icon_array;
     return $return;
