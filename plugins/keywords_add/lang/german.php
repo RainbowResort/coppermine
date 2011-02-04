9cf3b22fc990df69b80869f5bcde151b312fabe8
eenemeenemuu <eenemeenemuu@62f5abee-3c11-0410-b416-987344bf19e2>
2011-02-04 11:04:13 +0000

    Updated German language file.

--- plugins/keywords_add/lang/german.php
+++ plugins/keywords_add/lang/german.php
@@ -18,20 +18,20 @@
 if (!defined('IN_COPPERMINE')) { die('Not in Coppermine...'); }
 
 $lang_plugin_keywords_add = array(
-  'display_name'  => 'Keywords add',			// Display Name
-  'config_title'  => 'Verwendung von Keywords add',			// Title of the button on the gallery config menu
-  'config_button' => 'Keywords add',				// Label of the button on the gallery config menu
-  'install_note'  => 'Um das Plugin zu verwenden nutze den Button in der Admin Toolbar.',	// Note about configuring plugin
-  'install_click' => 'Klicke den Button um das PlugIn zu installieren.',	// Message to install plugin
+  'display_name'  => 'Keywords add', // Display Name
+  'config_title'  => 'Verwendung von Keywords add', // Title of the button on the gallery config menu
+  'config_button' => 'Keywords add', // Label of the button on the gallery config menu
+  'install_note'  => 'Um das Plugin zu verwenden nutze den Button in der Admin Toolbar.', // Note about configuring plugin
+  'install_click' => 'Klicke den Button um das PlugIn zu installieren.', // Message to install plugin
   'version'       => 'Ver 1.1',
-  'album_name'    => 'W&auml;hle das Album aus, in welches Du die Stichworte hinzuf&uuml;gen willst',
-  'add_info'      => 'Stichworte hinzuf&uuml;gen',
-  'keyword'	  => 'Stichworte',
+  'album_name'    => 'Wähle das Album aus, in welches Du die Stichworte hinzufügen willst',
+  'add_info'      => 'Stichworte hinzufügen',
+  'keyword'       => 'Stichworte',
   'caution'       => 'Vorsicht: Bei Titel, Beschreibung und benutzerdefinierten Feldern werden Informationen, welche bereits eingegeben wurden, <br> 
   durch die hier eingegebenen neuen Informationen ersetzt.<br>
   Diese Felder deshalb frei lassen, um den bestehenden Inhalt zu erhalten.',
-  'title'	    => 'Titel',
-  'description'      => 'Beschreibung',
+  'title'         => 'Titel',
+  'description'   => 'Beschreibung',
 );
 
 $lang_plugin_keywords_add_config = array(
