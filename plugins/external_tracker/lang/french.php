22a1b5190dda4fcb7c5924eb8a64f0766ccb0701
frantzk <frantzk@62f5abee-3c11-0410-b416-987344bf19e2>
2011-02-26 10:03:32 +0000

    translation (by FBleu) of the external_tracker plugin (lang file and doc)

--- plugins/external_tracker/lang/french.php
+++ plugins/external_tracker/lang/french.php
@@ -15,22 +15,21 @@
 if (!defined('IN_COPPERMINE')) { die('Not in Coppermine...'); }
 
 $lang_plugin_external_tracker = array(
-
-'clean_up_question' => 'Suprimer les parametres?',
-'clean_up_inst'     => 'Le cookie a été installé ou mis à jour. Son durée de vie est de 10 ans.',
-'changes_saved'     => 'Vos changements ont été sauvés.',
-'no_changes'        => 'Pas de changement.',
-'acitive'           => 'Active',
+'clean_up_question' => 'Suppression des paramètres ?',
+'clean_up_inst'     => 'Le cookie est installé ou mis à jour avec succès. Le cookie expirera dans 10 ans.',
+'changes_saved'     => 'Vos modifications ont été enregistrées.',
+'no_changes'        => 'Il n\'y a eu aucun changement.',
+'active'            => 'Actif',
 'service_name'      => 'Nom du service',
-'tracker'           => 'Tracker',
-'extra_settings'    => 'Paramètre extra',
+'tracker'           => 'Traqueur',
+'extra_settings'    => 'Paramètres supplémentaire',
 'help'              => 'Aide',
 'help_for_service'  => 'Aide pour %s',
-'description'       => 'Ce plugin ajoute un code pour un tracker externe dans chaque page si vous vous n\'êtes pas connecté en tant qu\'administrateur. Un exlusion additionnel par cookie est aussi disponible après l\'installation de ce plugin.',
+'description'       => 'Ce plugin ajoute un code de traqueur externe à chaque page si vous n’êtes pas connecté en tant qu\'administrateur. Un cookie d’exclusion supplémentaire est également disponible après l\'installation de ce plugin.',
 'credits'           => 'Crédits complets',
-'support_track'     => 'Trackers supportés',
+'support_track'     => 'Traqueurs pris en charge',
 'and'               => 'et',
-'supp_thread'       => 'Thread pour support',
+'supp_thread'       => 'Forum pour support',
 'install_cookie'    => 'Installer ou mettre à jour le cookie',
 );
 ?>
