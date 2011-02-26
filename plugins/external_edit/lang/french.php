a73d1834b3c90ae2fc2c86b38df5cbd72e3f7cdf
frantzk <frantzk@62f5abee-3c11-0410-b416-987344bf19e2>
2011-02-26 09:00:34 +0000

    translation oft the external_edit plugin (by FBleu)

---
+++ plugins/external_edit/lang/french.php
@@ -0,0 +1,53 @@
+<?php
+/**************************************************
+  Coppermine 1.5.x Plugin - external_edit
+  *************************************************
+  Copyright (c) 2010 Joachim Müller
+  *************************************************
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 3 of the License, or
+  (at your option) any later version.
+  ********************************************
+  $HeadURL: https://coppermine.svn.sourceforge.net/svnroot/coppermine/branches/cpg1.5.x/plugins/external_edit/lang/english.php $
+  $Revision: 7977 $
+  $LastChangedBy: gaugau $
+  $Date: 2010-10-15 17:08:34 +0200 (ven. 15 oct. 2010) $
+  **************************************************/
+
+$lang_plugin_external_edit['plugin_name'] = 'Édition externe';
+$lang_plugin_external_edit['plugin_description'] = 'Vous permet d\'éditer les images de votre galerie en utilisant l\'éditeur d\'images en ligne de %s';
+$lang_plugin_external_edit['author'] = '%s';
+$lang_plugin_external_edit['announcement_thread'] = 'Sujet d\'annonce';
+$lang_plugin_external_edit['plugin_description_detail'] = 'Lorsque vous utilisez ce plugin, vous verrez un autre bouton à côté du bouton d\'édition des fichiers qui vous permets de faire des envois à l\'éditeur d\'image dans une fenêtre iframe séparée, où vous pouvez éditer / couper / faire une rotation de votre fichier d\'une manière qui équivaut à pratiquement celle d’un éditeur d’images fonctionnant sur un ordinateur de bureau. Une fois que vous avez terminé, le fichier est sauvegardé à nouveau dans Coppermine, en conservant vos modifications.';
+$lang_plugin_external_edit['edit_file'] = 'Modifier le fichier via Fotoflexer';
+$lang_plugin_external_edit['edit_file_explain'] = 'Vous permet d\'éditer les images de votre galerie en utilisant l\'éditeur d\'images en ligne de Fotoflexer.com';
+$lang_plugin_external_edit['error_referer'] = 'Référence inadéquate';
+$lang_plugin_external_edit['error_token'] = 'Symbole inadéquate';
+$lang_plugin_external_edit['error_user'] = 'Utilisateur inadéquate';
+$lang_plugin_external_edit['error_lookup'] = 'Recherche échoué';
+$lang_plugin_external_edit['error_broken'] = 'Le fichier semble être corrompu. Restauration d\'une sauvegarde.';
+$lang_plugin_external_edit['importing_remote_image'] = 'Importation d\'images à distance';
+$lang_plugin_external_edit['referer_check'] = 'Vérification de la référence %s';
+$lang_plugin_external_edit['token_check'] = 'Vérification du symbole %s';
+$lang_plugin_external_edit['user_auth'] = 'Authentification de l\'utilisateur %s';
+$lang_plugin_external_edit['database_record'] = 'Enregistrement de la recherche dans la base de données %s';
+$lang_plugin_external_edit['create_backup'] = 'Créer une sauvegarde %s';
+$lang_plugin_external_edit['check_permissions'] = 'Vérifiaction des permissions';
+$lang_plugin_external_edit['copy_remote_file'] = 'Copie des fichiers distants %s';
+$lang_plugin_external_edit['file_integrity'] = 'Intégrité des fichiers %s';
+$lang_plugin_external_edit['thumbnail'] = 'Vignette %s';
+$lang_plugin_external_edit['intermediate'] = 'Intermédiaire %s';
+$lang_plugin_external_edit['resized_fullsize'] = 'Redimensionné la taille maximale %s';
+$lang_plugin_external_edit['watermarked_thumb'] = 'Filigranne pour la vignette %s';
+$lang_plugin_external_edit['watermarked_intermediate'] = 'Filigranne pour la taille intermédiare %s';
+$lang_plugin_external_edit['watermarked_fullsize'] = 'Filigranne pour la taille réelle %s';
+$lang_plugin_external_edit['unwatermarked_original'] = 'Sans filigranne sur l\'original %s';
+$lang_plugin_external_edit['original'] = 'Original %s';
+$lang_plugin_external_edit['failed'] = 'Échec';
+$lang_plugin_external_edit['skipped'] = 'Ignoré';
+$lang_plugin_external_edit['invalid'] = 'Invalide';
+$lang_plugin_external_edit['success'] = 'Succès';
+$lang_plugin_external_edit['created'] = 'Crée';
+
+?>
