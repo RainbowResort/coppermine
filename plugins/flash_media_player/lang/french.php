165032b247ae1d3011d872bc65e6e01273a58010
frantzk <frantzk@62f5abee-3c11-0410-b416-987344bf19e2>
2011-02-27 17:37:49 +0000

    Adding french translation to the flash_media_player plugin

---
+++ plugins/flash_media_player/lang/french.php
@@ -0,0 +1,27 @@
+<?php
+/**************************************************
+  Coppermine 1.5.x Plugin - Flash Media Player
+  *************************************************
+  Copyright (c) 2009 eenemeenemuu
+  ********************************************
+  $HeadURL: https://coppermine.svn.sourceforge.net/svnroot/coppermine/branches/cpg1.5.x/plugins/flash_media_player/lang/french.php $
+  $Revision: 6932 $
+  $LastChangedBy: eenemeenemuu $
+  $Date: 2009-12-30 10:54:33 +0100 (mer. 30 déc. 2009) $
+**************************************************/
+
+if (!defined('IN_COPPERMINE')) {
+    die('Not in Coppermine...');
+}
+
+$lang_plugin_flash_media_player['description'] = 'Utiliser %s pour lire les fichiers flv, mp4, mp3 &amp; aac. Prendre en considération la <a href="http://creativecommons.org/licenses/by-nc-sa/3.0/deed.en_US" class="external">licence non-commerciale</a>!';
+$lang_plugin_flash_media_player['announcement_thread'] = 'Sujet d\'annonce';
+$lang_plugin_flash_media_player['details'] = 'Détails';
+$lang_plugin_flash_media_player['extra_info_movie'] = 'Votre galerie est configurée pour autoriser le téléchargement des fichiers vidéo de types';
+$lang_plugin_flash_media_player['extra_info_sound'] = 'Votre galerie est configurée pour autoriser le téléchargement des fichiers audio de types';
+$lang_plugin_flash_media_player['extra_info_highlighted'] = 'Le lecteur JW Media player se chargera des types de fichiers mis en évidence';
+$lang_plugin_flash_media_player['extra_info_message_box'] = 'Pour déterminer le type de fichiers que vous pouvez télécharger dans Coppermine, Aller dans la configuration de Coppermine et modifier &quot;Paramètres des fichiers&quot; &rarr; &quot;Types de vidéos autorisées&quot; / &quot;Types de fichiers sons autorisés&quot; en conséquence';
+$lang_plugin_flash_media_player['install_info'] = 'Ce plugin permet de configurer Coppermine afin de permettre le téléchargement de fichiers flash et d’autres qu\'il supporte (FLV, MP4, MP4, AAC). Cela aura bien sûr un impact sur vous en tant qu\'administrateur ainsi que pour les utilisateurs enregistrés ! '.$lang_plugin_flash_media_player['extra_info_message_box'].'.';
+$lang_plugin_flash_media_player['uninstall_info'] = 'La lecture des fichiers flv, mp4, mp3 et aac existants ne fonctionnera plus';
+
+?>
