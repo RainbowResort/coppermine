88bd3c3d7eb72b8a9a6bf207e52ccfcc19509c94
eenemeenemuu <eenemeenemuu@62f5abee-3c11-0410-b416-987344bf19e2>
2011-05-19 13:09:39 +0000

    Updated Portuguese language file (user contribution).

--- plugins/limit_upload/lang/portuguese.php
+++ plugins/limit_upload/lang/portuguese.php
@@ -21,15 +21,15 @@ if (!defined('IN_COPPERMINE')) {
 
 $lang_plugin_limit_upload['limit_upload'] = 'Limite de envios';
 $lang_plugin_limit_upload['description'] = 'Limita os envios do utilizador para um número máximo ou para um determinado período';
-$lang_plugin_limit_upload['announcement_thread'] = 'Local da discussão';
-$lang_plugin_limit_upload['limit_reached_x'] = 'Atingiu o limite máximo de envios %s files %s.';
-$lang_plugin_limit_upload['limit_reached_wait'] = 'Deve esperar %s %s antes de enviar outra foto.';
+$lang_plugin_limit_upload['announcement_thread'] = 'Announcement thread';
+$lang_plugin_limit_upload['limit_reached_x'] = 'Atingiu o limite máximo de envios - %s fotografias %s.';
+$lang_plugin_limit_upload['limit_reached_wait'] = 'Deverá esperar %s %s antes de enviar outra fotografia.';
 $lang_plugin_limit_upload['saved'] = 'Configuração guardada.';
 $lang_plugin_limit_upload['upload_limit'] = 'Limite de envios (total de ficheiros ou período de tempo)';
 $lang_plugin_limit_upload['upload_limit_values'] = array (
     'total' => 'total',
     'hour' => 'por hora',
-    'day' => 'por dias (24 horas)',
+    'day' => 'por dia (24 horas)',
     'week' => 'por semana (7 dias)',
     'month' => 'por mês (30 dias)',
     'year' => 'por ano (365 dias)'
