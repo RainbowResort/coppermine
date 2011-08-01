6962647aa6a6959016232a0ddc410639b655e62c
eenemeenemuu <eenemeenemuu@62f5abee-3c11-0410-b416-987344bf19e2>
2011-08-01 10:58:04 +0000

    Preparing release of cpg1.5.14.

--- cpg1.5.x/js/scripts.js
+++ cpg1.5.x/js/scripts.js
@@ -9,7 +9,7 @@
     as published by the Free Software Foundation.
 
     ********************************************
-    Coppermine version: 1.5.13
+    Coppermine version: 1.5.14
     $HeadURL$
     $Revision$
 **********************************************/
