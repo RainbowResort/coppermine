85935273247e6132cf373ea476ee4e83b5fb3c54
eenemeenemuu <eenemeenemuu@62f5abee-3c11-0410-b416-987344bf19e2>
2011-08-02 08:37:10 +0000

    Counted version count up in SVN repository from cpg1.5.14 to cpg1.5.15.

--- cpg1.5.x/include/init.inc.php
+++ cpg1.5.x/include/init.inc.php
@@ -10,12 +10,12 @@
   as published by the Free Software Foundation.
 
   ********************************************
-  Coppermine version: 1.5.14
+  Coppermine version: 1.5.15
   $HeadURL$
   $Revision$
 **********************************************/
 
-define('COPPERMINE_VERSION', '1.5.14');
+define('COPPERMINE_VERSION', '1.5.15');
 define('COPPERMINE_VERSION_STATUS', 'stable');
 
 if (!defined('IN_COPPERMINE')) {
--- cpg1.5.x/include/iptc.inc.php
+++ cpg1.5.x/include/iptc.inc.php
@@ -10,7 +10,7 @@
   as published by the Free Software Foundation.
   
   ********************************************
-  Coppermine version: 1.5.14
+  Coppermine version: 1.5.15
   $HeadURL$
   $Revision$
 
