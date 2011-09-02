b2165d98a3872172edd5759966f7bc481b19c13d
eenemeenemuu <eenemeenemuu@62f5abee-3c11-0410-b416-987344bf19e2>
2011-09-02 09:57:48 +0000

    Added random_file_name_suffix plugin.

---
+++ plugins/random_file_name_suffix/codebase.php
@@ -0,0 +1,36 @@
+<?php
+/**************************************************
+  Coppermine 1.5.x Plugin - Random File Name Suffix
+  *************************************************
+  Copyright (c) 2011 eenemeenemuu
+  *************************************************
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 3 of the License, or
+  (at your option) any later version.
+  ********************************************
+  $HeadURL$
+  $Revision$
+  $LastChangedBy$
+  $Date$
+**************************************************/
+
+if (!defined('IN_COPPERMINE')) die('Not in Coppermine...');
+
+$thisplugin->add_filter('upload_file_name', 'rfns_upload_file_name');
+
+function rfns_upload_file_name($picture_name) {
+
+    $characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    for ($i = 0; $i < 6; $i++) {
+        $suffix .= $characters[mt_rand(0, strlen($characters)-1)];
+    }
+
+    $picture_name_parts = explode(".", $picture_name);
+    $count = count($picture_name_parts);
+    $picture_name_parts[$count] = $picture_name_parts[$count-1];
+    $picture_name_parts[$count-1] = $suffix;
+    $picture_name = implode(".", $picture_name_parts);
+
+    return $picture_name;
+}
---
+++ plugins/random_file_name_suffix/configuration.php
@@ -0,0 +1,22 @@
+<?php
+/**************************************************
+  Coppermine 1.5.x Plugin - Random File Name Suffix
+  *************************************************
+  Copyright (c) 2011 eenemeenemuu
+  *************************************************
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 3 of the License, or
+  (at your option) any later version.
+  ********************************************
+  $HeadURL$
+  $Revision$
+  $LastChangedBy$
+  $Date$
+**************************************************/
+
+$name = 'Random File Name Suffix';
+$description = 'Adds a random suffix to the file name during upload';
+$author='eenemeenemuu';
+$version='0.1';
+$plugin_cpg_version = array('min' => '1.5');
