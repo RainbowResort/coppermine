4f846f854ff0bb86e1a613375d00040d90a1316b
eenemeenemuu <eenemeenemuu@62f5abee-3c11-0410-b416-987344bf19e2>
2011-10-21 14:02:18 +0000

    Updated version number to 1.1 (first public release).

--- plugins/CPGAnis/configuration.php
+++ plugins/CPGAnis/configuration.php
@@ -23,7 +23,7 @@ $name = 'ANIS Java Applet';
 $description = 'Dynamic Pan and Zoom of full size images using <a href="http://www.ssec.wisc.edu/visit/AniS/" target="_blank">ANIS</a>.';
 $author = 'Created for cpg1.4.x by Donnoman@donovanbray.com from <a href="http://cpg-contrib.org" target="_blank" rel="external" class="external">cpg-contrib.org</a>';
 $author .= '<br />Ported to cpg1.5.x by <a href="http://forum.coppermine-gallery.net/index.php?action=profile;u=24278" rel="external" class="external">eenemeenemuu</a>';
-$version = '1.0';
+$version = '1.1';
 $plugin_cpg_version = array('min' => '1.5');
-$extra_info = $install_info = '<a href="http://forum.coppermine-gallery.net/index.php/topic,TODO.0.html" rel="external" class="admin_menu">'.cpg_fetch_icon('announcement', 1).'Announcement thread</a>';
+$extra_info = $install_info = '<a href="http://forum.coppermine-gallery.net/index.php/topic,73757.0.html" rel="external" class="admin_menu">'.cpg_fetch_icon('announcement', 1).'Announcement thread</a>';
 ?>
