8ca8007d19695ea2116aef43636182c57255b4b2
eenemeenemuu <eenemeenemuu@62f5abee-3c11-0410-b416-987344bf19e2>
2012-01-31 15:32:50 +0000

    Fixed broken IP address lookup (thread ID 74346).

--- cpg1.5.x/banning.php
+++ cpg1.5.x/banning.php
@@ -27,6 +27,11 @@ if (!GALLERY_ADMIN_MODE) {
     cpg_die(ERROR, $lang_errors['access_denied'], __FILE__, __LINE__);
 }
 
+if ($superCage->post->keyExists('ip_lookup')) {
+    header("Location: http://whois.domaintools.com/".$superCage->post->getRaw('ip_lookup'));
+    exit;
+}
+
 js_include('js/date.js');
 js_include('js/jquery.datePicker.js');
 js_include('js/banning.js');
@@ -635,7 +640,7 @@ print '<input type="hidden" name="timestamp" value="' . $timestamp . '" />';
 print <<< EOT
 </form>
 <br />
-<form action="http://ws.arin.net/whois/" method="get" name="lookup" id="cpgform2" target="_blank">
+<form action="{$CPG_PHP_SELF}" method="post" name="lookup" id="cpgform2" target="_blank">
 EOT;
 
 starttable('-2','','','');
@@ -645,7 +650,7 @@ print <<< EOT
             <strong>{$lang_banning_php['lookup_ip']}</strong>{$help_array['ip_lookup']}
         </td>
         <td class="tableb">
-            <input type="text" class="textinput" size="20" name="queryinput" value="{$comm_info['msg_ip']}" maxlength="15" />
+            <input type="text" class="textinput" size="20" name="ip_lookup" value="{$comm_info['msg_ip']}" maxlength="15" />
         </td>
         <td class="tableb">
             <button type="submit" class="button" name="submit" id="submit_lookup" value="{$lang_common['ok']}" style="display:block">{$icon_array['go']}{$lang_common['ok']}</button>
