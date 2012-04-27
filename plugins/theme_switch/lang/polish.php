bd008bf8e13ce257bd4d9784586081f1d665967e
eenemeenemuu <eenemeenemuu@62f5abee-3c11-0410-b416-987344bf19e2>
2012-04-27 08:17:06 +0000

    Added Portuguese and Polish language files (user contributions).

---
+++ plugins/theme_switch/lang/polish.php
@@ -0,0 +1,35 @@
+<?php
+/**************************************************
+  Coppermine 1.5.x Plugin - Theme switch
+  *************************************************
+  Copyright (c) 2010-2012 eenemeenemuu
+  *************************************************
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 3 of the License, or
+  (at your option) any later version.
+  ********************************************
+  $HeadURL$
+  $Revision$
+  $LastChangedBy$
+  $Date$
+**************************************************/
+/*********************************************
+  Language file for the  Theme switch plugin
+  ********************************************
+ Polish translation by pixstreet
+ Web site: http://www.pixstreet.co.uk
+ Contact: http://www.pixstreet.co.uk/gallery/contact.php 
+**********************************************/
+
+if (!defined('IN_COPPERMINE')) die('Not in Coppermine...');
+
+$lang_plugin_theme_switch['theme_switch'] = 'Theme switch';
+$lang_plugin_theme_switch['description'] = 'Zmienia motyw dla urządzeń mobilnych (n.p. smartfonów)';
+$lang_plugin_theme_switch['announcement_thread'] = 'Zobacz zapowiedź';
+$lang_plugin_theme_switch['select'] = 'Motywy dla urządzeń mobilnych';
+$lang_plugin_theme_switch['saved'] = 'Twoje ustawienia zostały zapisane.';
+$lang_plugin_theme_switch['mobile_view'] = 'Wersja mobilna';
+$lang_plugin_theme_switch['normal_view'] = 'Wersja klasyczna';
+
+?>
---
+++ plugins/theme_switch/lang/portuguese.php
@@ -0,0 +1,36 @@
+<?php
+/**************************************************
+  Coppermine 1.5.x Plugin - Theme switch
+  *************************************************
+  Copyright (c) 2010-2012 eenemeenemuu
+  *************************************************
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 3 of the License, or
+  (at your option) any later version.
+  ********************************************
+  $HeadURL$
+  $Revision$
+  $LastChangedBy$
+  $Date$
+**************************************************/
+/*********************************************
+  Language file for the  Theme switch plugin
+  ********************************************
+ Portuguese translation by 
+ Afonso de Bragança
+ Web site: http://www.photoaves.com
+ Email: afonso@photoaves.com
+**********************************************/
+
+if (!defined('IN_COPPERMINE')) die('Not in Coppermine...');
+
+$lang_plugin_theme_switch['theme_switch'] = 'Alterador do Tema';
+$lang_plugin_theme_switch['description'] = 'Muda o tema para uso em aparelhos portáteis (ex: celulares, Iphones, Ipads)';
+$lang_plugin_theme_switch['announcement_thread'] = 'Linha de suporte para o plugin';
+$lang_plugin_theme_switch['select'] = 'Tema para aparelhos portáteis';
+$lang_plugin_theme_switch['saved'] = 'Suas configurações foram salvas.';
+$lang_plugin_theme_switch['mobile_view'] = 'Visão portátil';
+$lang_plugin_theme_switch['normal_view'] = 'Visão normal';
+
+?>
