0007579f52298361798ae18213167d1c83a3d04b
eenemeenemuu <eenemeenemuu@62f5abee-3c11-0410-b416-987344bf19e2>
2012-05-02 13:12:19 +0000

    Added Polish language file (user contribution).

---
+++ plugins/bbcode_control/lang/polish.php
@@ -0,0 +1,68 @@
+<?php
+/*************************
+  Coppermine Photo Gallery
+  ************************
+  Copyright (c) 2003-2008 Dev Team
+  v1.1 originally written by Gregory DEMAR
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License version 3
+  as published by the Free Software Foundation.
+
+  ********************************************
+  Coppermine version: 1.5.x
+  $HeadURL$
+  $Revision$
+  $LastChangedBy$
+  $Date$
+**********************************************/
+/*********************************************
+  Language file for the bbcode_control plugin
+  ********************************************
+  Polish translation by doppler
+**********************************************/
+
+// admin.php
+$lang_plugin_bbcode_control['process_bbcode'] = 'Włącz BBCode:';
+$lang_plugin_bbcode_control['display_buttons'] = 'Wyświetlaj przyciski:';
+$lang_plugin_bbcode_control['enable_admin'] = 'Tylko admin';
+$lang_plugin_bbcode_control['enable_all'] = 'Wszyscy';
+$lang_plugin_bbcode_control['all_buttons'] = 'Wszystkie kody';
+$lang_plugin_bbcode_control['saved'] = 'Twoje ustawienia zostały zapisane';
+$lang_plugin_bbcode_control['local_images'] = 'Wyświetlaj obrazki tylko we własnej domenie (przeciwdziałanie nieuprawnionemu wykorzystywaniu)';
+$lang_plugin_bbcode_control['limit_image_width'] = 'Maksymalna szerokość obrazka (0 = bez limitu)';
+$lang_plugin_bbcode_control['limit_image_height'] = 'Maksymalna wysokość obrazka (0 = bez limitu)';
+
+// admin.php & codebase.php
+$lang_plugin_bbcode_control['b'] = 'Pogrubienie [b]';
+$lang_plugin_bbcode_control['u'] = 'Podkreślenie [u]';
+$lang_plugin_bbcode_control['i'] = 'Pochylenie [i]';
+$lang_plugin_bbcode_control['s'] = 'Przekreślenie [s]';
+$lang_plugin_bbcode_control['img'] = 'Obrazek [img]';
+$lang_plugin_bbcode_control['url'] = 'Link [url]';
+$lang_plugin_bbcode_control['color'] = 'Kolor czcionki [color]';
+$lang_plugin_bbcode_control['size'] = 'Wielkość czcionki [size]';
+$lang_plugin_bbcode_control['quote'] = 'Cytat [quote]';
+$lang_plugin_bbcode_control['youtube'] = 'Youtube wideo [youtube]';
+$lang_plugin_bbcode_control['tt'] = 'Pismo maszynowe [tt]';
+$lang_plugin_bbcode_control['hr'] = 'Linia pozioma [hr]';
+
+
+// admin.php & configuration.php
+$lang_plugin_bbcode_control['show_in_file_info'] = 'Pokaż tylko wbudowane BBCody w informacjach';
+
+// codebase.php
+$lang_plugin_bbcode_control['cleanup'] = 'Chcesz usunąć konfigurację pluginu z bazy?';
+$lang_plugin_bbcode_control['config_link_title'] = 'Konfiguracja pluginu BBCode Control';
+$lang_plugin_bbcode_control['save_bandwidth'] = 'Zdjęcie nie wyświtlane poza galerią! Kliknij tu aby je zobaczyć.';
+$lang_plugin_bbcode_control['please_enter_text'] = 'Wprowadź tekst który chcesz zobaczyć sformatowany';
+$lang_plugin_bbcode_control['insert_at_position'] = 'Wprowadzać w pozycji kursora';
+
+// configuration.php
+$lang_plugin_bbcode_control['description_new_codes'] = 'Dodaj nowe BBCody do Twojej galerii';
+$lang_plugin_bbcode_control['description_control_codes'] = 'włącz/wyłącz BBCody';
+$lang_plugin_bbcode_control['description_buttons'] = 'Kontroluj wyświetlanie BBCodów przez dodanie/edycję komentarzy';
+$lang_plugin_bbcode_control['install_info'] = 'Możesz konfigurować plugin po instalacji używając odpowiadającego mu pliku aministracyjnego';
+$lang_plugin_bbcode_control['announcement_thread'] = 'Ogłoszenia';
+
+?>
