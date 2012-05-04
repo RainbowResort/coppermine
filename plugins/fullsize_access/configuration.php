592fb4de8b0da571caa80160f59721bc784f3c5b
eenemeenemuu <eenemeenemuu@62f5abee-3c11-0410-b416-987344bf19e2>
2012-05-04 10:37:39 +0000

    Added plugin 'fullsize_access'.

---
+++ plugins/fullsize_access/configuration.php
@@ -0,0 +1,11 @@
+<?php
+$name='Secured fullsize download';
+$description = <<<EOT
+Secures access to fullsize images and adds a download link for registered users. <br />
+Adds secured links for downloading albums and favorites as a zip file. <br />
+Adds a download history mechanism and statistics page
+EOT;
+$author='Klaus Schwarzburg';
+$version='0.2';
+$plugin_cpg_version = array('min' => '1.5');
+?>
---
+++ plugins/fullsize_access/fullsize_check.php
@@ -0,0 +1,9 @@
+<?php
+function fullsize_check_user(){
+	if( USER_ID ){
+		return(true);
+	} else{
+		return(false);
+	}	
+}
+?>
