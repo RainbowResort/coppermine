6d555084e531e1bf9a8fc34f6dd23caf24d24dbc
eenemeenemuu <eenemeenemuu@62f5abee-3c11-0410-b416-987344bf19e2>
2012-05-16 07:27:52 +0000

    Added Italian language file (user contribution).

---
+++ plugins/theme_switch/lang/italian.php
@@ -0,0 +1,28 @@
+<?php
+/**************************************************
+  Coppermine 1.5.x Plugin - Theme switch
+  *************************************************
+  Copyright (c) 2010-2012 eenemeenemuu
+  *************************************************
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 3 of the License, or
+  (at your option) any later version.
+  ********************************************
+  $HeadURL$
+  $Revision$
+  $LastChangedBy$
+  $Date$
+**************************************************/
+
+if (!defined('IN_COPPERMINE')) die('Not in Coppermine...');
+
+$lang_plugin_theme_switch['theme_switch'] = 'Cambia tema';
+$lang_plugin_theme_switch['description'] = 'Cambia il tema per i dispositivi mobile (es. smartphone)';
+$lang_plugin_theme_switch['announcement_thread'] = 'Thread ufficiale';
+$lang_plugin_theme_switch['select'] = 'Tema per i dispositivi mobile';
+$lang_plugin_theme_switch['saved'] = 'Impostazioni salvate';
+$lang_plugin_theme_switch['mobile_view'] = 'Tema Mobile';
+$lang_plugin_theme_switch['normal_view'] = 'Tema Classico';
+
+?>
