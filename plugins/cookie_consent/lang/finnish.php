3a4c8c5fa2260f49b452b8806129e4aba3245dae
eenemeenemuu <eenemeenemuu@62f5abee-3c11-0410-b416-987344bf19e2>
2012-06-20 06:51:36 +0000

    Added Finnish language file (user contribution). Updated German language file.

---
+++ plugins/cookie_consent/lang/finnish.php
@@ -0,0 +1,28 @@
+<?php
+/**************************************************
+  Coppermine 1.5.x Plugin - Cookie consent
+  *************************************************
+  Copyright (c) 2012 eenemeenemuu
+  ********************************************
+  $HeadURL$
+  $Revision$
+  $LastChangedBy$
+  $Date$
+**************************************************/
+
+if (!defined('IN_COPPERMINE')) die('Not in Coppermine...');
+
+$lang_plugin_cookie_consent['name'] = 'Lupa evästeille';
+$lang_plugin_cookie_consent['description'] = 'Pyytää käyttäjältä luvan, jotta Coppermine voi asettaa evästeitä EU Direktiivin %s mukaan.';
+$lang_plugin_cookie_consent['announcement_thread'] = 'Tiedoteviesti';
+$lang_plugin_cookie_consent['why_cookies'] = 'Haluaisimme asettaa evästeitä tietokoneellesi. Saat lisätietoa <a href="index.php?file=cookie_consent/info">täältä</a>.';
+$lang_plugin_cookie_consent['accept'] = 'Hyväksyn evästeiden käytön tällä sivustolla.';
+$lang_plugin_cookie_consent['cookie_name'] = 'Evästeen nimi';
+$lang_plugin_cookie_consent['used_for'] = 'Käyttö';
+$lang_plugin_cookie_consent['cookie_data'] = 'Tallentaa useita käyttäjän asetuksia, kuten kieli, lajittelujärjestys tai hakuehdot.';
+$lang_plugin_cookie_consent['cookie_cpg_session'] = 'Istuntoeväste, kun galleriaa ei ole integroitu toisen ohjelman kanssa.';
+$lang_plugin_cookie_consent['cookie_mambo_sessioncookie'] = 'Istuntoeväste, kun galleria on integroitu Mambon kanssa.';
+$lang_plugin_cookie_consent['cookie_picinfo'] = 'Tallentaa tiedoston tietolaatikon näkyvyyden.';
+$lang_plugin_cookie_consent['cookie_fav'] = 'Tallentaa käyttäjän suosikkikuvat.';
+
+?>
--- plugins/cookie_consent/lang/german.php
+++ plugins/cookie_consent/lang/german.php
@@ -19,6 +19,10 @@ $lang_plugin_cookie_consent['why_cookies'] = 'Wir wollen Cookies auf Deinem Comp
 $lang_plugin_cookie_consent['accept'] = 'Ich akzeptiere Cookies von dieser Webseite.';
 $lang_plugin_cookie_consent['cookie_name'] = 'Cookie-Name';
 $lang_plugin_cookie_consent['used_for'] = 'Verwendung';
-
+$lang_plugin_cookie_consent['cookie_data'] = 'Speichert verschiedene Benutzereinstellungen (z.B. Sprache, Sortierung, Suchparameter)';
+$lang_plugin_cookie_consent['cookie_cpg_session'] = 'Session-Cookie für Galerien ohne Verbindung zu einer externen Software (meistens Foren)';
+$lang_plugin_cookie_consent['cookie_mambo_sessioncookie'] = 'Session-Cookie für Galerien, die mit der Software "Mambo" verbunden sind';
+$lang_plugin_cookie_consent['cookie_picinfo'] = 'Speichert die letzte Auswahl, ob die Dateiinformationen angezeigt werden soll oder nicht';
+$lang_plugin_cookie_consent['cookie_fav'] = 'Speichert die Favoriten';
 
 ?>
